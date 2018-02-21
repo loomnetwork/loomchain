0156d929ca658e50be0ed64e8475c3687c9ed60d
Vadim Macagon <vadim.macagon@gmail.com>
2018-02-21 20:34:30 +1000

    Add an example of a simple REST server that provides a query API
    The Unity SDK sample was designed to work with this example REST server.
    This example server forwards txs to the DAppChain and runs queries via
    Tendermint ABCI.

--- core/src/loom/examples/experiment/experiment.go
+++ core/src/loom/examples/experiment/experiment.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -46,7 +47,10 @@ func (a *experimentHandler) Handle(state loom.State, txBytes []byte) (loom.TxHan
 	if err := proto.Unmarshal(txBytes, tx); err != nil {
 		return r, err
 	}
-	logger.Debug(fmt.Sprintf("Got DummyTx with value '%s'", tx.GetVal()))
+	logger.Debug(fmt.Sprintf("Got DummyTx { key: '%s', value: '%s' }", tx.Key, tx.Val))
+	// Run the tx & update the app state as needed.
+	saveDummyValue(state, tx.Key, tx.Val)
+	saveLastDummyKey(state, tx.Key)
 	return r, nil
 }
 
@@ -77,7 +81,7 @@ func startCmd(cmd *cobra.Command, args []string) error {
 			},
 			&experimentHandler{},
 		),
-		QueryHandler: &queryLogger{},
+		QueryHandler: &queryHandler{},
 		Store:        store.GetCommitKVStore(mainStoreKey),
 	}
 	return startTendermint(app)
@@ -113,10 +117,51 @@ func startTendermint(app abci.Application) error {
 	return nil
 }
 
-type queryLogger struct {
+type queryHandler struct {
 }
 
-func (q *queryLogger) Handle(state loom.State, path string, data []byte) ([]byte, error) {
+func (q *queryHandler) Handle(state loom.State, path string, data []byte) ([]byte, error) {
 	logger.Info(fmt.Sprintf("Query received, path: '%s', data: '%v'", path, data))
-	return nil, fmt.Errorf("Invalid query for path '%s'", path)
+	var val string
+	var err error
+
+	switch path {
+	case "app/last-key":
+		val, err = loadLastDummyKey(state)
+		break
+	case "app/dummy":
+		val, err = loadDummyValue(state, string(data))
+		break
+	default:
+		return nil, fmt.Errorf("Invalid query for path '%s'", path)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	return []byte(val), nil
+}
+
+func saveLastDummyKey(state loom.State, key string) {
+	state.Set([]byte("app/last-key"), []byte(key))
+}
+
+func loadLastDummyKey(state loom.State) (string, error) {
+	val := state.Get([]byte("app/last-key"))
+	if len(val) == 0 {
+		return "", errors.New("last key not set")
+	}
+	return string(val), nil
+}
+
+func saveDummyValue(state loom.State, key, val string) {
+	state.Set([]byte("app/dummy/"+key), []byte(val))
+}
+
+func loadDummyValue(state loom.State, key string) (string, error) {
+	val := state.Get([]byte("app/dummy/" + key))
+	if len(val) == 0 {
+		return "", fmt.Errorf("no value stored for key '%s'", key)
+	}
+	return string(val), nil
 }
