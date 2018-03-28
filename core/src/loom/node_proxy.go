62c9951210873223137cc7398c236f9157825809
Vadim Macagon <vadim.macagon@gmail.com>
2018-03-28 22:25:46 +1000

    Simplify the proxying of the ABCI API
    The example server can now proxy any REST/JSONRPC/WS ABCI request to the
    DAppChain node, in theory. In practice only `broadcast_tx_commit` is
    currently proxied.

--- core/src/loom/node_proxy.go
+++ core/src/loom/node_proxy.go
@@ -1,15 +1,11 @@
 package loom
 
 import (
-	"errors"
-	"io/ioutil"
-	"net/http"
-
-	"github.com/gorilla/mux"
 	rpcclient "github.com/tendermint/tendermint/rpc/client"
+	rpcserver "github.com/tendermint/tendermint/rpc/lib/server"
 )
 
-// NodeProxy exposes a REST API for sending txs to a DAppChain node.
+// NodeProxy proxies a subset of the Tendermint REST/RPC/WS API to a DAppChain node.
 type NodeProxy struct {
 	rpcClient rpcclient.Client
 }
@@ -20,33 +16,9 @@ func NewNodeProxy(c rpcclient.Client) *NodeProxy {
 	}
 }
 
-func (np *NodeProxy) CommitTx(w http.ResponseWriter, r *http.Request) {
-	if r == nil || r.Body == nil {
-		WriteError(w, errors.New("expecting a non-nil body"))
-		return
+// RPCRoutes returns a subset of RPC handlers for the Tendermint REST/RPC/WS API.
+func (np *NodeProxy) RPCRoutes() map[string]*rpcserver.RPCFunc {
+	return map[string]*rpcserver.RPCFunc{
+		"broadcast_tx_commit": rpcserver.NewRPCFunc(np.rpcClient.BroadcastTxCommit, "tx"),
 	}
-	defer r.Body.Close()
-
-	tx, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		WriteError(w, err)
-		return
-	}
-
-	resp, err := np.rpcClient.BroadcastTxCommit(tx)
-	if err != nil {
-		WriteError(w, err)
-		return
-	}
-
-	WriteSuccess(w, resp)
-}
-
-// RegisterCommitTx register a mux.Router handler that exposes a POST endpoint for committing
-// transaction to the DAppChain. The response to the POST request will contain the output from the
-// tx handler that processed the tx in the DAppChain node. Note that the response will only be
-// sent back after the block the tx ends up in gets committed.
-func (np *NodeProxy) RegisterCommitTx(r *mux.Router) error {
-	r.HandleFunc("/tx", np.CommitTx).Methods("POST")
-	return nil
 }
