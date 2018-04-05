c17f5b2a8ab5a1d1773d960578c872e346df88ad
sriep <piers.shepperson@btinternet.com>
2018-04-05 22:34:04 +0100

    Add ProcessSendTx and ProcessDeployTx plus testing code.

--- core/src/loom/vm/handler.go
+++ core/src/loom/vm/handler.go
@@ -2,15 +2,27 @@ package vm
 
 import (
 	"github.com/gogo/protobuf/proto"
-
 	"loom"
 	"loom/store"
+	"github.com/ethereum/go-ethereum/core/vm/runtime"
+	"github.com/ethereum/go-ethereum/common"
+	//"fmt"
+	"github.com/ethereum/go-ethereum/params"
+	"math/big"
+	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/ethereum/go-ethereum/core/state"
+	tmcommon "github.com/tendermint/tmlibs/common"
 )
+
+type evmState struct {
+	loom.State
+	evmDB state.StateDB
+}
 
 var vmPrefix = []byte("vm")
 
-func ProcessDeployTx(state loom.State, txBytes []byte) (loom.TxHandlerResult, error) {
-	var r loom.TxHandlerResult
+func ProcessSendTx(state loom.State, txBytes []byte) (loom.TxHandlerResult, error) {
+	var r loom.TxHandlerResult //Tags []common.KVPair
 
 	tx := &DeployTx{}
 	err := proto.Unmarshal(txBytes, tx)
@@ -22,5 +34,104 @@ func ProcessDeployTx(state loom.State, txBytes []byte) (loom.TxHandlerResult, er
 	vmState := store.PrefixKVStore(state, vmPrefix)
 	vmState.Set(tx.To.Local, tx.Code)
 
-	return r, nil
+	//Send create transaction to EVM
+	cfg := getConfig(state.(evmState).evmDB)
+	res, _, txErr := runtime.Call(common.BytesToAddress(tx.To.Local), tx.Code, &cfg)
+
+	kvpResult := tmcommon.KVPair{[]byte{0}, res}
+	r.Tags = append(r.Tags,kvpResult)
+	return r, txErr
+}
+
+func ProcessDeployTx(state loom.State, txBytes []byte) (loom.TxHandlerResult, error) {
+	var r loom.TxHandlerResult //Tags []common.KVPair
+
+	tx := &DeployTx{}
+	err := proto.Unmarshal(txBytes, tx)
+	if err != nil {
+		return r, err
+	}
+
+	// Store EVM byte code
+	vmState := store.PrefixKVStore(state, vmPrefix)
+	vmState.Set(tx.To.Local, tx.Code)
+
+	//Send create transaction to EVM
+	cfg := getConfig(state.(evmState).evmDB)
+	res, addr, _, txErr := runtime.Create(tx.Code, &cfg)
+
+	kvpResult := tmcommon.KVPair{[]byte{0}, res}
+	kvpAddr := tmcommon.KVPair{[]byte{1}, addr[:]}
+	r.Tags = append(r.Tags,kvpResult)
+	r.Tags = append(r.Tags,kvpAddr)
+	return r, txErr
+}
+
+func getConfig(statedb state.StateDB) (runtime.Config) {
+
+	cliqueCfg := params.CliqueConfig{
+		Period: 10, // Number of seconds between blocks to enforce
+		Epoch:  1000,  // Epoch length to reset votes and checkpoint
+	}
+
+	chainConfig := params.ChainConfig{
+		ChainId: big.NewInt(0),  // Chain id identifies the current chain and is used for replay protection
+		HomesteadBlock: nil,  // Homestead switch block (nil = no fork, 0 = already homestead)
+		DAOForkBlock:   nil,    // TheDAO hard-fork switch block (nil = no fork)
+		DAOForkSupport: true,     // Whether the nodes supports or opposes the DAO hard-fork
+		// EIP150 implements the Gas price changes (https://github.com/ethereum/EIPs/issues/150)
+		EIP150Block: nil,     // EIP150 HF block (nil = no fork)
+		EIP150Hash: common.StringToHash("myHash"),   // EIP150 HF hash (needed for header only clients as only gas pricing changed)
+		EIP155Block: big.NewInt(0), // EIP155 HF block
+		EIP158Block: big.NewInt(0),  // EIP158 HF block
+		ByzantiumBlock:      nil,      // Byzantium switch block (nil = no fork, 0 = already on byzantium)
+		ConstantinopleBlock: nil, // Constantinople switch block (nil = no fork, 0 = already activated)
+		// Various consensus engines
+		Ethash: new(params.EthashConfig),
+		Clique: &cliqueCfg,
+	}
+
+	logCfg := vm.LogConfig{
+		DisableMemory:  false, // disable memory capture
+		DisableStack:   false, // disable stack capture
+		DisableStorage: false, // disable storage capture
+		Limit:          0,  // maximum length of output, but zero means unlimited
+	}
+	logger := vm.NewStructLogger(&logCfg)
+	evmCfg :=  vm.Config{
+		// Debug enabled debugging Interpreter options
+		Debug: true,
+		// Tracer is the op code logger
+		Tracer: logger,
+		// NoRecursion disabled Interpreter call, callcode,
+		// delegate call and create.
+		NoRecursion: false,
+		// Enable recording of SHA3/keccak preimages
+		EnablePreimageRecording: true,
+		// JumpTable contains the EVM instruction table. This
+		// may be left uninitialised and will be set to the default
+		// table.
+		//JumpTable: [256]operation,
+	}
+
+	//db, _ := ethdb.NewMemDatabase()
+	//genAllocation := make(core.GenesisAlloc) //empty one is ok?????
+	//statedb := tests.MakePreState(db, genAllocation)
+
+	cfg := runtime.Config{
+		ChainConfig: &chainConfig, // passed to vm.NewEVM
+		Difficulty:  big.NewInt(20000), //context
+		Origin:      common.StringToAddress("myOrigin"),  //context
+		Coinbase:    common.StringToAddress("myCoinBase"),  //context
+		BlockNumber: big.NewInt(0),  //context
+		Time:        big.NewInt(0),  //context
+		GasLimit:    0x2fefd8,  //context
+		GasPrice:    big.NewInt(0),  //context
+		Value:       big.NewInt(0),  //unused!?
+		Debug:       true,  //unused!?
+		EVMConfig:   evmCfg, // passed to vm.NewEVM
+		State:     &statedb, // passed to vm.NewEVM
+		GetHashFn: func(uint64) common.Hash { return common.Hash{} }, //context
+	}
+	return cfg
 }
--- core/src/loom/vm/vm_test.go
+++ core/src/loom/vm/vm_test.go
@@ -10,6 +10,16 @@ import (
 
 	"loom"
 	"loom/store"
+	"io/ioutil"
+	"fmt"
+	"os"
+	"encoding/json"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/vm/runtime"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/crypto/sha3"
+	"bytes"
+	"encoding/binary"
 )
 
 func mockState() loom.State {
@@ -17,17 +27,205 @@ func mockState() loom.State {
 	return loom.NewStoreState(context.Background(), store.NewMemStore(), header)
 }
 
+// Pseudo code
+// loomToken = new LoomToken()
+// delegateCallToken = new DelegateCallToken()
+// transferGateway = new (loomToken, delegateCallToken, 0)
+// loomToken.transfer( transferGateway, 10)  -> returns true
 func TestProcessDeployTx(t *testing.T) {
-	tx := &DeployTx{
+	//Create state database
+	evmStore := evmState {
+		mockState(),
+		*new(state.StateDB),
+	}
+	evmStore.evmDB = getStatDB(t)
+
+	addrLoomToken := createLoomToken(t, evmStore)
+	addrDelegateCallToken := createDelegateCallToken(t, evmStore)
+	addrTransferGateway := createTransferGateway(t, evmStore, addrLoomToken, addrDelegateCallToken)
+	_ = callTransfer(t, evmStore, addrLoomToken, addrTransferGateway, uint64(10))
+}
+
+func getStatDB(t *testing.T) state.StateDB {
+	bytecode := common.Hex2Bytes("6060604052600a8060106000396000f360606040526008565b00")
+	_, sdb, err := runtime.Execute(bytecode, nil, nil)
+	require.Nil(t, err)
+	return *sdb
+}
+
+func createLoomToken(t *testing.T, evmStore evmState) ([]byte) {
+	var res loom.TxHandlerResult
+	loomTokenData := getContractData("./testdata/LoomToken.json")
+	loomTokenTx := &DeployTx{
 		To: &loom.Address{
 			ChainId: "mock",
-			Local:   []byte{1, 2, 3},
+			Local:   []byte{},
 		},
-		Code: []byte{4, 5, 6},
+		Code: common.Hex2Bytes(snipOx(loomTokenData.Bytecode)),
 	}
-	b, err := proto.Marshal(tx)
+	loomTokenB, err := proto.Marshal(loomTokenTx)
 	require.Nil(t, err)
 
-	_, err = ProcessDeployTx(mockState(), b)
+	res, err = ProcessDeployTx(evmStore, loomTokenB)
+
 	require.Nil(t, err)
+	result := res.Tags[0].Value
+	if !checkEqual(result, common.Hex2Bytes(snipOx(loomTokenData.DeployedBytecode))) {
+		t.Error("create loomToken did not return deployed bytecode")
+	}
+	return res.Tags[1].Value
+}
+
+func createDelegateCallToken(t *testing.T, evmStore evmState) ([]byte) {
+	var res loom.TxHandlerResult
+	delegateCallTokenData := getContractData("./testdata/DelegateCallToken.json")
+	delegateCallTokenTx := &DeployTx{
+		To: &loom.Address{
+			ChainId: "mock",
+			Local:   []byte{},
+		},
+		Code: common.Hex2Bytes(snipOx(delegateCallTokenData.Bytecode)),
+	}
+	delegateCallTokenB, err := proto.Marshal(delegateCallTokenTx)
+	require.Nil(t, err)
+
+	res, err = ProcessDeployTx(evmStore, delegateCallTokenB)
+
+	require.Nil(t, err)
+	result := res.Tags[0].Value
+	if !checkEqual(result, common.Hex2Bytes(snipOx(delegateCallTokenData.DeployedBytecode))) {
+		t.Error("create delegate call token did not return deployed bytecode")
+	}
+	return res.Tags[1].Value
+}
+
+func createTransferGateway(t *testing.T, evmStore evmState, loomAdr, delAdr []byte) ([]byte) {
+	var empty []byte
+	var res loom.TxHandlerResult
+	transferGatewayData := getContractData("./testdata/TransferGateway.json")
+	inParams := evmParamsB(common.Hex2Bytes(snipOx(transferGatewayData.Bytecode)), loomAdr, delAdr, empty)
+	transferGatewayTx := &DeployTx{
+		To: &loom.Address{
+			ChainId: "mock",
+			Local:   []byte{},
+		},
+		Code: inParams,
+	}
+	transferGatewayB, err := proto.Marshal(transferGatewayTx)
+	require.Nil(t, err)
+
+	res, err = ProcessDeployTx(evmStore, transferGatewayB)
+	require.Nil(t, err)
+	result := res.Tags[0].Value
+	if !checkEqual(result, common.Hex2Bytes(snipOx(transferGatewayData.DeployedBytecode))) {
+		t.Error("create transfer Gateway did not return deployed bytecode")
+	}
+	return res.Tags[1].Value
+}
+
+func callTransfer(t *testing.T, evmStore evmState, addr1, addr2 []byte, amount uint64) (bool) {
+	var res loom.TxHandlerResult
+	inParams := evmParams("transfer(address,uint256)", addr2,  uint64ToByte(amount))
+	transferTx := &DeployTx{
+		To: &loom.Address{
+			ChainId: "mock",
+			Local:   addr1,
+		},
+		Code: inParams,
+	}
+	transferTxB, err := proto.Marshal(transferTx)
+	require.Nil(t, err)
+
+	res, err = ProcessSendTx(evmStore, transferTxB)
+	result := res.Tags[0].Value
+	if (checkEqual(result, []byte{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1})) {
+		return true
+	} else if (checkEqual(result, []byte{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0})) {
+		return false
+	} else {
+		t.Error("calling transfer did not return boolean")
+	}
+	return false
+}
+
+func getContractData(filename string) (ContractData) {
+	raw, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	var data ContractData
+	err2 := json.Unmarshal(raw, &data)
+	if (err2 != nil) {
+		fmt.Println("Unmarshall error", err2)
+	}
+	return data
+}
+type ContractData struct {
+	ContractName    string 				`json:"contractName"`
+	Abi []interface{}					`json:"abi"`
+	Bytecode   string					`json:"bytecode"`
+	DeployedBytecode string				`json:"deployedBytecode"`
+	SourceMap string					`json:"sourceMap"`
+	DeployedSourceMap string			`json:"deployedSourceMap"`
+	Source string						`json:"source"`
+	SourcePath string					`json:"sourcePath"`
+	Ast map[string]interface{}			`json:"ast"`
+	LegacyAST map[string]interface{}	`json:"legacyAST"`
+	Compiler map[string]interface{}		`json:"compiler"`
+	Networks map[string]interface{}		`json:"networks"`
+	SchemaVersion string				`json:"schemaVersion"`
+	UpdateAI string						`json:"updatedAt"`
+}
+
+func snipOx(hexString string) (string) {
+	if hexString[0:2] == string("0x") {
+		hexString = hexString[2:]
+	}
+	return hexString
+}
+
+func checkEqual(b1, b2 []byte) bool {
+	if b1 == nil && b2 == nil {
+		return true;
+	}
+	if b1 == nil || b2 == nil {
+		return false;
+	}
+	if len(b1) != len(b2) {
+		return false
+	}
+	for i := range b1 {
+		if b1[i] != b2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func evmParams(funcName string, params ...[]byte) ([]byte){
+	funcNameToBytes := []byte(funcName)
+	d := sha3.NewKeccak256()
+	d.Write(funcNameToBytes)
+	hashedFuncName := d.Sum(nil)
+	clipedHashedFuncName := hashedFuncName[0:4]
+	return evmParamsB(clipedHashedFuncName, params...)
+}
+
+func evmParamsB(prefix []byte, params ...[]byte) ([]byte){
+	padded := prefix
+	for _, param := range params {
+		paramPadded := common.LeftPadBytes(param, 32)
+		padded = append(padded, paramPadded...)
+	}
+	return padded
+}
+
+func uint64ToByte(theInt uint64) ([]byte) {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, theInt)
+	if err != nil {
+		fmt.Println("error coverting int to bytes", err)
+	}
+	return buf.Bytes()
 }
