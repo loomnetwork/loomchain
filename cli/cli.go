578b3b78c7c611fd195925312b850d8c474ccc04
Kyle Teague <kyteague@gmail.com>
2018-04-10 21:10:46 +0700

    check plugin output

--- cli/cli.go
+++ cli/cli.go
@@ -8,7 +8,10 @@ import (
 )
 
 func Commands(backend backend.Backend, app *loom.Application) []*cobra.Command {
-	return []*cobra.Command{NewRunCommand(backend, app)}
+	return []*cobra.Command{
+		NewInitCommand(backend, app),
+		NewRunCommand(backend, app),
+	}
 }
 
 func NewRunCommand(backend backend.Backend, app *loom.Application) *cobra.Command {
@@ -17,6 +20,16 @@ func NewRunCommand(backend backend.Backend, app *loom.Application) *cobra.Comman
 		Short: "Run the blockchain node",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return backend.Run(app)
+		},
+	}
+}
+
+func NewInitCommand(backend backend.Backend, app *loom.Application) *cobra.Command {
+	return &cobra.Command{
+		Use:   "init",
+		Short: "Initialize the blockchain",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return backend.Init()
 		},
 	}
 }
