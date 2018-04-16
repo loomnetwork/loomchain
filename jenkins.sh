c60e411702400b118a72f14be3d0dde5a926f9d7
Matthew Campbell <hyper@hyperworks.nu>
2018-04-16 18:14:55 +0700

    jenkins git clone plugins

--- jenkins.sh
+++ jenkins.sh
@@ -6,12 +6,15 @@ PKG=github.com/loomnetwork/loom
 
 # setup temp GOPATH
 export GOPATH=/tmp/gopath-$BUILD_TAG
+export 
 export PATH=$GOPATH:$PATH:/var/lib/jenkins/workspace/commongopath/bin
 
 LOOM_SRC=$GOPATH/src/$PKG
 mkdir -p $LOOM_SRC
 rsync -r --delete . $LOOM_SRC
 
+cd $GOPATH/src/github.com/loomnetwork
+git clone github.com/loomnetwork/loom-plugin
 
 cd $LOOM_SRC
 dep ensure -vendor-only
