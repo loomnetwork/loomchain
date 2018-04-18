2c4ef7e8764547168aa1263ae9ee41a17aa597a1
sriep <piers.shepperson@btinternet.com>
2018-04-18 19:21:06 +0100

    Added Commit to Evm and LoomEvm to save changes to ethdb. Added LoomVm to save ethereum data after each transaction.

--- vm/vm.loomevm.go
+++ vm/vm.loomevm.go
@@ -23,9 +23,9 @@ type LoomEvm struct {
 func NewLoomEvm(loomState loom.State) *LoomEvm {
 	p := new(LoomEvm)
 	p.db = NewLoomEthdb(loomState)
-	oldRoot := loomState.Get(rootKey)
+	oldRoot, _ := p.db.Get(rootKey)
 	_state, _ := state.New(common.BytesToHash(oldRoot), state.NewDatabase(p.db))
-	p.evm = *NewEvmFrom(_state)
+	p.evm = *NewEvmFrom(*_state)
 	return p
 }
 
@@ -39,4 +39,53 @@ func (levm LoomEvm) Call(caller, addr loom.Address, input []byte) ([]byte, error
 
 func (levm LoomEvm) StaticCall(caller, addr loom.Address, input []byte) ([]byte, error) {
 	return levm.evm.StaticCall(caller, addr, input)
+}
+
+func(levm LoomEvm) Commit() (common.Hash, error)  {
+	root, err := levm.evm.Commit()
+	if (err == nil) {
+		levm.db.Put(rootKey,  root[:])
+	}
+	return root, err
+}
+
+var LoomVmFactory = func(state loom.State) VM {
+	return *NewLoomVm(state)
+}
+
+type LoomVm struct {
+	state loom.State
+}
+
+func NewLoomVm(loomState loom.State) *LoomVm {
+	p := new(LoomVm)
+	p.state = loomState
+	return p
+}
+
+func (lvm LoomVm) Create(caller loom.Address, code []byte) ([]byte, loom.Address, error) {
+	levm := NewLoomEvm(lvm.state)
+	ret, addr, err := levm.evm.Create(caller, code)
+	if err == nil {
+		_, err = levm.Commit()
+	}
+	return ret, addr, err
+}
+
+func (lvm LoomVm) Call(caller, addr loom.Address, input []byte) ([]byte, error) {
+	levm := NewLoomEvm(lvm.state)
+	ret, err := levm.evm.Call(caller, addr, input)
+	if err == nil {
+		_, err = levm.Commit()
+	}
+	return ret, err
+}
+
+func (lvm LoomVm) StaticCall(caller, addr loom.Address, input []byte) ([]byte, error) {
+	levm := NewLoomEvm(lvm.state)
+	ret, err :=  levm.evm.StaticCall(caller, addr, input)
+	if err == nil {
+		_, err = levm.Commit()
+	}
+	return ret, err
 }
