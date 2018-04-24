7ebb4ebd6e1326e0f682d9e4ca8dba5993d1a486
Kyle Teague <kyteague@gmail.com>
2018-04-24 17:34:50 +0700

    fix tests

--- vm/test_loomtoken.go
+++ vm/test_loomtoken.go
@@ -6,8 +6,9 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/loomnetwork/loom"
 	"github.com/stretchr/testify/require"
+
+	lp "github.com/loomnetwork/loom-plugin"
 )
 
 // Pseudo code
@@ -15,7 +16,7 @@ import (
 // delegateCallToken = new DelegateCallToken()
 // transferGateway = new (loomToken, delegateCallToken, 0)
 // loomToken.transfer( transferGateway, 10)  -> returns true
-func testLoomTokens(t *testing.T, vm VM, caller loom.Address) {
+func testLoomTokens(t *testing.T, vm VM, caller lp.Address) {
 	loomData := getContractData("./testdata/LoomToken.json")
 	delegateCallData := getContractData("./testdata/DelegateCallToken.json")
 
@@ -25,7 +26,7 @@ func testLoomTokens(t *testing.T, vm VM, caller loom.Address) {
 	_ = callTransfer(t, vm, caller, addrLoomToken, addrTransferGateway, uint64(10))
 }
 
-func createTransferGateway(t *testing.T, vm VM, caller, loomAdr, delAdr loom.Address) loom.Address {
+func createTransferGateway(t *testing.T, vm VM, caller, loomAdr, delAdr lp.Address) lp.Address {
 	var empty []byte
 	transferGatewayData := getContractData("./testdata/TransferGateway.json")
 	inParams := evmParamsB(common.Hex2Bytes(snipOx(transferGatewayData.Bytecode)), loomAdr.Local, delAdr.Local, empty)
@@ -39,7 +40,7 @@ func createTransferGateway(t *testing.T, vm VM, caller, loomAdr, delAdr loom.Add
 	return addr
 }
 
-func callTransfer(t *testing.T, vm VM, caller, contractAddr, addr2 loom.Address, amount uint64) bool {
+func callTransfer(t *testing.T, vm VM, caller, contractAddr, addr2 lp.Address, amount uint64) bool {
 	inParams := evmParams("transfer(address,uint256)", addr2.Local, uint64ToByte(amount))
 
 	res, err := vm.Call(caller, contractAddr, inParams)
