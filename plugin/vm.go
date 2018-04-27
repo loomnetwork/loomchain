1180b15fe9ace90d475199a12bb2ea561870c6f0
aditya <aditya@oogway.in>
2018-04-27 23:07:03 +0700

    send events from plugin

--- plugin/vm.go
+++ plugin/vm.go
@@ -3,6 +3,7 @@ package plugin
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/json"
 	"errors"
 	"time"
 
@@ -15,6 +16,7 @@ import (
 	"github.com/loomnetwork/loom-plugin/types"
 	"github.com/loomnetwork/loom-plugin/util"
 	"github.com/loomnetwork/loom/auth"
+	"github.com/loomnetwork/loom/log"
 	"github.com/loomnetwork/loom/vm"
 )
 
@@ -37,8 +39,9 @@ func dataPrefix(addr cmn.Address) []byte {
 }
 
 type PluginVM struct {
-	Loader Loader
-	State  loom.State
+	Loader       Loader
+	State        loom.State
+	EventHandler loom.EventHandler
 }
 
 var _ vm.VM = &PluginVM{}
@@ -62,10 +65,12 @@ func (vm *PluginVM) run(
 	}
 
 	contractCtx := &contractContext{
-		caller:  caller,
-		address: addr,
-		State:   loom.StateWithPrefix(dataPrefix(addr), vm.State),
-		VM:      vm,
+		caller:       caller,
+		address:      addr,
+		State:        loom.StateWithPrefix(dataPrefix(addr), vm.State),
+		VM:           vm,
+		eventHandler: vm.EventHandler,
+		readOnly:     readOnly,
 	}
 
 	isInit := len(input) == 0
@@ -148,6 +153,8 @@ type contractContext struct {
 	address cmn.Address
 	loom.State
 	vm.VM
+	eventHandler loom.EventHandler
+	readOnly     bool
 }
 
 var _ lp.Context = &contractContext{}
@@ -174,6 +181,24 @@ func (c *contractContext) Now() time.Time {
 	return time.Unix(c.State.Block().Time, 0)
 }
 
-func (c *contractContext) Emit(event []byte) {
+type emitData struct {
+	Caller  cmn.Address
+	Address cmn.Address
+	Data    []byte
+}
 
+func (c *contractContext) Emit(event []byte) {
+	if c.readOnly {
+		return
+	}
+	data := &emitData{
+		Caller:  c.caller,
+		Address: c.address,
+		Data:    event,
+	}
+	emitMsg, err := json.Marshal(data)
+	if err != nil {
+		log.Root.Info("Error in event marshalling for event: %s", string(event))
+	}
+	c.eventHandler.Post(c.State, emitMsg)
 }
