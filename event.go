3b17121dd45d023c7df39d013b31cfe3854feefb
aditya <aditya@oogway.in>
2018-04-27 12:05:12 +0700

    stash and fire events post commit

--- event.go
+++ event.go
@@ -2,13 +2,18 @@ package loom
 
 import (
 	"encoding/json"
+	"log"
 )
 
 type EventHandler interface {
 	PostCommit(state State, txBytes []byte, res TxHandlerResult) error
+	EmitBlockTx(height int64) error
 }
 
 type EventDispatcher interface {
+	Stash(index int64, msg []byte) error
+	FetchStash(index int64) ([][]byte, error)
+	PurgeStash(index int64) error
 	Send(index int64, msg []byte) error
 }
 
@@ -33,5 +38,22 @@ func (ed *DefaultEventHandler) PostCommit(state State, txBytes []byte, res TxHan
 	if err != nil {
 		return err
 	}
-	return ed.dispatcher.Send(height, msg)
+	return ed.dispatcher.Stash(height, msg)
+}
+
+func (ed *DefaultEventHandler) EmitBlockTx(height int64) error {
+	msgs, err := ed.dispatcher.FetchStash(height)
+	if err != nil {
+		return err
+	}
+	for _, msg := range msgs {
+		if err := ed.dispatcher.Send(height, msg); err != nil {
+			log.Printf("Error sending event: height: %d; msg: %+v\n", height, msg)
+		}
+	}
+	if err := ed.dispatcher.PurgeStash(height); err != nil {
+		log.Printf("Error purging stash for height %d: %+v", height, err)
+		return err
+	}
+	return nil
 }
