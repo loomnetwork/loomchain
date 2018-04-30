735fb2a9f372fda7f4679ebe3bc2e4d2936e8d12
Ady Romantika <ady.romantika@mbww.com>
2018-04-30 20:59:41 +0800

    Add host key

--- jenkins.cmd
+++ jenkins.cmd
@@ -1,2 +1,2 @@
 @ECHO OFF
-runas /user:kevin "C:\msys64\usr\bin\bash.exe -l -c 'id && cd /c/jenkins/workspace/loom-sdk-pipeline-test && ./jenkins.sh'"
+C:\msys64\usr\bin\bash.exe -l -c "ssh -o StrictHostKeyChecking=false github.com && cd /c/jenkins/workspace/loom-sdk-pipeline-test && ./jenkins.sh"
