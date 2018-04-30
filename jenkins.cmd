d2e94cd376caef539f7b077196d4ca4f2746ae11
Ady Romantika <ady.romantika@mbww.com>
2018-04-30 20:49:20 +0800

    Add quotes

--- jenkins.cmd
+++ jenkins.cmd
@@ -1,2 +1,2 @@
 @ECHO OFF
-runas /user:kevin C:\msys64\usr\bin\bash.exe -l -c "id && cd /c/jenkins/workspace/loom-sdk-pipeline-test && ./jenkins.sh"
+runas /user:kevin "C:\msys64\usr\bin\bash.exe -l -c 'id && cd /c/jenkins/workspace/loom-sdk-pipeline-test && ./jenkins.sh'"
