379fffb95183992177a6038427ce5483514bdd52
Ady Romantika <ady.romantika@mbww.com>
2018-04-30 21:04:49 +0800

    Disable customizations

--- jenkins.cmd
+++ jenkins.cmd
@@ -1,2 +1,2 @@
 @ECHO OFF
-C:\msys64\usr\bin\bash.exe -l -c "ssh -o StrictHostKeyChecking=false github.com && cd /c/jenkins/workspace/loom-sdk-pipeline-test && ./jenkins.sh"
+C:\msys64\usr\bin\bash.exe -l -c "cd /c/jenkins/workspace/loom-sdk-pipeline-test && ./jenkins.sh"
