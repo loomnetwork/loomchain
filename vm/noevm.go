02452d47dc3e2c8d1cc77779cc8386f64d34c109
sriep <piers.shepperson@btinternet.com>
2018-05-02 16:11:09 +0100

    Add NewLoomVm to non evm build.

--- vm/noevm.go
+++ vm/noevm.go
@@ -9,3 +9,5 @@ import (
 var LoomEvmFactory func(state loomchain.State) VM
 var EvmFactory func(state loomchain.State) VM
 var LoomVmFactory func(state loomchain.State) VM
+
+func NewLoomVm(state loomchain.State, eventHandler loomchain.EventHandler) *VM { return nil }
