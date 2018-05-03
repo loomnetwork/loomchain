080b12c247198218aa0aa4806e121cbeb3fe8446
sriep <piers.shepperson@btinternet.com>
2018-05-03 14:02:25 +0100

    Add test for ethereum event handling

--- vm/testdata/TestEvent.sol
+++ vm/testdata/TestEvent.sol
@@ -1,15 +1,9 @@
 pragma solidity ^0.4.18;
 contract TestEvent {
-    event MyEvent(uint number, uint value);
-    event TestEvent2(address indexed from, uint value);
-    event Transfer(string indexed from, address indexed to, uint value);
-    function sendEvent(uint i, uint value) public   {
-        MyEvent(i, value);
-    }
-    function sendEvents(uint number, uint value, string from) public {
-        MyEvent(number, value);
-        TestEvent2(msg.sender, value);
-        Transfer(from, msg.sender, value);
+    event MyEvent(uint number);
+
+    function sendEvent(uint i) public   {
+        MyEvent(i);
     }
 
 }
