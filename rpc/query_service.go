00aaa2a873cfdba032dbb13cf1b77b5f1503b2c6
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2018-05-09 18:07:37 +0700

    add basic metrics on query server; change query service as an interface

---
+++ rpc/query_service.go
@@ -0,0 +1,45 @@
+package rpc
+
+import (
+	"net/http"
+
+	"github.com/loomnetwork/loomchain/log"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	amino "github.com/tendermint/go-amino"
+	rpcserver "github.com/tendermint/tendermint/rpc/lib/server"
+)
+
+// QueryService provides neccesary methods for the client to query appication states
+type QueryService interface {
+	Query(contract string, query []byte) ([]byte, error)
+	Nonce(key string) (uint64, error)
+}
+
+// MakeQueryServiceHandler returns a http handler mapping to query service
+func MakeQueryServiceHandler(svc QueryService, logger log.TMLogger) http.Handler {
+	// set up websocket route
+	codec := amino.NewCodec()
+	wsmux := http.NewServeMux()
+	routes := map[string]*rpcserver.RPCFunc{}
+	routes["query"] = rpcserver.NewRPCFunc(svc.Query, "contract,query")
+	routes["nonce"] = rpcserver.NewRPCFunc(svc.Nonce, "key")
+	rpcserver.RegisterRPCFuncs(wsmux, routes, codec, logger)
+	wm := rpcserver.NewWebsocketManager(routes, codec)
+	wsmux.HandleFunc("/queryws", wm.WebsocketHandler)
+
+	// setup default route
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if req.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		wsmux.ServeHTTP(w, req)
+	})
+
+	// setup metrics route
+	mux.Handle("/metrics", promhttp.Handler())
+
+	return mux
+}
