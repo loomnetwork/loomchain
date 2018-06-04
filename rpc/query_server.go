7783460fdc637fc85191054277b3942dbc3e4b30
sriep <piers.shepperson@btinternet.com>
2018-06-04 14:05:36 +0100

    Handle Query caller paramter.

--- rpc/query_server.go
+++ rpc/query_server.go
@@ -91,13 +91,11 @@ var _ QueryService = &QueryServer{}
 // Query returns data of given contract from the application states
 // The contract parameter should be a hex-encoded local address prefixed by 0x
 func (s *QueryServer) Query(caller, contract string, query []byte, vmType vm.VMType) ([]byte, error) {
-	localCallerAddr, err := decodeHexAddress(contract)
+	callerAddr, err := loom.ParseAddress(caller)
 	if err != nil {
-		return nil, err
-	}
-	callerAddr := loom.Address{
-		ChainID: s.ChainID,
-		Local:   localCallerAddr,
+		callerAddr = loom.Address{
+			ChainID: s.ChainID,
+		}
 	}
 
 	localContractAddr, err := decodeHexAddress(contract)
