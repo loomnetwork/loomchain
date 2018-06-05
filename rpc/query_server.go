a17aa56626a3434b0b88bb4be9215f78b58fe6b6
sriep <piers.shepperson@btinternet.com>
2018-06-05 14:38:17 +0100

    Handle empty caller address.

--- rpc/query_server.go
+++ rpc/query_server.go
@@ -151,6 +151,10 @@ func (s *QueryServer) QueryEvm(caller, contract loom.Address, query []byte) ([]b
 	return vm.StaticCall(caller, contract, query)
 }
 
+// GetCode returns the runtime byte-code of a contract running on a DAppChain's EVM.
+// Gives an error for non-EVM contracts.
+// contract - address of the contract in the form of a string. (Use loom.Address.String() to convert)
+// return []byte - runtime bytecode of the contract.
 func (s *QueryServer) GetCode(contract string) ([]byte, error) {
 	contractAddr, err := loom.ParseAddress(contract)
 	if err != nil {
