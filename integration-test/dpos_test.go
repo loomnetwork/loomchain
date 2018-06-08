f6e0856fc7c1bb8d0aaaca0aaa4763a64f9cbe22
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2018-06-08 18:06:04 +0700

    Add contracts and validator test as part of go test

---
+++ integration-test/dpos_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestContractDPOS(t *testing.T) {
+	tests := []struct {
+		testFile string
+	}{
+		{"dpos.toml"},
+	}
+
+	for _, test := range tests {
+		config, err := newConfig("dpos", test.testFile, "")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		binary, err := exec.LookPath("go")
+		if err != nil {
+			t.Fatal(err)
+		}
+		// required binary
+		cmd := exec.Cmd{
+			Dir:  config.BaseDir,
+			Path: binary,
+			Args: []string{binary, "build", "-o", "example-cli", "github.com/loomnetwork/go-loom/examples/cli"},
+		}
+		if err := cmd.Run(); err != nil {
+			t.Fatal(fmt.Errorf("fail to execute command: %s\n%v", strings.Join(cmd.Args, " "), err))
+		}
+
+		doRun(t, *config)
+	}
+}
