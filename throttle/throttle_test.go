599e60f3952e8093c1056da7a984be2cafc4e8fb
extrodox <myhemen@gmail.com>
2018-06-08 17:37:22 +0530

    throttle test suit

---
+++ throttle/throttle_test.go
@@ -0,0 +1,83 @@
+package throttle
+
+import (
+	"context"
+	"testing"
+	"github.com/gogo/protobuf/proto"
+	abci "github.com/tendermint/abci/types"
+	"golang.org/x/crypto/ed25519"
+	loomAuth "github.com/loomnetwork/loomchain/auth"
+	"github.com/loomnetwork/go-loom/auth"
+	"github.com/loomnetwork/loomchain"
+	"github.com/loomnetwork/loomchain/store"
+	"github.com/stretchr/testify/require"
+	"fmt"
+	"github.com/loomnetwork/go-loom"
+)
+
+
+func throttleMiddlewareHandler(t *testing.T, th *Throttle, i int16, state loomchain.State, tx auth.SignedTx, ctx context.Context) {
+	defer func() {
+		if rval := recover(); rval != nil {
+			require.Equal(t, rval, fmt.Sprintf("Out of access count for current session: %d out of %d, Try after sometime!", i, 100))
+			t.Log( rval)
+		}
+	}()
+
+	ThrottleTxMiddleware.ProcessTx(state.WithContext(ctx), tx.Inner,
+		func(state loomchain.State, txBytes []byte) (res loomchain.TxHandlerResult, err error) {
+
+			origin := loomAuth.Origin(state.Context())
+			require.False(t,  origin.IsEmpty())
+			if i <= th.maxAccessCount {
+				require.Nil(t, err)
+				require.Equal(t, th.getAccessCount(), i)
+			}
+
+			return loomchain.TxHandlerResult{}, nil
+		},
+	)
+}
+
+func TestThrottleTxMiddleware(t *testing.T) {
+
+
+	origBytes := []byte("origin")
+	_, privKey, err := ed25519.GenerateKey(nil)
+	require.Nil(t, err)
+
+	signer := auth.NewEd25519Signer([]byte(privKey))
+	signedTx := auth.SignTx(signer, origBytes)
+	signedTxBytes, err := proto.Marshal(signedTx)
+	state := loomchain.NewStoreState(nil, store.NewMemStore(), abci.Header{})
+
+	var tx auth.SignedTx
+	err = proto.Unmarshal(signedTxBytes, &tx)
+	require.Nil(t, err)
+
+
+
+
+	require.Equal(t, len(tx.PublicKey), ed25519.PublicKeySize)
+
+	require.Equal(t,  len(tx.Signature), ed25519.SignatureSize)
+
+	require.True(t,  ed25519.Verify(tx.PublicKey, tx.Inner, tx.Signature))
+
+	origin := loom.Address{
+		ChainID: state.Block().ChainID,
+		Local:   loom.LocalAddressFromPublicKey(tx.PublicKey),
+	}
+
+	th := GetThrottle(origin)
+
+	ctx := context.WithValue(state.Context(), loomAuth.ContextKeyOrigin, origin)
+
+	i := int16(1)
+	for i <= th.maxAccessCount*2 {
+		throttleMiddlewareHandler(t,  th, i , state , tx , ctx )
+		i += 1
+	}
+
+
+}
