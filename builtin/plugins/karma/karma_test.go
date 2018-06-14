cd37396eb96d216ce63d86117dbefca0e7e5a310
extrodox <myhemen@gmail.com>
2018-06-14 06:05:16 +0530

    karma test for get state

--- builtin/plugins/karma/karma_test.go
+++ builtin/plugins/karma/karma_test.go
@@ -1,252 +1,49 @@
 package karma
 
 import (
-	"github.com/loomnetwork/go-loom"
+	"encoding/hex"
 	"testing"
+	"github.com/loomnetwork/go-loom"
 	"github.com/loomnetwork/go-loom/plugin/contractpb"
 	"github.com/loomnetwork/go-loom/plugin"
-	ktypes "github.com/loomnetwork/loomchain/builtin/plugins/karma/types"
 	"github.com/stretchr/testify/require"
+	"github.com/loomnetwork/loomchain/builtin/plugins/karma/types"
 )
 
 var (
-	addr1 = loom.MustParseAddress("chain:0xb16a379ec18d4093666f8f38b11a3071c920207d")
-	addr2 = loom.MustParseAddress("chain:0x5cecd1f7261e1f4c684e297be3edf03b825e01c4")
-	addr3 = loom.MustParseAddress("chain:0x5cecd1f7261e1f4c684e297be3edf03b825e01c4")
-	addr4 = loom.MustParseAddress("chain:0x5cecd1f7261e1f4c684e297be3edf03b825e01c5")
-
-	maxKarma float64	= 10000
-	oraclePublicAddress = addr1.Local.String()
-	oraclePublicAddress2= addr2.Local.String()
-	oraclePublicAddress3= addr3.Local.String()
-	userPublicAddress 	= addr4.Local.String()
-
-	sources 			= map[string]float64{
-								"sms": 10.0,
-								"oauth": 10.0,
-								"token": 5.0,
-							}
-	sourceStates 		= map[string]float64{
-								"sms": 1.0,
-								"oauth": 5.0,
-								"token": 10.0,
-							}
-	extremeSourceStates = map[string]float64{
-								"sms": 1000.0,
-								"oauth": 5000.0,
-								"token": 10.0,
-							}
-	deleteSourceKeys	= []string{"sms", "oauth"}
+	valPubKeyHex1 = "3866f776276246e4f9998aa90632931d89b0d3a5930e804e02299533f55b39e1"
 )
 
-
-
-func TestKarma_Init(t *testing.T) {
-
+func TestKarmaGetState(t *testing.T) {
+	smsKarma := int64(10)
+	oathKarma := int64(10)
+	tokeKarma := int64(1)
+	c := &Karma{}
+	pubKey, _ := hex.DecodeString(valPubKeyHex1)
+	addr := loom.Address{
+		Local: loom.LocalAddressFromPublicKey(pubKey),
+	}
 	ctx := contractpb.WrapPluginContext(
-		plugin.CreateFakeContext(addr1, addr1),
+		plugin.CreateFakeContext(addr, addr),
 	)
 
-	contract := &Karma{}
+	config := &Config{
+		SmsKarma:           smsKarma,
+		OauthKarma:        	oathKarma,
+		TokenKarma: 		tokeKarma,
+		LastUpdateTime: 	ctx.Now().Unix(),
+	}
 
-	err := contract.Init(ctx, &ktypes.KarmaInitRequest{
-		Params: &Params{
-			MaxKarma: maxKarma,
-			OraclePublicAddress: oraclePublicAddress,
-			Sources: sources,
-		},
-	})
-	require.Nil(t, err)
+	ctx.Set(configKey, config)
 
-	config, err := contract.GetConfig(ctx, &ktypes.KarmaUser{})
+	params := &karma.KarmaOwner{
+		Owner: "Owner",
+	}
+	resp, err := c.GetConfig(ctx, params)
+
 	require.Nil(t, err)
-	require.Equal(t, maxKarma, config.MaxKarma)
-	require.Equal(t, oraclePublicAddress, config.Oracle.Address)
-	require.Equal(t, sources, config.Sources)
+	require.Equal(t, smsKarma, resp.SmsKarma)
+	require.Equal(t, oathKarma, resp.OauthKarma)
+	require.Equal(t, tokeKarma, resp.TokenKarma)
 
 }
-
-func TestKarma_validateOracle(t *testing.T) {
-
-	ctx := contractpb.WrapPluginContext(
-		plugin.CreateFakeContext(addr1, addr1),
-	)
-
-	contract := &Karma{}
-
-	err := contract.Init(ctx, &ktypes.KarmaInitRequest{
-		Params: &Params{
-			MaxKarma: maxKarma,
-			OraclePublicAddress: oraclePublicAddress,
-			Sources: sources,
-		},
-	})
-	require.Nil(t, err)
-
-	err = contract.validateOracle(ctx, &ktypes.KarmaUser{
-		Address:oraclePublicAddress,
-	})
-	require.Nil(t, err)
-
-	err = contract.validateOracle(ctx, &ktypes.KarmaUser{
-		Address:userPublicAddress,
-	})
-	require.NotNil(t, err)
-
-}
-
-func TestKarma_LifeCycleTest(t *testing.T) {
-
-	ctx := contractpb.WrapPluginContext(
-		plugin.CreateFakeContext(addr1, addr1),
-	)
-	contract := &Karma{}
-
-	//Init Test
-	err := contract.Init(ctx, &ktypes.KarmaInitRequest{
-		Params: &Params{
-			MaxKarma: maxKarma,
-			OraclePublicAddress: oraclePublicAddress,
-			Sources: sources,
-		},
-	})
-	require.Nil(t, err)
-
-	//GetState Test
-	ko := &ktypes.KarmaUser{
-		Address:userPublicAddress,
-	}
-
-	oracle := &ktypes.KarmaUser{
-		Address:oraclePublicAddress,
-	}
-
-	err = contract.AddNewSourcesForUser(ctx, &ktypes.KarmaStateUser{
-		User:ko,
-		Oracle:oracle,
-		SourceStates: sourceStates,
-
-	})
-	require.Nil(t, err)
-
-	//GetState Test
-	state, err := contract.GetState(ctx, ko)
-	require.Nil(t, err)
-	require.Equal(t, sourceStates, state.SourceStates)
-
-	//GetTotal Test
-	karmaTotal, err := contract.GetTotal(ctx, ko)
-	require.Nil(t, err)
-	require.Equal(t, float64(110), karmaTotal.Count)
-
-
-	//UpdateSourcesForUser Test
-	err = contract.UpdateSourcesForUser(ctx, &ktypes.KarmaStateUser{
-		User:ko,
-		Oracle:oracle,
-		SourceStates: extremeSourceStates,
-
-	})
-	require.Nil(t, err)
-
-	//GetState after UpdateSourcesForUser Test to test the change
-	state, err = contract.GetState(ctx, ko)
-	require.Nil(t, err)
-	require.Equal(t, extremeSourceStates, state.SourceStates)
-
-	//GetState after UpdateSourcesForUser and also MaxKarma Test to test the change
-	karmaTotal, err = contract.GetTotal(ctx, ko)
-	require.Nil(t, err)
-	require.Equal(t, float64(10000), karmaTotal.Count)
-
-	//DeleteSourcesForUser Test
-	err = contract.DeleteSourcesForUser(ctx, &ktypes.KarmaStateKeyUser{
-		User:ko,
-		Oracle:oracle,
-		StateKeys: deleteSourceKeys,
-
-	})
-	require.Nil(t, err)
-
-	//GetState after DeleteSourcesForUser Test
-	state, err = contract.GetState(ctx, ko)
-	require.Nil(t, err)
-	require.Equal(t, map[string]float64{"token": 10.0}, state.SourceStates)
-
-	//GetTotal after DeleteSourcesForUser Test to test the change
-	karmaTotal, err = contract.GetTotal(ctx, ko)
-	require.Nil(t, err)
-	require.Equal(t, float64(50), karmaTotal.Count)
-
-
-	//Update entire config anf change oracle
-	err = contract.UpdateConfig(ctx, &ktypes.KarmaParamsOracle{
-		Params: &ktypes.KarmaParams{
-			MaxKarma: maxKarma,
-			OraclePublicAddress: oraclePublicAddress2,
-			Sources: sources,
-		},
-		Oracle:oracle,
-	})
-	require.Nil(t, err)
-
-	//Testing old oracle to be disabled
-	err = contract.UpdateSourcesForUser(ctx, &ktypes.KarmaStateUser{
-		User:ko,
-		Oracle:oracle,
-		SourceStates: sourceStates,
-	})
-	require.NotNil(t, err)
-
-	//Testing new oracle
-	oracle2 := &ktypes.KarmaUser{
-		Address:oraclePublicAddress2,
-	}
-	err = contract.UpdateSourcesForUser(ctx, &ktypes.KarmaStateUser{
-		User:ko,
-		Oracle:oracle2,
-		SourceStates: sourceStates,
-	})
-	require.Nil(t, err)
-
-	state, err = contract.GetState(ctx, ko)
-	require.Nil(t, err)
-	require.Equal(t, sourceStates, state.SourceStates)
-
-	karmaTotal, err = contract.GetTotal(ctx, ko)
-	require.Nil(t, err)
-	require.Equal(t, float64(110), karmaTotal.Count)
-
-
-	//Update config in parts for max karma
-	err = contract.UpdateConfigMaxKarma(ctx, &ktypes.KarmaParamsOracleNewMaxKarma{
-		MaxKarma:10,
-		Oracle:oracle2,
-	})
-	require.Nil(t, err)
-
-	karmaTotal, err = contract.GetTotal(ctx, ko)
-	require.Nil(t, err)
-	require.Equal(t, float64(10), karmaTotal.Count)
-
-	//Update config in parts for new oracle
-	oracle3 := &ktypes.KarmaUser{
-		Address:oraclePublicAddress3,
-	}
-	err = contract.UpdateConfigOracle(ctx, &ktypes.KarmaParamsOracleNewOracle{
-		NewOraclePublicAddress:oraclePublicAddress3,
-		Oracle:oracle2,
-	})
-	require.Nil(t, err)
-
-	err = contract.UpdateConfigMaxKarma(ctx, &ktypes.KarmaParamsOracleNewMaxKarma{
-		MaxKarma:12,
-		Oracle:oracle3,
-	})
-	require.Nil(t, err)
-
-	karmaTotal, err = contract.GetTotal(ctx, ko)
-	require.Nil(t, err)
-	require.Equal(t, float64(12), karmaTotal.Count)
-}
-
