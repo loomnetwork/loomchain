7370591a49e8ae2f8f811e961c940376e0fb2eac
extrodox <myhemen@gmail.com>
2018-06-15 04:56:00 +0530

    Fully configurable karma system

--- cmd/loom/config.go
+++ cmd/loom/config.go
@@ -166,9 +166,13 @@ func marshalInit(pb proto.Message) (json.RawMessage, error) {
 func defaultGenesis(validator *loom.Validator) (*genesis, error) {
 	karmaInit, err := marshalInit(&karma.InitRequest{
 		Params: &karma.Params{
-			SmsKarma:	10,
-			OauthKarma: 10,
-			TokenKarma:  1,
+			MaxKarma: 10000,
+			OraclePublicAddress: "irn38gFRpNOzoySXECh5JZVoPm1Hw6UAqCdeqv4IQlM=", // change to real oracle key
+			Sources: map[string]float64{
+				"sms": 1.0, //default sources and values
+				"oauth": 3.0, //default sources and values
+				"token": 5.0, //default sources and values
+			},
 		},
 	})
 	dposInit, err := marshalInit(&dpos.InitRequest{
