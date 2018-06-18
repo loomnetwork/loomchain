66917649b910a871f3167708ca39b62f9bfa71be
sriep <piers.shepperson@btinternet.com>
2018-06-18 10:52:15 +0100

    Fix !evm build

--- eth/subs/noevm.go
+++ eth/subs/noevm.go
@@ -9,8 +9,16 @@ import (
 type EthSubscriptions struct {
 }
 
-func (s *EthSubscriptions) Add(filter string) (string, error) {
+func (s EthSubscriptions) AddLogPoll(filter string, height uint64) (string, error) {
 	return "", nil
+}
+
+func (s EthSubscriptions) AddBlockPoll(height uint64) string {
+	return ""
+}
+
+func (s EthSubscriptions) AddTxPoll(height uint64) string {
+	return ""
 }
 
 func (s *EthSubscriptions) Poll(state loomchain.ReadOnlyState, id string) ([]byte, error) {
