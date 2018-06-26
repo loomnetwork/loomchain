2a38e38eae0fc533267d687463cd6d8b42aa93b2
sriep <piers.shepperson@btinternet.com>
2018-06-26 14:58:21 +0100

    Point to loomchain phonkee/go-pusub. Change name to ResetHub.

--- rpc/query_server.go
+++ rpc/query_server.go
@@ -360,6 +360,7 @@ func (s *QueryServer) GetEvmBlockByHash(hash []byte, full bool) ([]byte, error) 
 	return query.GetBlockByHash(state, hash, full)
 }
 
+// https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_gettransactionbyhash
 func (s QueryServer) GetEvmTransactionByHash(hash []byte) (resp []byte, err error) {
 	state := s.StateProvider.ReadOnlyState()
 	return query.GetTxByHash(state, hash)
