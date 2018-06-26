aeb08587a09df6b81a2c2101ddc6dc6393843c47
sriep <piers.shepperson@btinternet.com>
2018-06-26 19:35:03 +0100

    Change EmitTxEvent. Add defer block to catch panics from go-pubsub.Publish Change GetEvmTransactionByHash parmeter

--- evm/testdata/CallPrecompiles.sol
+++ evm/testdata/CallPrecompiles.sol
@@ -9,11 +9,12 @@ contract CallPrecompiles {
 
     function callPFAssembly(uint32 _addr, bytes input) public {
         address addr = _addr;
-        //uint256[4] memory input;
-        bool rtv;
+        bool success;
         bytes memory callResult;
+        byte[4] memory _input;
+        _input[0] = input[0];
         assembly{
-            rtv := call(0, addr, 0, input, 0xFFFF, callResult, 0xFFFF)
+            success := call(0, addr, 0, _input, 0x80, callResult, 0x40)
         }
         pcfResult = callResult;
     }
