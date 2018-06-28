d9db36d4954889af47b5ac5ea97d228048ad74f0
sriep <piers.shepperson@btinternet.com>
2018-06-28 11:32:45 +0100

    Add to unit test

--- evm/testdata/CallPrecompiles.sol
+++ evm/testdata/CallPrecompiles.sol
@@ -5,24 +5,25 @@ contract CallPrecompiles {
         return addr.call(_input);
     }
 
-    function callPFAssembly(uint64 _addr, bytes _input, uint64 _outLength) public view returns (byte res) {
+    function callPFAssembly(uint64 _addr, bytes _input, uint64 _outLength) public view returns (uint256[20] p) {
         address addr = _addr;
         uint256 inSize = _input.length * 4 + 1;
         uint256 outSize = _outLength * 0x20;
+        bytes memory res = new bytes(_outLength);
         assembly{
             let start := add(_input, 0x04)
+            let resData := add(res, 0x20)
             if iszero(call(
-                5000,
-                addr,
-                0,
-                start,
-                inSize,
-                0x40,
-                outSize
+            5000,
+            addr,
+            0,
+            start,
+            inSize,
+            p,
+            outSize
             )) {
                 revert(0,0)
             }
-            res := mload(0x40)
             mstore(0x40, add(0x40, outSize))
         }
     }
