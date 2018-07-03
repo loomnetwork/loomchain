4e8c45c253cc1c9432d46232f65230de3c633b8e
Matthew Campbell <hyper@hyperworks.nu>
2018-07-03 21:21:51 +0700

    update and move plasma_cash oracle into this repo

--- builtin/plugins/plasma_cash/cmd/oracle/main.go
+++ builtin/plugins/plasma_cash/cmd/oracle/main.go
@@ -9,17 +9,21 @@ import (
 
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/loomnetwork/go-loom/auth"
-	"github.com/loomnetwork/go-loom/client"
+	"github.com/loomnetwork/go-loom/client/plasma_cash/eth"
 	"github.com/loomnetwork/loomchain/builtin/plugins/plasma_cash/oracle"
 	"golang.org/x/crypto/ed25519"
 )
 
 func main() {
+	//TODO read environment variables or command line to get this data
+	rootChainHexAddress := "0x00000000"
+	privateKeyHex := "0x000000"
+
 	_, loomPrivKey, err := ed25519.GenerateKey(nil)
 	if err != nil {
 		log.Fatalf("failed to gnerate private key for authority: %v", err)
 	}
-	ethPrivKeyHexStr := client.GetTestAccountHexKey("authority")
+	ethPrivKeyHexStr := privateKeyHex
 	ethPrivKey, err := crypto.HexToECDSA(strings.TrimPrefix(ethPrivKeyHexStr, "0x"))
 	if err != nil {
 		log.Fatalf("failed to load private key: %v", err)
@@ -28,13 +32,14 @@ func main() {
 		PlasmaBlockInterval: 1000,
 		DAppChainClientCfg: oracle.DAppChainPlasmaClientConfig{
 			ChainID:  "default",
-			WriteURI: "http://localhost:46658/rpc",
+			WriteURI: "http://localhost:46658/rpc", //TODO make configs
 			ReadURI:  "http://localhost:46658/query",
 			Signer:   auth.NewEd25519Signer(loomPrivKey),
 		},
-		EthClientCfg: oracle.EthPlasmaClientConfig{
-			EthereumURI:      "http://localhost:8545",
-			PlasmaHexAddress: client.GetContractHexAddress("root_chain"),
+		EthClientCfg: eth.EthPlasmaClientConfig{
+			//TODO add options for infura also
+			EthereumURI:      "http://localhost:8545", //TODO make configs
+			PlasmaHexAddress: rootChainHexAddress,
 			PrivateKey:       ethPrivKey,
 			OverrideGas:      true,
 		},
