f2d27e61ce0790a1299a47ec7eedefe3dda28c4f
sriep <piers.shepperson@btinternet.com>
2018-07-04 16:23:45 +0100

    Create binary files for evm tests.

--- cmd/loom/main_test.go
+++ cmd/loom/main_test.go
@@ -4,9 +4,13 @@ import (
 	"testing"
 	"time"
 
+	"encoding/json"
 	"fmt"
 	"github.com/loomnetwork/loomchain/e2e/common"
+	"io/ioutil"
 	"os/exec"
+	"path"
+	"path/filepath"
 	"strings"
 )
 
@@ -20,12 +24,14 @@ func TestE2eEvm(t *testing.T) {
 	}
 	common.LoomPath = "../../loom"
 	common.ContractDir = "../../contracts"
+	//
 	for _, test := range tests {
 		*common.Validators = test.n
 		config, err := common.NewConfig("evm", test.testFile, test.genFile)
 		if err != nil {
 			t.Fatal(err)
 		}
+		readTestFiles("testbins.json", common.BaseDir, "evm")
 
 		binary, err := exec.LookPath("go")
 		if err != nil {
@@ -56,4 +62,28 @@ func TestE2eEvm(t *testing.T) {
 		// pause before running the next test
 		time.Sleep(500 * time.Millisecond)
 	}
+}
+
+func readTestFiles(inFile, baseDir, name string) error {
+	data, err := ioutil.ReadFile(inFile)
+	if err != nil {
+		return err
+	}
+	var fileContents []struct {
+		Filename string `json:"filename"`
+		Contents string `json:"contents"`
+	}
+	if err := json.Unmarshal(data, &fileContents); err != nil {
+		return err
+	}
+
+	basedirAbs, err := filepath.Abs(path.Join(baseDir, name))
+	if err != nil {
+		return err
+	}
+	for _, fileInfo := range fileContents {
+		absFileName := (path.Join(basedirAbs, fileInfo.Filename))
+		ioutil.WriteFile(absFileName, []byte(fileInfo.Contents), 0644)
+	}
+	return nil
 }
