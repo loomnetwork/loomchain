83d2eeaee1a7747abd08bef4f102464a66164810
sriep <piers.shepperson@btinternet.com>
2018-07-19 09:36:22 +0100

    touch

--- evm/loomethdb_test.go
+++ evm/loomethdb_test.go
@@ -4,9 +4,8 @@ package evm
 
 import (
 	"bytes"
-	"testing"
-
 	"github.com/stretchr/testify/require"
+	"testing"
 )
 
 // This test only verifies running a sort twice gives same result
@@ -59,5 +58,34 @@ func TestSortKeys2(t *testing.T) {
 
 	for i := 0; i < len(test1); i++ {
 		require.Equal(t, string(test2[i].key), string(test1[i].key))
+	}
+}
+
+func TestSortSecureKeys(t *testing.T) {
+	test1 := []kvPair{
+		{[]byte("secure-key-q�����;� ��Z���'=��ks֝B"), []byte("data1")},
+		{[]byte("secure-key-؀&*>�Y��F8I听Qia���SQ�6��f@"), []byte("data2")},
+		{[]byte("secure-key-)\n��T�b��E��8o�K���H@�6/���c"), []byte("data3")},
+		{[]byte("h����Ntԇ�ב��E��K]}�ɐW��a7��"), []byte("data4")},
+		{[]byte("�牔!��FQ���e�8���M˫����ܤ�S"), []byte("data5")},
+		{[]byte("�Ka����ͯ>/�� �\tߕ|���}j���<<�"), []byte("data6")},
+		{[]byte("-�F�bt����S	�A������;BT�b�gF"), []byte("data7")},
+	}
+	test1 = sortKeys([]byte("secure-key-"), test1)
+
+	test2 := []kvPair{
+		{[]byte("secure-key-)\n��T�b��E��8o�K���H@�6/���c"), []byte("data3")},
+		{[]byte("secure-key-q�����;� ��Z���'=��ks֝B"), []byte("data1")},
+		{[]byte("secure-key-؀&*>�Y��F8I听Qia���SQ�6��f@"), []byte("data2")},
+		{[]byte("h����Ntԇ�ב��E��K]}�ɐW��a7��"), []byte("data4")},
+		{[]byte("�牔!��FQ���e�8���M˫����ܤ�S"), []byte("data5")},
+		{[]byte("�Ka����ͯ>/�� �\tߕ|���}j���<<�"), []byte("data6")},
+		{[]byte("-�F�bt����S	�A������;BT�b�gF"), []byte("data7")},
+	}
+
+	test2 = sortKeys([]byte("secure-key-"), test2)
+
+	for i := 0; i < len(test1); i++ {
+		require.Equal(t, 0, bytes.Compare(test1[i].key, test2[i].key))
 	}
 }
