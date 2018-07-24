4234ea256e61abe1d570a82d363b132352fb58b1
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2018-07-24 14:06:43 +0700

    WIP: remove parale test from coin

--- e2e/coin_test.go
+++ e2e/coin_test.go
@@ -26,7 +26,6 @@ func TestContractCoin(t *testing.T) {
 	for _, tc := range tests {
 		test := tc
 		t.Run(test.name, func(t *testing.T) {
-			t.Parallel()
 			config, err := common.NewConfig(test.name, test.testFile, test.genFile, test.validators, test.accounts)
 			if err != nil {
 				t.Fatal(err)
