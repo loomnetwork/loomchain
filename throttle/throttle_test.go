22b6ab39ec1953bc084041b26f98e3bff863f151
Matthew Campbell <hyper@hyperworks.nu>
2018-08-02 19:21:05 +0100

    fix more test logs

--- throttle/throttle_test.go
+++ throttle/throttle_test.go
@@ -2,26 +2,26 @@ package throttle
 
 import (
 	"context"
+	"fmt"
+	"runtime/debug"
 	"testing"
+
 	"github.com/gogo/protobuf/proto"
-	abci "github.com/tendermint/abci/types"
-	"golang.org/x/crypto/ed25519"
-	loomAuth "github.com/loomnetwork/loomchain/auth"
+	"github.com/loomnetwork/go-loom"
 	"github.com/loomnetwork/go-loom/auth"
 	"github.com/loomnetwork/loomchain"
+	loomAuth "github.com/loomnetwork/loomchain/auth"
 	"github.com/loomnetwork/loomchain/log"
 	"github.com/loomnetwork/loomchain/store"
 	"github.com/stretchr/testify/require"
-	"github.com/loomnetwork/go-loom"
-	"fmt"
-	"runtime/debug"
+	abci "github.com/tendermint/abci/types"
+	"golang.org/x/crypto/ed25519"
 )
-
 
 func throttleMiddlewareHandler(t *testing.T, ttm loomchain.TxMiddlewareFunc, state loomchain.State, tx auth.SignedTx, ctx context.Context) (loomchain.TxHandlerResult, error) {
 	defer func() {
 		if rval := recover(); rval != nil {
-			logger := log.Root
+			logger := log.Default
 			logger.Error("Panic in TX Handler", "rvalue", rval)
 			println(debug.Stack())
 		}
@@ -35,9 +35,9 @@ func throttleMiddlewareHandler(t *testing.T, ttm loomchain.TxMiddlewareFunc, sta
 
 func TestThrottleTxMiddleware(t *testing.T) {
 	log.Setup("debug", "file://-")
-	log.Root.With("module", "throttle-middleware")
-	var maxAccessCount = int64(10);
-	var sessionDuration = int64(600);
+	log.Default.With("module", "throttle-middleware")
+	var maxAccessCount = int64(10)
+	var sessionDuration = int64(600)
 	origBytes := []byte("origin")
 	_, privKey, err := ed25519.GenerateKey(nil)
 	require.Nil(t, err)
@@ -53,9 +53,9 @@ func TestThrottleTxMiddleware(t *testing.T) {
 
 	require.Equal(t, len(tx.PublicKey), ed25519.PublicKeySize)
 
-	require.Equal(t,  len(tx.Signature), ed25519.SignatureSize)
+	require.Equal(t, len(tx.Signature), ed25519.SignatureSize)
 
-	require.True(t,  ed25519.Verify(tx.PublicKey, tx.Inner, tx.Signature))
+	require.True(t, ed25519.Verify(tx.PublicKey, tx.Inner, tx.Signature))
 
 	origin := loom.Address{
 		ChainID: state.Block().ChainID,
@@ -63,22 +63,21 @@ func TestThrottleTxMiddleware(t *testing.T) {
 	}
 
 	ctx := context.WithValue(state.Context(), loomAuth.ContextKeyOrigin, origin)
-	tmx := GetThrottleTxMiddleWare(maxAccessCount,sessionDuration)
+	tmx := GetThrottleTxMiddleWare(maxAccessCount, sessionDuration)
 	i := int64(1)
 
-	totalAccessCount := maxAccessCount*2
+	totalAccessCount := maxAccessCount * 2
 
 	fmt.Println(ctx, tmx, i, totalAccessCount)
 
 	for i <= totalAccessCount {
-		_, err := throttleMiddlewareHandler(t, tmx, state , tx , ctx )
+		_, err := throttleMiddlewareHandler(t, tmx, state, tx, ctx)
 		if i <= maxAccessCount {
 			require.Nil(t, err)
-		}else{
+		} else {
 			require.Error(t, err, fmt.Sprintf("Out of access count for current session: %d out of %d, Try after sometime!", i, maxAccessCount))
 		}
 		i += 1
 	}
-
 
 }
