c705fc52a65ac9bd19287c26b64e1dd43300bc9d
Vadim Macagon <vadim.macagon@gmail.com>
2018-08-06 02:55:05 +0700

    Fix IAVLStore Range()

--- store/store_test.go
+++ store/store_test.go
@@ -3,7 +3,11 @@ package store
 import (
 	"testing"
 
+	"github.com/loomnetwork/go-loom/plugin"
+	"github.com/loomnetwork/go-loom/util"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	dbm "github.com/tendermint/tmlibs/db"
 )
 
 var (
@@ -167,4 +171,104 @@ func TestCacheTxRollback(t *testing.T) {
 	assert.Nil(t, v2)
 	v3 = s.Get(key3)
 	assert.Nil(t, v3)
+}
+
+type storeTestFactory func(t *testing.T) (KVStore, string)
+
+func TestStoreRange(t *testing.T) {
+	factories := []storeTestFactory{
+		func(t *testing.T) (KVStore, string) {
+			db := dbm.NewMemDB()
+			s, err := NewIAVLStore(db)
+			require.NoError(t, err)
+			return s, "IAVLStore"
+		},
+		func(t *testing.T) (KVStore, string) {
+			return NewMemStore(), "MemStore"
+		},
+	}
+
+	for _, f := range factories {
+		s, storeName := f(t)
+
+		prefix1 := []byte("doremi")
+		prefix2 := append([]byte("stop"), byte(255))
+		prefix3 := append([]byte("stop"), byte(0))
+
+		entries := []*plugin.RangeEntry{
+			&plugin.RangeEntry{Key: []byte("abc"), Value: []byte("1")},
+			&plugin.RangeEntry{Key: []byte("abc123"), Value: []byte("2")},
+
+			&plugin.RangeEntry{Key: util.PrefixKey(prefix1, []byte("1")), Value: []byte("3")},
+			&plugin.RangeEntry{Key: util.PrefixKey(prefix1, []byte("2")), Value: []byte("4")},
+			&plugin.RangeEntry{Key: util.PrefixKey(prefix1, []byte("3")), Value: []byte("5")},
+
+			&plugin.RangeEntry{Key: util.PrefixKey(prefix2, []byte("3")), Value: []byte("6")},
+			&plugin.RangeEntry{Key: util.PrefixKey(prefix2, []byte("2")), Value: []byte("7")},
+			&plugin.RangeEntry{Key: util.PrefixKey(prefix2, []byte("1")), Value: []byte("8")},
+			&plugin.RangeEntry{Key: util.PrefixKey(prefix2, []byte("4")), Value: []byte("9")},
+
+			&plugin.RangeEntry{Key: util.PrefixKey(prefix3, []byte{byte(0)}), Value: []byte("10")},
+			&plugin.RangeEntry{Key: util.PrefixKey(prefix3, []byte{byte(255)}), Value: []byte("11")},
+		}
+		for _, e := range entries {
+			s.Set(e.Key, e.Value)
+		}
+
+		expected := []*plugin.RangeEntry{
+			entries[0],
+			entries[1],
+		}
+		actual := s.Range([]byte("abc"))
+
+		require.Len(t, actual, 2)
+		if storeName != "MemStore" { // TODO: MemStore iteration order should be stable, no random
+			for i := range expected {
+				require.EqualValues(t, expected[i], actual[i], storeName)
+			}
+		}
+
+		require.Len(t, s.Range([]byte("abc123")), 1)
+		require.EqualValues(t, entries[1], s.Range([]byte("abc123"))[0], storeName)
+
+		expected = []*plugin.RangeEntry{
+			entries[2],
+			entries[3],
+			entries[4],
+		}
+		actual = s.Range(prefix1)
+		require.Len(t, actual, len(expected), storeName)
+		if storeName != "MemStore" {
+			for i := range expected {
+				require.EqualValues(t, expected[i], actual[i], storeName)
+			}
+		}
+
+		expected = []*plugin.RangeEntry{
+			entries[7],
+			entries[6],
+			entries[5],
+			entries[8],
+		}
+		actual = s.Range(prefix2)
+		require.Len(t, actual, len(expected), storeName)
+		// TODO: MemStore keys should be iterated in ascending order
+		if storeName != "MemStore" {
+			for i := range expected {
+				require.EqualValues(t, expected[i], actual[i], storeName)
+			}
+		}
+
+		expected = []*plugin.RangeEntry{
+			entries[9],
+			entries[10],
+		}
+		actual = s.Range(prefix3)
+		require.Len(t, actual, len(expected), storeName)
+		if storeName != "MemStore" {
+			for i := range expected {
+				require.EqualValues(t, expected[i], actual[i], storeName)
+			}
+		}
+	}
 }
