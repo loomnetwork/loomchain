33d614e19c1f740c2cc34aa513013e1bdc0d4faf
Piers Shepperson <piers.shepperson@btinternet.com>
2018-08-28 10:22:15 +0100

    Improve ethcoin EVM integration to support selfdestruct (#312)

--- evm/interfaces.go
+++ evm/interfaces.go
@@ -8,6 +8,9 @@ import (
 type AccountBalanceManager interface {
 	GetBalance(addr loom.Address) (*loom.BigUInt, error)
 	Transfer(from, to loom.Address, amount *loom.BigUInt) error
+	AddBalance(addr loom.Address, amount *loom.BigUInt) error
+	SubBalance(addr loom.Address, amount *loom.BigUInt) error
+	SetBalance(addr loom.Address, amount *loom.BigUInt) error
 }
 
 type AccountBalanceManagerFactoryFunc func(readOnly bool) AccountBalanceManager
--- evm/loom_statedb.go
+++ evm/loom_statedb.go
@@ -31,17 +31,19 @@ func (s *LoomStateDB) GetBalance(addr common.Address) *big.Int {
 	return s.abm.GetBalance(addr)
 }
 
-// The EVM shouldn't be calling any of the functions below, the only way to manipulate an account
-// balance is through a transfer between accounts via the ethcoin contract.
-
-func (s *LoomStateDB) SubBalance(common.Address, *big.Int) {
-	panic("not implemented")
+func (s *LoomStateDB) SubBalance(address common.Address, amount *big.Int) {
+	s.abm.SubBalance(address, amount)
 }
 
-func (s *LoomStateDB) AddBalance(common.Address, *big.Int) {
-	panic("not implemented")
+func (s *LoomStateDB) AddBalance(address common.Address,amount *big.Int) {
+	s.abm.AddBalance(address, amount)
 }
 
-func (s *LoomStateDB) SetBalance(addr common.Address, amount *big.Int) {
-	panic("not implemented")
+func (s *LoomStateDB) SetBalance(address common.Address, amount *big.Int) {
+	s.abm.SetBalance(address, amount)
+}
+
+func (s *LoomStateDB) Suicide(address common.Address) bool {
+	s.SetBalance(address, common.Big0)
+	return s.StateDB.Suicide(address)
 }
