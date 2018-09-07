3f0f9d924de3d9d8ab07476236398736b2d3aed6
Matthew Campbell <hyper@hyperworks.nu>
2018-09-07 12:28:48 +0700

    fix test

--- throttle/middleware.go
+++ throttle/middleware.go
@@ -3,33 +3,34 @@ package throttle
 import (
 	// "errors"
 	"fmt"
-	`github.com/loomnetwork/go-loom`
-	`github.com/loomnetwork/go-loom/types`
-	`github.com/loomnetwork/loomchain/builtin/plugins/karma`
-	`github.com/loomnetwork/loomchain/registry`
-	`github.com/loomnetwork/loomchain/registry/factory`
-	"github.com/pkg/errors"
-	lauth "github.com/loomnetwork/go-loom/auth"
+
 	"github.com/gogo/protobuf/proto"
+	"github.com/loomnetwork/go-loom"
+	lauth "github.com/loomnetwork/go-loom/auth"
+	"github.com/loomnetwork/go-loom/types"
 	"github.com/loomnetwork/loomchain"
 	"github.com/loomnetwork/loomchain/auth"
+	"github.com/loomnetwork/loomchain/builtin/plugins/karma"
 	"github.com/loomnetwork/loomchain/log"
+	"github.com/loomnetwork/loomchain/registry"
+	"github.com/loomnetwork/loomchain/registry/factory"
+	"github.com/pkg/errors"
 )
 
 func GetThrottleTxMiddleWare(
-		maxAccessCount int64,
-		sessionDuration int64,
-		karmaEnabled bool,
-		deployKarmaCount int64,
-		deployEnabled bool,
-		callEnabled bool,
-		oracle loom.Address,
-		registryVersion factory.RegistryVersion,
-	) loomchain.TxMiddlewareFunc {
-	var createRegistry   factory.RegistryFactoryFunc
+	maxAccessCount int64,
+	sessionDuration int64,
+	karmaEnabled bool,
+	deployKarmaCount int64,
+	deployEnabled bool,
+	callEnabled bool,
+	oracle loom.Address,
+	registryVersion factory.RegistryVersion,
+) loomchain.TxMiddlewareFunc {
+	var createRegistry factory.RegistryFactoryFunc
 	var registryObject registry.Registry
 	th := NewThrottle(maxAccessCount, sessionDuration, karmaEnabled, deployKarmaCount, deployEnabled, callEnabled, oracle)
-	
+
 	return loomchain.TxMiddlewareFunc(func(
 		state loomchain.State,
 		txBytes []byte,
@@ -42,34 +43,34 @@ func GetThrottleTxMiddleWare(
 			}
 			registryObject = createRegistry(state)
 		}
-		
+
 		if (0 == th.karmaContractAddress.Compare(loom.Address{})) {
 			th.karmaContractAddress, err = registryObject.Resolve("karma")
 			if err != nil {
 				return next(state, txBytes)
 			}
 		}
-		
+
 		karmaState, err := th.getKarmaState(state)
 		if err != nil {
 			return res, errors.Wrap(err, "throttle: cannot find karma state")
 		}
-		
+
 		origin := auth.Origin(state.Context())
 		if origin.IsEmpty() {
 			return res, errors.New("throttle: transaction has no origin")
 		}
-		
+
 		var nonceTx lauth.NonceTx
 		if err := proto.Unmarshal(txBytes, &nonceTx); err != nil {
 			return res, errors.Wrap(err, "throttle: unwrap nonce Tx")
 		}
-		
+
 		var tx loomchain.Transaction
 		if err := proto.Unmarshal(nonceTx.Inner, &tx); err != nil {
 			return res, errors.New("throttle: unmarshal tx")
 		}
-		
+
 		var karmaConfig karma.Config
 		if karmaState.Has(karma.GetConfigKey()) {
 			curConfigB := karmaState.Get(karma.ConfigKey)
@@ -83,45 +84,45 @@ func GetThrottleTxMiddleWare(
 		if !karmaConfig.Enabled {
 			return next(state, txBytes)
 		}
-		
+
 		var oracle types.Address
 		if karmaState.Has(karma.OracleKey) {
 			oracleB := karmaState.Get(karma.OracleKey)
-			if err :=proto.Unmarshal(oracleB, &oracle); err != nil {
+			if err := proto.Unmarshal(oracleB, &oracle); err != nil {
 				return res, errors.Wrap(err, "throttle: getting karma oracle")
 			}
 		} else {
 			return res, errors.New("throttle: karma oracle not found")
 		}
-		
+
 		if tx.Id == 1 && !th.deployEnabled {
-			if  0 != origin.Compare(th.oracle) {
+			if 0 != origin.Compare(th.oracle) {
 				return res, errors.New("throttle: deploy  tx not enabled")
 			}
 		}
-		
+
 		if tx.Id == 2 && !th.callEnabled {
 			if 0 != origin.Compare(th.oracle) {
 				return res, errors.New("throttle: call tx not enabled")
 			}
 		}
-		
+
 		limiterCtx, deployLimiterCtx, err, err1 := th.run(state, "ThrottleTxMiddleWare", tx.Id, nonceTx.Sequence)
 
 		if err != nil || err1 != nil {
 			log.Error(err.Error())
 			return res, err
 		}
-		
+
 		if karmaConfig.SessionMaxAccessCount > 0 {
-			if  limiterCtx.Reached {
+			if limiterCtx.Reached {
 				message := fmt.Sprintf("Out of access count for current session: %d out of %d, Try after sometime! Total access count %d", limiterCtx.Limit-limiterCtx.Remaining, limiterCtx.Limit, th.totalAccessCount[origin.String()])
 				log.Error(message)
 				return res, errors.New(message)
 			}
-			if tx.Id == 1  {
-				fmt.Println("Remaining",deployLimiterCtx.Remaining,"limit",deployLimiterCtx.Limit)
-				message := fmt.Sprintf("Remaining %d limit %d", deployLimiterCtx.Remaining,"limit",deployLimiterCtx.Limit)
+			if tx.Id == 1 {
+				fmt.Println("Remaining", deployLimiterCtx.Remaining, "limit", deployLimiterCtx.Limit)
+				message := fmt.Sprintf("Remaining %d limit %d", deployLimiterCtx.Remaining, deployLimiterCtx.Limit)
 				log.Error(message)
 				if deployLimiterCtx.Reached {
 					//Not using limiting logic in this iteration
