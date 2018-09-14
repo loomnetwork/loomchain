bab4fb1d187d25bc4af55f127643ccb0eeb6c700
sriep <piers.shepperson@btinternet.com>
2018-09-14 16:49:13 +0100

    E2E tests for karma

---
+++ e2e/karma_1_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+	"testing"
+	"time"
+	
+	"github.com/loomnetwork/loomchain/e2e/common"
+)
+
+func TestE2eKarma(t *testing.T) {
+	tests := []struct {
+		name       string
+		testFile   string
+		validators int
+		accounts   int
+		genFile    string
+		yamlFile   string
+	}{
+		{"karma", "karma-1-test.toml", 1, 10, "karma-1-test.json", "karma-1-test.yaml"},
+	}
+	common.LoomPath = "../loom"
+	common.ContractDir = "../contracts"
+	
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			config, err := common.NewConfig(test.name, test.testFile, test.genFile, test.yamlFile, test.validators, test.accounts)
+			if err != nil {
+				t.Fatal(err)
+			}
+			
+			binary, err := exec.LookPath("go")
+			if err != nil {
+				t.Fatal(err)
+			}
+			// required binary
+			cmd := exec.Cmd{
+				Dir:  config.BaseDir,
+				Path: binary,
+				Args: []string{
+					binary,
+					"build",
+					"-tags",
+					"evm",
+					"-o",
+					"loom",
+					"github.com/loomnetwork/loomchain/cmd/loom",
+				},
+			}
+			if err := cmd.Run(); err != nil {
+				t.Fatal(fmt.Errorf("fail to execute command: %s\n%v", strings.Join(cmd.Args, " "), err))
+			}
+			
+			if err := common.DoRun(*config); err != nil {
+				t.Fatal(err)
+			}
+			
+			// pause before running the next test
+			time.Sleep(500 * time.Millisecond)
+		})
+	}
+}
