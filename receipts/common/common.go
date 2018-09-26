5474bee2b14cc1ef90c0a7f89a1b07ac64d5a47b
sriep <piers.shepperson@btinternet.com>
2018-09-26 09:43:04 +0100

    Added common.go

---
+++ receipts/common/common.go
@@ -0,0 +1,70 @@
+package common
+
+import (
+	`crypto/sha256`
+	`encoding/binary`
+	"github.com/gogo/protobuf/proto"
+	`github.com/loomnetwork/go-loom`
+	`github.com/loomnetwork/go-loom/plugin/types`
+	`github.com/loomnetwork/loomchain`
+	`github.com/loomnetwork/loomchain/eth/bloom`
+	`github.com/pkg/errors`
+)
+
+func WriteReceipt(
+		state loomchain.State,
+		caller, addr loom.Address,
+		events []*loomchain.EventData,
+		err error,
+		eventHadler loomchain.EventHandler,
+	) (types.EvmTxReceipt, error) {
+	var status int32
+	if err == nil {
+		status = 1
+	} else {
+		status = 0
+	}
+	block := state.Block()
+	txReceipt := types.EvmTxReceipt{
+		TransactionIndex:  state.Block().NumTxs,
+		BlockHash:         block.GetLastBlockID().Hash,
+		BlockNumber:       state.Block().Height,
+		CumulativeGasUsed: 0,
+		GasUsed:           0,
+		ContractAddress:   addr.Local,
+		LogsBloom:         bloom.GenBloomFilter(events),
+		Status:            status,
+		CallerAddress:     caller.MarshalPB(),
+	}
+	
+	preTxReceipt, errMarshal := proto.Marshal(&txReceipt)
+	if errMarshal != nil {
+		if err == nil {
+			return types.EvmTxReceipt{}, errors.Wrap(errMarshal, "marhsal tx receipt")
+		} else {
+			return types.EvmTxReceipt{}, errors.Wrapf(err, "marshalling reciept err %v", errMarshal)
+		}
+	}
+	h := sha256.New()
+	h.Write(preTxReceipt)
+	txHash := h.Sum(nil)
+	
+	txReceipt.TxHash = txHash
+	blockHeight := uint64(txReceipt.BlockNumber)
+	for _, event := range events {
+		event.TxHash = txHash
+		if eventHadler != nil {
+			_ = eventHadler.Post(blockHeight, event)
+		}
+		pEvent := types.EventData(*event)
+		txReceipt.Logs = append(txReceipt.Logs, &pEvent)
+	}
+
+	return txReceipt, nil
+}
+
+func BlockHeightToBytes(height uint64) []byte {
+	heightB := make([]byte, 8)
+	binary.LittleEndian.PutUint64(heightB, height)
+	return heightB
+}
