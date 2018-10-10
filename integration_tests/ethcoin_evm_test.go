be796002dec9986ca3d724a6ecc6f3ee5ef1186b
sriep <piers.shepperson@btinternet.com>
2018-10-10 19:41:37 +0100

    add debug param to newLoomVm call

--- integration_tests/ethcoin_evm_test.go
+++ integration_tests/ethcoin_evm_test.go
@@ -206,7 +206,7 @@ func (c *ethCoinIntegrationTestHelper) callEVM(ctx *plugin.FakeContextWithEVM, m
 	if err != nil {
 		return err
 	}
-	vm := evm.NewLoomVm(ctx.State, nil,nil, ctx.AccountBalanceManager)
+	vm := evm.NewLoomVm(ctx.State, nil,nil, ctx.AccountBalanceManager, true)
 	_, err = vm.Call(ctx.Message().Sender, c.Address, input, loom.NewBigUIntFromInt(0))
 	if err != nil {
 		return err
@@ -219,7 +219,7 @@ func (c *ethCoinIntegrationTestHelper) staticCallEVM(ctx *plugin.FakeContextWith
 	if err != nil {
 		return err
 	}
-	vm := evm.NewLoomVm(ctx.State, nil, nil,ctx.AccountBalanceManager)
+	vm := evm.NewLoomVm(ctx.State, nil, nil,ctx.AccountBalanceManager, true)
 	output, err := vm.StaticCall(ctx.Message().Sender, c.Address, input)
 	if err != nil {
 		return err
@@ -235,7 +235,7 @@ func deployContractToEVM(ctx *plugin.FakeContextWithEVM, filename string, caller
 	}
 	byteCode := common.FromHex(string(hexByteCode))
 
-	vm := evm.NewLoomVm(ctx.State, nil, nil,nil)
+	vm := evm.NewLoomVm(ctx.State, nil, nil,nil, true)
 	_, contractAddr, err = vm.Create(caller, byteCode, loom.NewBigUIntFromInt(0))
 	if err != nil {
 		return contractAddr, err
