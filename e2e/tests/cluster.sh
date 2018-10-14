dd4ffa14486ee2ef8596ee4f682c8e3b7daa10f8
Vadim Macagon <vadim.macagon@gmail.com>
2018-10-14 15:21:22 +0700

    Add Truffle tests for Loom EVM

---
+++ e2e/tests/cluster.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# This script can be used to spin up a local cluster for testing.
+
+set -exo pipefail
+
+echo "LOOM_BIN = ${LOOM_BIN}"
+
+INIT_CLUSTER=false
+START_CLUSTER=false
+STOP_CLUSTER=false
+CLUSTER_DIR=`pwd`
+CLUSTER_GENESIS=""
+
+
+while [[ "$#" > 0 ]]; do case $1 in
+  -i|--init) INIT_CLUSTER=true; shift;;
+  -g|--genesis) CLUSTER_GENESIS=$2; shift; shift;;
+  -d|--dir) CLUSTER_DIR=$2; shift; shift;;
+  --start) START_CLUSTER=true; shift;;
+  --stop) STOP_CLUSTER=true; shift;;
+  *) echo "Unknown parameter: $1"; shift; shift;;
+esac; done
+
+
+function init_cluster {
+    cd $CLUSTER_DIR
+    
+    if [[ -n "$CLUSTER_GENESIS" ]]; then
+        FLAGS="-g $CLUSTER_GENESIS"
+    fi
+
+    $LOOM_VALIDATORS_TOOL new \
+        $FLAGS \
+        -c loom.yml \
+        --base-dir $CLUSTER_DIR \
+        --contract-dir "" \
+        --name cluster \
+        --loom-path $LOOM_BIN \
+        --force
+    
+    echo "Initialized cluster in ${CLUSTER_DIR}/cluster"
+}
+
+function run_cluster {
+    cd $CLUSTER_DIR
+    
+    $LOOM_VALIDATORS_TOOL run --conf cluster/runner.toml > cluster.log 2>&1 &
+    LOOM_PID=$!
+    echo $LOOM_PID > $CLUSTER_DIR/loom.pid
+}
+
+function stop_cluster {
+    if [[ -z "$LOOM_PID" ]]; then
+        LOOM_PID=`cat ${CLUSTER_DIR}/loom.pid`
+        rm -f $CLUSTER_DIR/loom.pid
+    fi
+    if [[ -n "$LOOM_PID" ]]; then
+        echo "Stopping cluster (${LOOM_PID})"
+        kill -15 "${LOOM_PID}" &> /dev/null
+    fi
+    STOP_CLUSTER=false
+}
+
+if [[ "$INIT_CLUSTER" == true ]]; then
+    init_cluster
+fi
+
+if [[ "$START_CLUSTER" == true ]]; then
+    run_cluster
+fi
+
+if [[ "$STOP_CLUSTER" == true ]]; then
+    stop_cluster
+fi
