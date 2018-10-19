a0224fffc9b9a9c5ee2f46e33c9621fcbeab4410
Roy Blankman <riblankman@gmail.com>
2018-10-19 14:58:01 -0400

    initial electbydelegation function

--- builtin/plugins/dposv2/election.go
+++ builtin/plugins/dposv2/election.go
@@ -23,6 +23,25 @@ type DelegationResult struct {
 	DelegationTotal  loom.BigUInt
 }
 
+type byDelegationTotal []*DelegationResult
+
+func (s byDelegationTotal) Len() int {
+	return len(s)
+}
+
+func (s byDelegationTotal) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s byDelegationTotal) Less(i, j int) bool {
+	diff := int64(s[i].DelegationTotal.Cmp(&s[j].DelegationTotal))
+	if diff == 0 {
+		// make sure output is deterministic if power is equal
+		diff = int64(s[i].ValidatorAddress.Compare(s[j].ValidatorAddress))
+	}
+
+	return diff < 0
+}
 
 type byPower []*VoteResult
 
