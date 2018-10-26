07927ce8e7f1a06c1fda26b0ff6696af6899de57
Vadim Macagon <vadim.macagon@gmail.com>
2018-10-26 17:38:23 +0700

    Add PruningIAVLStore that periodically deletes old versions from app.db
    PruningIAVLStore should only be used on old overgrown clusters that we
    can't spin up fresh nodes on, it'll periodically prune old versions
    in a goroutine. Once the nodes are back to reasonable size switch back
    to the regular IAVLStore. PruningIAVLStore can be enabled via loom.yml
    setting:
    
    AppStore:
      PruneInterval: 30 // seconds, if zero regular IAVLStore will be used
    
    IAVLStore should be used in most cases, it now deletes one version as
    soon as soon as it saves a new one (each time a block is committed).
    Additional versions can be retained via loom.yml setting:
    
    AppStore:
      MaxVersions: 10 // defaults to 50, minimum is 2
    
    Regardless of which IAVLStore is used, the node will now compact app.db
    each time it starts. This can be disabled via loom.yml setting:
    
    AppStore:
      CompactOnLoad: false

---
+++ store/config.go
@@ -0,0 +1,22 @@
+package store
+
+type AppStoreConfig struct {
+	// If true the app store will be compacted before it's loaded to reclaim disk space.
+	CompactOnLoad bool
+	// Maximum number of app store versions to keep.
+	MaxVersions int64
+	// Number of seconds to wait after pruning a batch of old versions from the app store.
+	// If this is set to zero the app store will only be pruned after a new version is saved.
+	PruneInterval int64
+	// Number of versions to prune at a time.
+	PruneBatchSize int64
+}
+
+func DefaultConfig() *AppStoreConfig {
+	return &AppStoreConfig{
+		CompactOnLoad:  true,
+		MaxVersions:    50,
+		PruneInterval:  0,
+		PruneBatchSize: 50,
+	}
+}
