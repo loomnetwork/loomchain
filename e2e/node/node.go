0808c72641cbfd8b331b579eb6699d970d0add0a
Vadim Macagon <vadim.macagon@gmail.com>
2018-11-01 13:27:21 +0800

    Fix validators-tool to generate correct genesis for nodes
    validators-tool has been generating invalid genesis files since it got
    hacked to use DPOS v2 instead of v1, it only works with v2 now,
    but the default in loom is still v1. So, to actually get validators-tool
    to generates a cluster that starts you have to force nodes to be
    initialized for DPOS v2, easy enough to do by providing a base loom.yaml
    with `DPOSVersion: 2`. Unfortunately that didn't actually work because
    the base loom.yaml for the cluster wasn't copied to the node directory
    until after the node was initialized, these changes take care of that
    issue.
    
    Another issue is that the default genesis for the DPOS v2 contract had
    an invalid Location field, which prevented the node from loading the
    contract.

--- e2e/node/node.go
+++ e2e/node/node.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path"
@@ -60,6 +61,20 @@ func (n *Node) Init() error {
 		cp := exec.Command("cp", "-r", n.ContractDir, n.Dir)
 		if err := cp.Run(); err != nil {
 			return errors.Wrapf(err, "copy contract error")
+		}
+	}
+
+	// copy base loom.yaml (if there is one) to the node directory so that the node takes it into
+	// account when generating the default genesis
+	if len(n.BaseYaml) > 0 {
+		baseYaml, err := ioutil.ReadFile(n.BaseYaml)
+		if err != nil {
+			return errors.Wrap(err, "failed to read base loom.yaml file")
+		}
+
+		configPath := path.Join(n.Dir, "loom.yaml")
+		if err := ioutil.WriteFile(configPath, baseYaml, 0644); err != nil {
+			return errors.Wrap(err, "failed to write loom.yaml")
 		}
 	}
 
