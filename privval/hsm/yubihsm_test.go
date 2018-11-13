2a59e2be68dc96655b48193be22161bafe3f5307
Ostap <linux.dev53@gmail.com>
2018-11-13 07:02:48 -0500

    Added test cases for yubihsm

--- privval/hsm/yubihsm_test.go
+++ privval/hsm/yubihsm_test.go
@@ -8,11 +8,26 @@ import (
 
 const (
 	YHSM_TEST_CONN_URL   = "localhost:12345"
-	YHSM_TEST_AUTH_KEYID = 0
-	YHSM_TEST_PASSWORD   = "123456"
+	YHSM_TEST_AUTH_KEYID = 1
+	YHSM_TEST_PASSWORD   = "password"
 
 	YHSM_TEST_PRIVVAL_CONF = "yhsm_priv_validator.json"
 )
+
+// test for init
+func TestYubiInit(t *testing.T) {
+	if os.Getenv("HSM_YUBICO_TEST_ENABLE") != "true" {
+		t.Log("Yubico HSM Test Disabled")
+		return
+	}
+
+	pv := NewYubiHsmPV(YHSM_TEST_CONN_URL, YHSM_TEST_AUTH_KEYID, YHSM_TEST_PASSWORD)
+	err := pv.Init()
+	if err != nil {
+		t.Fatal(err)
+	}
+	pv.Destroy()
+}
 
 // test for genkey
 func TestYubiGenkey(t *testing.T) {
@@ -20,6 +35,28 @@ func TestYubiGenkey(t *testing.T) {
 		t.Log("Yubico HSM Test Disabled")
 		return
 	}
+
+	pv := NewYubiHsmPV(YHSM_TEST_CONN_URL, YHSM_TEST_AUTH_KEYID, YHSM_TEST_PASSWORD)
+	err := pv.Init()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer pv.Destroy()
+
+	err = pv.genEd25519KeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("sign key ID is '%v'", pv.SignKeyID)
+}
+
+// test for gen priv validator
+func TestYubiGenPrivval(t *testing.T) {
+	if os.Getenv("HSM_YUBICO_TEST_ENABLE") != "true" {
+		t.Log("Yubico HSM Test Disabled")
+		return
+	}
+
 	// check if priv validator is exist
 	if _, err := os.Stat(YHSM_TEST_PRIVVAL_CONF); !os.IsNotExist(err) {
 		t.Fatal("HSM priv validator file is already exist. Please try to remove it at first")
