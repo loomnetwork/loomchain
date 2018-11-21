aaa5f59c35455a9ffc15135071ffafc4ad4c49e5
Vadim Macagon <vadim.macagon@gmail.com>
2018-11-21 15:12:19 +0700

    Re-enable running tests in no-EVM builds
    Skipping them didn't seem to cut down on build times as much as expected

--- jenkins.sh
+++ jenkins.sh
@@ -24,7 +24,7 @@ export LOOM_BIN=`pwd`/loom
 export LOOM_VALIDATORS_TOOL=`pwd`/e2e/validators-tool
 
 make test
-make build-no-evm-tests
+make test-no-evm
 
 # setup & run truffle tests
 cd e2e/tests/truffle
