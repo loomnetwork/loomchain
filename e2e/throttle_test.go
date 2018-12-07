8be029c7e045c28d9761f005abe25a47f90c753f
sriep <piers.shepperson@btinternet.com>
2018-12-07 13:36:15 +0000

    E2E tests

--- e2e/throttle_test.go
+++ e2e/throttle_test.go
@@ -19,7 +19,7 @@ func TestE2eKarmaThrottle(t *testing.T) {
 		genFile    string
 		yamlFile   string
 	}{
-		{"throttle", "throttle-1-test.toml", 1, 10, "throttle-1-test.json", "throttle-1-test.yaml"},
+		{"throttle", "throttle-1-test.toml", 1, 2, "throttle-1-test.json", "throttle-1-test.yaml"},
 		{"origin-verification", "throttle-2-test.toml", 1, 2, "empty-genesis.json", "throttle-2-test.yaml"},
 	}
 	common.LoomPath = "../loom"
