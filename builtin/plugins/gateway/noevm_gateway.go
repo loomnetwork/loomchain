393592793669bc40cbff1b4dc25fd88cd18e2986
Parth <desaiparth08@gmail.com>
2018-12-31 17:16:45 +0530

    separating out noevm loomcoin gateway parts

--- builtin/plugins/gateway/noevm_gateway.go
+++ builtin/plugins/gateway/noevm_gateway.go
@@ -3,31 +3,18 @@
 package gateway
 
 import (
-	tgtypes "github.com/loomnetwork/go-loom/builtin/types/transfer_gateway"
 	"github.com/loomnetwork/go-loom/plugin"
 	contract "github.com/loomnetwork/go-loom/plugin/contractpb"
 )
 
-type (
-	InitRequest = tgtypes.TransferGatewayInitRequest
-)
-
 type Gateway struct {
-	loomCoinTG bool
 }
 
 func (gw *Gateway) Meta() (plugin.Meta, error) {
-	if gw.loomCoinTG {
-		return plugin.Meta{
-			Name:    "loomcoin-gateway",
-			Version: "0.1.0",
-		}, nil
-	} else {
-		return plugin.Meta{
-			Name:    "gateway",
-			Version: "0.1.0",
-		}, nil
-	}
+	return plugin.Meta{
+		Name:    "gateway",
+		Version: "0.1.0",
+	}, nil
 }
 
 func (gw *Gateway) Init(ctx contract.Context, req *InitRequest) error {
@@ -35,5 +22,3 @@ func (gw *Gateway) Init(ctx contract.Context, req *InitRequest) error {
 }
 
 var Contract plugin.Contract = contract.MakePluginContract(&Gateway{})
-
-var LoomCoinContract plugin.Contract = contract.MakePluginContract(&Gateway{})
