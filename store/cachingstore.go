20ee8da56efd5ece9c1cb8f1112d1e1a30bf3ee0
Vadim Macagon <vadim.macagon@gmail.com>
2019-01-02 14:12:01 +1000

    Split the CachingStore in two, ReadOnlyCachingStore is for queries...
    While the CachingStore now writes through the cache, but doesn't read
    from the cache.

--- store/cachingstore.go
+++ store/cachingstore.go
@@ -1,11 +1,11 @@
 package store
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/allegro/bigcache"
-	"github.com/loomnetwork/go-loom/plugin"
 
 	"github.com/loomnetwork/loomchain/log"
 )
@@ -33,9 +33,12 @@ type CachingStoreConfig struct {
 	Verbose bool
 }
 
+// CachingStore wraps a write-through cache around a VersionedKVStore.
+// NOTE: Writes update the cache, reads do not, to read from the cache use the store returned by
+//       ReadOnly().
 type CachingStore struct {
-	source VersionedKVStore
-	cache  *bigcache.BigCache
+	VersionedKVStore
+	cache *bigcache.BigCache
 }
 
 func DefaultCachingStoreConfig() *CachingStoreConfig {
@@ -94,8 +97,8 @@ func NewCachingStore(source VersionedKVStore, config *CachingStoreConfig) (*Cach
 	}
 
 	return &CachingStore{
-		source: source,
-		cache:  cache,
+		VersionedKVStore: source,
+		cache:            cache,
 	}, nil
 }
 
@@ -105,7 +108,7 @@ func (c *CachingStore) Delete(key []byte) {
 		// Only log error and dont error out
 		log.Error(fmt.Sprintf("[CachingStore] error while deleting key in cache, error: %v", err.Error()))
 	}
-	c.source.Delete(key)
+	c.VersionedKVStore.Delete(key)
 }
 
 func (c *CachingStore) Set(key, val []byte) {
@@ -114,11 +117,30 @@ func (c *CachingStore) Set(key, val []byte) {
 		// Only log error and dont error out
 		log.Error(fmt.Sprintf("[CachingStore] error while setting key in cache, error: %v", err.Error()))
 	}
-
-	c.source.Set(key, val)
+	c.VersionedKVStore.Set(key, val)
 }
 
-func (c *CachingStore) Has(key []byte) bool {
+// ReadOnlyCachingStore prevents any modification to the underlying backing store,
+// and uses the cache for reads.
+type ReadOnlyCachingStore struct {
+	*CachingStore
+}
+
+func NewReadOnlyCachingStore(cachingStore *CachingStore) *ReadOnlyCachingStore {
+	return &ReadOnlyCachingStore{
+		CachingStore: cachingStore,
+	}
+}
+
+func (c *ReadOnlyCachingStore) Delete(key []byte) {
+	panic("[ReadOnlyCachingStore] Delete() not implemented")
+}
+
+func (c *ReadOnlyCachingStore) Set(key, val []byte) {
+	panic("[ReadOnlyCachingStore] Set() not implemented")
+}
+
+func (c *ReadOnlyCachingStore) Has(key []byte) bool {
 	data, err := c.cache.Get(string(key))
 	exists := true
 
@@ -129,17 +151,17 @@ func (c *CachingStore) Has(key []byte) bool {
 		default:
 			// Since, there is no provision of passing error in the interface
 			// we would directly access source
-			log.Error(fmt.Sprintf("[CachingStore] error while getting key from cache, error: %v", e.Error()))
+			log.Error(fmt.Sprintf("[ReadOnlyCachingStore] error while getting key from cache, error: %v", e.Error()))
 		}
 
-		data = c.source.Get(key)
+		data = c.VersionedKVStore.Get(key)
 		if data == nil {
 			exists = false
 		} else {
 			exists = true
 			setErr := c.cache.Set(string(key), data)
 			if setErr != nil {
-				log.Error(fmt.Sprintf("[CachingStore] error while setting key in cache, error: %v", setErr.Error()))
+				log.Error(fmt.Sprintf("[ReadOnlyCachingStore] error while setting key in cache, error: %v", setErr.Error()))
 			}
 		}
 	}
@@ -147,11 +169,7 @@ func (c *CachingStore) Has(key []byte) bool {
 	return exists
 }
 
-func (c *CachingStore) Range(prefix []byte) plugin.RangeData {
-	return c.source.Range(prefix)
-}
-
-func (c *CachingStore) Get(key []byte) []byte {
+func (c *ReadOnlyCachingStore) Get(key []byte) []byte {
 	data, err := c.cache.Get(string(key))
 
 	if err != nil {
@@ -161,33 +179,25 @@ func (c *CachingStore) Get(key []byte) []byte {
 		default:
 			// Since, there is no provision of passing error in the interface
 			// we would directly access source
-			log.Error(fmt.Sprintf("[CachingStore] error while getting key from cache, error: %v", e.Error()))
+			log.Error(fmt.Sprintf("[ReadOnlyCachingStore] error while getting key from cache, error: %v", e.Error()))
 		}
 
-		data = c.source.Get(key)
+		data = c.VersionedKVStore.Get(key)
 		if data == nil {
 			return nil
 		}
 		setErr := c.cache.Set(string(key), data)
 		if setErr != nil {
-			log.Error(fmt.Sprintf("[CachingStore] error while setting key in cache, error: %v", setErr.Error()))
+			log.Error(fmt.Sprintf("[ReadOnlyCachingStore] error while setting key in cache, error: %v", setErr.Error()))
 		}
 	}
 	return data
 }
 
-func (c *CachingStore) Hash() []byte {
-	return c.source.Hash()
+func (c *ReadOnlyCachingStore) SaveVersion() ([]byte, int64, error) {
+	return nil, 0, errors.New("[ReadOnlyCachingStore] SaveVersion() not implemented")
 }
 
-func (c *CachingStore) Version() int64 {
-	return c.source.Version()
-}
-
-func (c *CachingStore) SaveVersion() ([]byte, int64, error) {
-	return c.source.SaveVersion()
-}
-
-func (c *CachingStore) Prune() error {
-	return c.source.Prune()
+func (c *ReadOnlyCachingStore) Prune() error {
+	return errors.New("[ReadOnlyCachingStore] Prune() not implemented")
 }
