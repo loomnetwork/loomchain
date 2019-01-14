4e72defd04c5c33c6d8f5cd9a8dcd8a3ce25d95a
suhasagg@gmail.com <root@ubuntu.ubuntu-domain>
2019-01-15 00:41:33 +0530

    .

--- events/log.go
+++ events/log.go
@@ -1,6 +1,11 @@
 package events
 
-import "github.com/loomnetwork/loomchain/log"
+import (
+	"encoding/json"
+
+	ptypes "github.com/loomnetwork/go-loom/plugin/types"
+	"github.com/loomnetwork/loomchain/log"
+)
 
 // LogEventDispatcher just logs events
 type LogEventDispatcher struct {
@@ -13,7 +18,15 @@ func NewLogEventDispatcher() *LogEventDispatcher {
 
 // Send sends the event
 func (ed *LogEventDispatcher) Send(index uint64, msg []byte) error {
+	var logs ptypes.EventData
+	//Can use proto.Unmarshal as well but it required proto.Marshal in following as well - emitMsg, err := json.Marshal(&eventData) in event.go, This was causing incorrect formatting of some logs
+	//Following code in log.Debug("Received published message", "msg", msg.Body(), "remote", clientCtx.GetRemoteAddr()) in query_server.go so used JSON.marshal but JSON it produces for published message is escaped json.
+	//Fix for the same is used in this function below
+	if err := json.Unmarshal(msg, &logs); err != nil {
+		return err
+	}
+	//NewJSONLogger is used of go-kit, this is primarily to avoid escaped JSON and proper JSON  format
+	log.JSONInfo("Event emitted", "index", index, "length", len(msg), "msg", logs)
 
-	log.Info("Event emitted: index: %d, length: %d, msg: %s\n", index, len(msg), msg)
 	return nil
 }
