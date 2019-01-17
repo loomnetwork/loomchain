b6dcc81f0911f7ac90b2411e86ba70fee3a76cb3
Roy Blankman <blankman@boustro.com>
2019-01-17 05:52:28 -0500

    adding CheckDistribution & error logging (#627)
    * cosmetic chagnes pre-downtime testing
    * adding check distribution function & tests
    * improving error logging
    * (dpos) creating util.go file

--- builtin/plugins/dposv2/dpos_test.go
+++ builtin/plugins/dposv2/dpos_test.go
@@ -873,11 +873,16 @@ func TestValidatorRewards(t *testing.T) {
 		require.Nil(t, err)
 	}
 
+	checkResponse, err := dposContract.CheckDistribution(contractpb.WrapPluginContext(dposCtx.WithSender(addr1)), &CheckDistributionRequest{})
+	require.Nil(t, err)
+	assert.Equal(t, checkResponse.Amount.Value.Cmp(&loom.BigUInt{big.NewInt(0)}), 1)
+
 	claimResponse, err := dposContract.ClaimDistribution(contractpb.WrapPluginContext(dposCtx.WithSender(addr1)), &ClaimDistributionRequest{
 		WithdrawalAddress: addr1.MarshalPB(),
 	})
 	require.Nil(t, err)
 	assert.Equal(t, claimResponse.Amount.Value.Cmp(&loom.BigUInt{big.NewInt(0)}), 1)
+	assert.Equal(t, claimResponse.Amount.Value.Cmp(&checkResponse.Amount.Value), 0)
 
 	delegator1Claim, err := dposContract.ClaimDistribution(contractpb.WrapPluginContext(dposCtx.WithSender(delegatorAddress1)), &ClaimDistributionRequest{
 		WithdrawalAddress: delegatorAddress1.MarshalPB(),
