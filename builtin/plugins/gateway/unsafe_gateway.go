e689de07c4ec249d5c4f62f8c24b0d0fa936a0df
Georgios Konstantopoulos <me@gakonst.com>
2019-01-23 10:32:04 +0200

    Add Unsafe functions without access control for: Resetting the owner Adding/Removing Oracles

--- builtin/plugins/gateway/unsafe_gateway.go
+++ builtin/plugins/gateway/unsafe_gateway.go
@@ -22,11 +22,36 @@ func (gw *UnsafeGateway) ResetMainnetBlock(ctx contract.Context, req *ResetMainn
 		return err
 	}
 
-	if loom.UnmarshalAddressPB(state.Owner).Compare(ctx.Message().Sender) != 0 {
-		return ErrNotAuthorized
+	state.LastMainnetBlockNum = req.GetLastMainnetBlockNum()
+
+	return saveState(ctx, state)
+}
+
+func (gw *UnsafeGateway) UnsafeAddOracle(ctx contract.Context, req *AddOracleRequest) error {
+	oracleAddr := loom.UnmarshalAddressPB(req.Oracle)
+	if ctx.Has(oracleStateKey(oracleAddr)) {
+		return ErrOracleAlreadyRegistered
 	}
 
-	state.LastMainnetBlockNum = req.GetLastMainnetBlockNum()
+	return addOracle(ctx, oracleAddr)
+}
+
+func (gw *UnsafeGateway) UnsafeRemoveOracle(ctx contract.Context, req *RemoveOracleRequest) error {
+	oracleAddr := loom.UnmarshalAddressPB(req.Oracle)
+	if !ctx.Has(oracleStateKey(oracleAddr)) {
+		return ErrOracleNotRegistered
+	}
+
+    return removeOracle(ctx, oracleAddr)
+}
+
+func (gw *UnsafeGateway) ResetOwnerKey(ctx contract.Context, req *AddOracleRequest) error {
+	state, err := loadState(ctx)
+	if err != nil {
+		return err
+	}
+
+	state.Owner = req.Oracle
 
 	return saveState(ctx, state)
 }
