4ef6068ceaf61ba85d61552adfe2d6221b317d03
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2019-01-29 16:52:07 +0700

    add tests and update event store interface

--- events/db_indexer_test.go
+++ events/db_indexer_test.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/loomnetwork/loomchain"
+	"github.com/loomnetwork/go-loom/plugin/types"
 	"github.com/loomnetwork/loomchain/store"
 	"github.com/stretchr/testify/require"
 )
@@ -12,25 +12,86 @@ import (
 func TestDBIndexerSendEvents(t *testing.T) {
 	var tests = []struct {
 		blockHeight uint64
-		eventData   loomchain.EventData
+		eventData   types.EventData
 		eror        error
 	}{
 		{
 			blockHeight: 1,
-			eventData:   loomchain.EventData{},
+			eventData:   types.EventData{},
 		},
 		{
 			blockHeight: 2,
-			eventData:   loomchain.EventData{},
+			eventData:   types.EventData{},
 		},
 	}
 
-	var dispatcher = &DBIndexerEventDispatcher{EventStore: store.NewMockEventStore()}
+	mockEventStore := store.NewMockEventStore(store.NewMemStore())
+	var dispatcher = &DBIndexerEventDispatcher{EventStore: mockEventStore}
 
 	for _, test := range tests {
 		msg, err := json.Marshal(test.eventData)
 		require.Nil(t, err)
 		err = dispatcher.Send(test.blockHeight, msg)
 		require.Equal(t, err, test.eror)
+	}
+}
+
+func TestDBIndexerGenUniquContractID(t *testing.T) {
+	var tests = []struct {
+		blockHeight uint64
+		eventData   types.EventData
+		wantID      uint64
+	}{
+		{
+			blockHeight: 1,
+			eventData: types.EventData{
+				PluginName: "plugin1",
+			},
+			wantID: 1,
+		},
+		{
+			blockHeight: 2,
+			eventData: types.EventData{
+				PluginName: "plugin2",
+			},
+			wantID: 2,
+		},
+		{
+			blockHeight: 2,
+			eventData: types.EventData{
+				PluginName: "plugin2",
+			},
+			wantID: 2,
+		},
+		{
+			blockHeight: 2,
+			eventData: types.EventData{
+				PluginName: "plugin10",
+			},
+			wantID: 3,
+		},
+		{
+			blockHeight: 2,
+			eventData: types.EventData{
+				PluginName: "plugin5",
+			},
+			wantID: 4,
+		},
+	}
+
+	mockEventStore := store.NewMockEventStore(store.NewMemStore())
+	var dispatcher = &DBIndexerEventDispatcher{EventStore: mockEventStore}
+
+	for _, test := range tests {
+		msg, err := json.Marshal(test.eventData)
+		require.Nil(t, err)
+		err = dispatcher.Send(test.blockHeight, msg)
+		require.Nil(t, err)
+	}
+
+	for _, test := range tests {
+		id, err := mockEventStore.GetContractID(test.eventData.PluginName)
+		require.Nil(t, err)
+		require.Equal(t, test.wantID, id)
 	}
 }
