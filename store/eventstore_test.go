0753c98126b04d239321260f50cf430c0989fa6d
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2019-01-30 18:28:56 +0700

    add filter event

--- store/eventstore_test.go
+++ store/eventstore_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/gogo/protobuf/proto"
 	"github.com/loomnetwork/go-loom/plugin/types"
 	"github.com/stretchr/testify/require"
 )
@@ -13,71 +14,88 @@ func TestEventStoreSet(t *testing.T) {
 	var eventStore EventStore = NewMockEventStore(memstore)
 
 	// set pluginname
-	err := eventStore.SetContractID("plugin1", 1)
+	var contractID uint64 = 1
+	err := eventStore.SetContractID("plugin1", contractID)
 	require.Nil(t, err)
 	val := memstore.Get(prefixPluginName("plugin1"))
-	require.EqualValues(t, []byte("1"), val)
+	require.EqualValues(t, contractID, bytesToUint64(val))
 
 	err = eventStore.SetContractID("plugin2", 2)
 	require.Nil(t, err)
 	val = memstore.Get(prefixPluginName("plugin2"))
-	require.EqualValues(t, []byte("2"), val)
+	require.EqualValues(t, 2, bytesToUint64(val))
 
 	err = eventStore.SetContractID("", 999)
 	require.Nil(t, err)
 	val = memstore.Get(prefixPluginName(""))
-	require.EqualValues(t, []byte("999"), val)
+	require.EqualValues(t, 999, bytesToUint64(val))
 
-	// SetEventByBlockHightEventIndex
-	event1 := []byte("event1")
-	err = eventStore.SetEventByBlockHightEventIndex(1, 0, event1)
+	// SetEventByBlockHeightEventIndex
+	event1 := types.EventData{BlockHeight: 1, EncodedBody: []byte("event1")}
+	err = eventStore.SetEvent(contractID, event1.BlockHeight, &event1)
 	require.Nil(t, err)
-	val = memstore.Get(prefixBlockHightEventIndex(1, 0))
-	require.EqualValues(t, event1, val)
+	val = memstore.Get(prefixBlockHightEventIndex(event1.BlockHeight, event1.TransactionIndex))
+	var gotevent1 types.EventData
+	err = proto.Unmarshal(val, &gotevent1)
+	require.Nil(t, err)
+	require.EqualValues(t, event1, gotevent1)
 
-	event2 := []byte("somelongerdata")
-	err = eventStore.SetEventByBlockHightEventIndex(100, 0, event2)
+	event2 := types.EventData{BlockHeight: 2, EncodedBody: []byte("event2")}
+	err = eventStore.SetEvent(contractID, event2.BlockHeight, &event2)
 	require.Nil(t, err)
-	val = memstore.Get(prefixBlockHightEventIndex(100, 0))
-	require.EqualValues(t, event2, val)
+	val = memstore.Get(prefixBlockHightEventIndex(event2.BlockHeight, event2.TransactionIndex))
+	var gotevent2 types.EventData
+	err = proto.Unmarshal(val, &gotevent2)
+	require.Nil(t, err)
+	require.EqualValues(t, event2, gotevent2)
 
-	// SetEventByContractIDBlockHightEventIndex
-	event3 := []byte("event3")
-	err = eventStore.SetEventByContractIDBlockHightEventIndex(20, 1, 0, event3)
+	event3 := types.EventData{BlockHeight: 20, TransactionIndex: 0, EncodedBody: []byte("event3")}
+	err = eventStore.SetEvent(contractID, event3.BlockHeight, &event3)
 	require.Nil(t, err)
-	val = memstore.Get(prefixContractIDBlockHightEventIndex(20, 1, 0))
-	require.EqualValues(t, event3, val)
+	val = memstore.Get(prefixContractIDBlockHightEventIndex(contractID, event3.BlockHeight, event3.TransactionIndex))
+	var gotevent3 types.EventData
+	err = proto.Unmarshal(val, &gotevent3)
+	require.Nil(t, err)
+	require.EqualValues(t, event3, gotevent3)
 
-	event4 := []byte("event4")
-	err = eventStore.SetEventByContractIDBlockHightEventIndex(4, 100, 0, event4)
+	event4 := types.EventData{BlockHeight: 20, TransactionIndex: 1, EncodedBody: []byte("event4")}
+	err = eventStore.SetEvent(contractID, event4.BlockHeight, &event4)
 	require.Nil(t, err)
-	val = memstore.Get(prefixContractIDBlockHightEventIndex(4, 100, 0))
-	require.EqualValues(t, event4, val)
+	val = memstore.Get(prefixContractIDBlockHightEventIndex(contractID, event4.BlockHeight, event4.TransactionIndex))
+	var gotevent4 types.EventData
+	err = proto.Unmarshal(val, &gotevent4)
+	require.Nil(t, err)
+	require.EqualValues(t, event4, gotevent4)
 }
 
 func TestEventStoreFilterSameBlockHeight(t *testing.T) {
 	memstore := NewMemStore()
 	var eventStore EventStore = NewMockEventStore(memstore)
-	type eventData struct {
-		blockHight uint64
-		eventIndex uint64
-		data       []byte
-	}
-	err := eventStore.SetContractID("plugin1", 1)
+	var contractID uint64 = 1
+	err := eventStore.SetContractID("plugin1", contractID)
 	require.Nil(t, err)
 
-	var tests []eventData
+	var blockHeight1 uint64 = 1
+	var blockHeight2 uint64 = 1
+
+	var eventData []*types.EventData
 	for i := 0; i < 10; i++ {
-		eventStore.SetEventByBlockHightEventIndex(1, uint64(i), []byte(fmt.Sprintf("event-%d-%d", 1, i)))
-		tests = append(tests, eventData{
-			blockHight: 1,
-			eventIndex: uint64(i),
-			data:       []byte(fmt.Sprintf("event-%d-%d", 1, i)),
-		})
+		event := types.EventData{
+			BlockHeight:      blockHeight1,
+			TransactionIndex: uint64(i),
+			EncodedBody:      []byte(fmt.Sprintf("event-%d-%d", blockHeight1, i)),
+		}
+		eventStore.SetEvent(contractID, blockHeight1, &event)
+		eventData = append(eventData, &event)
 	}
 	// more event for testing filter
 	for i := 0; i < 10; i++ {
-		eventStore.SetEventByBlockHightEventIndex(2, uint64(i), []byte(fmt.Sprintf("event-%d-%d", 2, i)))
+		event := types.EventData{
+			BlockHeight:      blockHeight2,
+			TransactionIndex: uint64(i),
+			EncodedBody:      []byte(fmt.Sprintf("event-%d-%d", blockHeight1, i)),
+		}
+		eventStore.SetEvent(contractID, blockHeight2, &event)
 	}
 
 	filter1 := &types.EventFilter{
@@ -87,10 +105,9 @@ func TestEventStoreFilterSameBlockHeight(t *testing.T) {
 	}
 	events, err := eventStore.FilterEvents(filter1)
 	require.Nil(t, err)
-	require.Equal(t, len(tests), len(events), "expect the same length")
-	for i, e := range events {
-		require.Equal(t, tests[i].blockHight, e.BlockHeight)
-		require.Equal(t, tests[i].eventIndex, e.TransactionIndex)
-		require.Equal(t, tests[i].data, e.EncodedBody)
-	}
+	require.Equal(t, len(eventData), len(events), "expect the same length")
+	// TODO: sort the events because underlying eventstore uses map, which has no order
+	// for i, e := range events {
+	// 	require.EqualValues(t, eventData[i], e)
+	// }
 }
