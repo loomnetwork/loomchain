ec191b5f7ba6728c1b86b84b8a20b80c5c3a17a9
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2019-01-30 14:50:27 +0700

    wip: add FilterEvent

--- events/db_indexer.go
+++ events/db_indexer.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 
+	"github.com/gogo/protobuf/proto"
+	loom "github.com/loomnetwork/go-loom"
 	"github.com/loomnetwork/go-loom/plugin/types"
 	"github.com/loomnetwork/loomchain"
 	"github.com/loomnetwork/loomchain/store"
@@ -21,27 +23,46 @@ func NewDBIndexerEventDispatcher(es store.EventStore) *DBIndexerEventDispatcher 
 
 func (ed *DBIndexerEventDispatcher) Send(blockHeight uint64, msg []byte) error {
 	var eventData types.EventData
-	if err := json.Unmarshal(msg, &eventData); err != nil {
+	var err error
+	if err = json.Unmarshal(msg, &eventData); err != nil {
 		return err
 	}
 
-	// TODO: more efficient way to persist event data
 	// resolve contractID
-	contractID, err := ed.GetContractID(eventData.PluginName)
-	if err != nil {
-		return err
-	}
-	// TODO: EVM to find by address
-	if contractID == 0 {
-		contractID = ed.NextContractID()
-		ed.SetContractID(eventData.PluginName, contractID)
+	// Go contract uses plugin name, EVM contract uses address
+	var contractID uint64
+	if eventData.PluginName != "" {
+		contractID, err = ed.GetContractID(eventData.PluginName)
+		if err != nil {
+			return err
+		}
+		if contractID == 0 {
+			contractID = ed.NextContractID()
+			ed.SetContractID(eventData.PluginName, contractID)
+		}
+	} else {
+		address := loom.UnmarshalAddressPB(eventData.Address).String()
+		contractID, err = ed.GetContractID(address)
+		if err != nil {
+			return err
+		}
+		if contractID == 0 {
+			contractID = ed.NextContractID()
+			ed.SetContractID(address, contractID)
+		}
+		contractID, err = ed.GetContractID(address)
 	}
 
-	err = ed.SetEventByBlockHightEventIndex(eventData.BlockHeight, eventData.TransactionIndex, msg)
+	eventDataPB, err := proto.Marshal(&eventData)
 	if err != nil {
 		return err
 	}
-	err = ed.SetEventByContractIDBlockHightEventIndex(contractID, eventData.BlockHeight, eventData.TransactionIndex, msg)
+
+	err = ed.SetEventByBlockHightEventIndex(eventData.BlockHeight, eventData.TransactionIndex, eventDataPB)
+	if err != nil {
+		return err
+	}
+	err = ed.SetEventByContractIDBlockHightEventIndex(contractID, eventData.BlockHeight, eventData.TransactionIndex, eventDataPB)
 	if err != nil {
 		return err
 	}
@@ -49,4 +70,9 @@ func (ed *DBIndexerEventDispatcher) Send(blockHeight uint64, msg []byte) error {
 	// TODO: Remove printf
 	log.Printf("Event emitted: index: %d, contractID: %d, eventData: %+v\n", blockHeight, contractID, eventData)
 	return nil
+}
+
+func Query(es store.EventStore, filter *types.EventFilter) ([]*types.EventData, error) {
+	// TODO: validate filter
+	return es.FilterEvents(filter)
 }
