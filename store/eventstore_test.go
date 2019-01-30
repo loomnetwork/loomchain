c00ec22d2e78be732941be5c840571b886a6c996
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2019-01-30 14:50:27 +0700

    wip: add FilterEvent

--- store/eventstore_test.go
+++ store/eventstore_test.go
@@ -1,12 +1,14 @@
 package store
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/loomnetwork/go-loom/plugin/types"
 	"github.com/stretchr/testify/require"
 )
 
-func TestEventStore(t *testing.T) {
+func TestEventStoreSet(t *testing.T) {
 	memstore := NewMemStore()
 	var eventStore EventStore = NewMockEventStore(memstore)
 
@@ -51,4 +53,44 @@ func TestEventStore(t *testing.T) {
 	require.Nil(t, err)
 	val = memstore.Get(prefixContractIDBlockHightEventIndex(4, 100, 0))
 	require.EqualValues(t, event4, val)
+}
+
+func TestEventStoreFilterSameBlockHeight(t *testing.T) {
+	memstore := NewMemStore()
+	var eventStore EventStore = NewMockEventStore(memstore)
+	type eventData struct {
+		blockHight uint64
+		eventIndex uint64
+		data       []byte
+	}
+	err := eventStore.SetContractID("plugin1", 1)
+	require.Nil(t, err)
+
+	var tests []eventData
+	for i := 0; i < 10; i++ {
+		eventStore.SetEventByBlockHightEventIndex(1, uint64(i), []byte(fmt.Sprintf("event-%d-%d", 1, i)))
+		tests = append(tests, eventData{
+			blockHight: 1,
+			eventIndex: uint64(i),
+			data:       []byte(fmt.Sprintf("event-%d-%d", 1, i)),
+		})
+	}
+	// more event for testing filter
+	for i := 0; i < 10; i++ {
+		eventStore.SetEventByBlockHightEventIndex(2, uint64(i), []byte(fmt.Sprintf("event-%d-%d", 2, i)))
+	}
+
+	filter1 := &types.EventFilter{
+		FromBlock: 1,
+		ToBlock:   1,
+		Contract:  "plugin1",
+	}
+	events, err := eventStore.FilterEvents(filter1)
+	require.Nil(t, err)
+	require.Equal(t, len(tests), len(events), "expect the same length")
+	for i, e := range events {
+		require.Equal(t, tests[i].blockHight, e.BlockHeight)
+		require.Equal(t, tests[i].eventIndex, e.TransactionIndex)
+		require.Equal(t, tests[i].data, e.EncodedBody)
+	}
 }
