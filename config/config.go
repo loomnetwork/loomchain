f81e63473611111047e40764b42b98d47cf302c5
suhasagg <suhasagg@gmail.com>
2019-02-10 15:27:59 +0530

    Config File

--- config/config.go
+++ config/config.go
@@ -13,6 +13,7 @@ import (
 
 	dposv2OracleCfg "github.com/loomnetwork/loomchain/builtin/plugins/dposv2/oracle/config"
 	plasmacfg "github.com/loomnetwork/loomchain/builtin/plugins/plasma_cash/config"
+	"github.com/loomnetwork/loomchain/events"
 	"github.com/loomnetwork/loomchain/gateway"
 	hsmpv "github.com/loomnetwork/loomchain/privval/hsm"
 	receipts "github.com/loomnetwork/loomchain/receipts/handler"
@@ -26,59 +27,85 @@ import (
 )
 
 type Config struct {
-	RootDir             string
-	DBName              string
-	DBBackend           string
-	GenesisFile         string
-	PluginsDir          string
-	QueryServerHost     string
-	EventDispatcherURI  string
-	ContractLogLevel    string
-	LogDestination      string
-	LoomLogLevel        string
-	BlockchainLogLevel  string
-	Peers               string
-	PersistentPeers     string
-	RPCListenAddress    string
-	ChainID             string
-	RPCProxyPort        int32
-	RPCBindAddress      string
-	// Controls whether or not empty blocks should be generated periodically if there are no txs or
-	// AppHash changes. Defaults to true.
-	CreateEmptyBlocks          bool
-	SessionMaxAccessCount      int64
-	SessionDuration            int64
-	LogStateDB                 bool
-	LogEthDbBatch              bool
+	// Cluster
+	ChainID                    string
 	RegistryVersion            int32
 	ReceiptsVersion            int32
 	EVMPersistentTxReceiptsMax uint64
-	TransferGateway            *gateway.TransferGatewayConfig
-	LoomCoinTransferGateway    *gateway.TransferGatewayConfig
-	PlasmaCash                 *plasmacfg.PlasmaCashSerializableConfig
+
 	// When this setting is enabled Loom EVM accounts are hooked up to the builtin ethcoin Go contract,
 	// which makes it possible to use the payable/transfer features of the EVM to transfer ETH in
 	// Solidity contracts running on the Loom EVM. This setting is disabled by default, which means
 	// all the EVM accounts always have a zero balance.
 	EVMAccountsEnabled bool
-	EVMDebugEnabled    bool
+	DPOSVersion        int64
 	BootLegacyDPoS     bool
 
-	Oracle              string
-	DeployEnabled       bool
-	CallEnabled         bool
-	CallSessionDuration int64
-	Karma               *KarmaConfig
-	DPOSVersion         int64
+	// Controls whether or not empty blocks should be generated periodically if there are no txs or
+	// AppHash changes. Defaults to true.
+	CreateEmptyBlocks bool
 
+	// Network
+	QueryServerHost  string
+	RPCListenAddress string
+	RPCProxyPort     int32
+	RPCBindAddress   string
+	Peers            string
+	PersistentPeers  string
+
+	// Throttle
+	Oracle                      string
+	DeployEnabled               bool
+	CallEnabled                 bool
+	SessionDuration             int64
+	Karma                       *KarmaConfig
+	GoContractDeployerWhitelist *throttle.GoContractDeployerWhitelistConfig
+	TxLimiter                   *throttle.TxLimiterConfig
+
+	// Logging
+	LogDestination     string
+	ContractLogLevel   string
+	LoomLogLevel       string
+	BlockchainLogLevel string
+	LogStateDB         bool
+	LogEthDbBatch      bool
+	Metrics            *Metrics
+
+	// Transfer gateway
+	TransferGateway         *gateway.TransferGatewayConfig
+	LoomCoinTransferGateway *gateway.TransferGatewayConfig
+
+	// Plasma Cash
+	PlasmaCash *plasmacfg.PlasmaCashSerializableConfig
+
+	// Cashing store
 	CachingStoreConfig *store.CachingStoreConfig
-    BlockStoreConfig   *store.BlockStoreConfig
+
+	//BlockStore Config
+	BlockStoreConfig   *store.BlockStoreConfig
+	//Hsm
+	HsmConfig *hsmpv.HsmConfig
+
+	// Oracle serializable
+	// todo Cannot be read in from file due to nested pointers to structs.
 	DPOSv2OracleConfig *dposv2OracleCfg.OracleSerializableConfig
 
-	AppStore  *store.AppStoreConfig
-	HsmConfig *hsmpv.HsmConfig
-	TxLimiter *throttle.TxLimiterConfig
-	Metrics   *Metrics
+	// AppStore
+	AppStore *store.AppStoreConfig
+
+	// Should pretty much never be changed
+	RootDir     string
+	DBName      string
+	DBBackend   string
+	GenesisFile string
+	PluginsDir  string
+
+	// Dragons
+	EVMDebugEnabled bool
+
+	// Evenstore
+	EventStore      *events.EventStoreConfig
+	EventDispatcher *events.EventDispatcherConfig
 }
 
 type Metrics struct {
@@ -88,6 +115,7 @@ type Metrics struct {
 type KarmaConfig struct {
 	Enabled         bool  // Activate karma module
 	ContractEnabled bool  // Allows you to deploy karma contract to collect data even if chain doesn't use it
+	UpkeepEnabled   bool  // Adds an upkeep cost to deployed and active contracts for each user
 	MaxCallCount    int64 // Maximum number call transactions per session duration
 	SessionDuration int64 // Session length in seconds
 }
@@ -102,6 +130,7 @@ func DefaultKarmaConfig() *KarmaConfig {
 	return &KarmaConfig{
 		Enabled:         false,
 		ContractEnabled: false,
+		UpkeepEnabled:   false,
 		MaxCallCount:    0,
 		SessionDuration: 0,
 	}
@@ -156,7 +185,30 @@ func ParseConfig() (*Config, error) {
 	v.AddConfigPath(filepath.Join("./", "config")) // search root directory /config
 	v.AddConfigPath("./../../../")
 
-	v.ReadInConfig()
+	if err := v.ReadInConfig(); err != nil {
+		return nil, err
+	}
+	conf := DefaultConfig()
+	err := v.Unmarshal(conf)
+	if err != nil {
+		return nil, err
+	}
+	return conf, err
+}
+
+func ParseConfigFrom(filename string) (*Config, error) {
+	v := viper.New()
+	v.AutomaticEnv()
+	v.SetEnvPrefix("LOOM")
+
+	v.SetConfigName(filename)                      // name of config file (without extension)
+	v.AddConfigPath("./")                          // search root directory
+	v.AddConfigPath(filepath.Join("./", "config")) // search root directory /config
+	v.AddConfigPath("./../../../")
+
+	if err := v.ReadInConfig(); err != nil {
+		return nil, err
+	}
 	conf := DefaultConfig()
 	err := v.Unmarshal(conf)
 	if err != nil {
@@ -191,7 +243,6 @@ func DefaultConfig() *Config {
 		PluginsDir:                 "contracts",
 		QueryServerHost:            "tcp://127.0.0.1:9999",
 		RPCListenAddress:           "tcp://0.0.0.0:46657", //TODO this is an ephemeral port in linux, we should move this
-		EventDispatcherURI:         "",
 		ContractLogLevel:           "info",
 		LoomLogLevel:               "info",
 		LogDestination:             "",
@@ -211,12 +262,11 @@ func DefaultConfig() *Config {
 		EVMAccountsEnabled:         false,
 		EVMDebugEnabled:            false,
 
-		Oracle:              "",
-		DeployEnabled:       true,
-		CallEnabled:         true,
-		CallSessionDuration: 1,
-		BootLegacyDPoS:      false,
-		DPOSVersion:         1,
+		Oracle:         "",
+		DeployEnabled:  true,
+		CallEnabled:    true,
+		BootLegacyDPoS: false,
+		DPOSVersion:    1,
 	}
 	cfg.TransferGateway = gateway.DefaultConfig(cfg.RPCProxyPort)
 	cfg.LoomCoinTransferGateway = gateway.DefaultLoomCoinTGConfig(cfg.RPCProxyPort)
@@ -224,11 +274,16 @@ func DefaultConfig() *Config {
 	cfg.AppStore = store.DefaultConfig()
 	cfg.HsmConfig = hsmpv.DefaultConfig()
 	cfg.TxLimiter = throttle.DefaultTxLimiterConfig()
+	cfg.GoContractDeployerWhitelist = throttle.DefaultGoContractDeployerWhitelistConfig()
+
 	cfg.DPOSv2OracleConfig = dposv2OracleCfg.DefaultConfig()
 	cfg.CachingStoreConfig = store.DefaultCachingStoreConfig()
 	cfg.BlockStoreConfig = store.DefaultBlockCacheConfig()
 	cfg.Metrics = DefaultMetrics()
 	cfg.Karma = DefaultKarmaConfig()
+
+	cfg.EventDispatcher = events.DefaultEventDispatcherConfig()
+	cfg.EventStore = events.DefaultEventStoreConfig()
 	return cfg
 }
 
@@ -244,6 +299,8 @@ func (c *Config) Clone() *Config {
 	clone.AppStore = c.AppStore.Clone()
 	clone.HsmConfig = c.HsmConfig.Clone()
 	clone.TxLimiter = c.TxLimiter.Clone()
+	clone.EventStore = c.EventStore.Clone()
+	clone.EventDispatcher = c.EventDispatcher.Clone()
 	return &clone
 }
 
@@ -305,11 +362,9 @@ func parseCfgTemplate() (*template.Template, error) {
 
 const defaultLoomYamlTemplate = `# Loom Node config file
 # See https://loomx.io/developers/docs/en/loom-yaml.html for additional info.
-
 # 
 # Cluster-wide settings that must not change after cluster is initialized.
 #
-
 # Cluster ID
 ChainID: "{{ .ChainID }}"
 RegistryVersion: {{ .RegistryVersion }}
@@ -317,48 +372,58 @@ ReceiptsVersion: {{ .ReceiptsVersion }}
 EVMPersistentTxReceiptsMax: {{ .EVMPersistentTxReceiptsMax }}
 EVMAccountsEnabled: {{ .EVMAccountsEnabled }}
 DPOSVersion: {{ .DPOSVersion }}
-
+BootLegacyDPoS: {{ .BootLegacyDPoS }}
 CreateEmptyBlocks: {{ .CreateEmptyBlocks }}
-
 #
 # Network
 #
-
 QueryServerHost: "{{ .QueryServerHost }}"
 RPCListenAddress: "{{ .RPCListenAddress }}"
 RPCProxyPort: {{ .RPCProxyPort }}
 RPCBindAddress: "{{ .RPCBindAddress }}"
-EventDispatcherURI: "{{ .EventDispatcherURI }}"
 Peers: "{{ .Peers }}"
 PersistentPeers: "{{ .PersistentPeers }}"
-
 #
-# Karma
+# Throttle
 #
-
-Oracle: {{ .Oracle }}
+Oracle: "{{ .Oracle }}"
 DeployEnabled: {{ .DeployEnabled }}
 CallEnabled: {{ .CallEnabled }}
 SessionDuration: {{ .SessionDuration }}
-KarmaEnabled: {{ .KarmaEnabled }}
-KarmaMaxCallCount: {{ .KarmaMaxCallCount }}
-KarmaSessionDuration: {{ .KarmaSessionDuration }}
-
+Karma:
+  Enabled: {{ .Karma.Enabled }}
+  ContractEnabled: {{ .Karma.ContractEnabled }}
+  UpkeepEnabled: {{ .Karma.UpkeepEnabled }}
+  MaxCallCount: {{ .Karma.MaxCallCount }}
+  SessionDuration: {{ .Karma.SessionDuration }}
+GoContractDeployerWhitelist:
+  Enabled: {{ .GoContractDeployerWhitelist.Enabled }}
+  DeployerAddressList:
+  {{- range .GoContractDeployerWhitelist.DeployerAddressList}}
+    - "{{. -}}"
+  {{- end}}
+TxLimiter:
+  LimitDeploys: {{ .TxLimiter.LimitDeploys }}
+  LimitCalls: {{ .TxLimiter.LimitCalls }}
+  CallSessionDuration: {{ .TxLimiter.CallSessionDuration }}
+  DeployerAddressList:
+  {{- range .TxLimiter.DeployerAddressList}}
+  - "{{. -}}" 
+  {{- end}}
 #
 # Logging
 #
-
 LogDestination: "{{ .LogDestination }}"
 ContractLogLevel: "{{ .ContractLogLevel }}"
 LoomLogLevel: "{{ .LoomLogLevel }}"
 BlockchainLogLevel: "{{ .BlockchainLogLevel }}"
 LogStateDB: {{ .LogStateDB }}
 LogEthDbBatch: {{ .LogEthDbBatch }}
-
+Metrics:
+  EventHandling: {{ .Metrics.EventHandling }}
 #
 # Transfer Gateway
 #
-
 TransferGateway:
   # Enables the Transfer Gateway Go contract on the node, must be the same on all nodes.
   ContractEnabled: {{ .TransferGateway.ContractEnabled }}
@@ -391,25 +456,113 @@ TransferGateway:
   OracleReconnectInterval: {{ .TransferGateway.OracleReconnectInterval }}
   # Address on from which the out-of-process Oracle should expose the status & metrics endpoints.
   OracleQueryAddress: "{{ .TransferGateway.OracleQueryAddress }}"
-
 #
 # Plasma Cash
 #
-
 PlasmaCash:
   ContractEnabled: {{ .PlasmaCash.ContractEnabled }}
   OracleEnabled: {{ .PlasmaCash.OracleEnabled }}
-
+#
+# Cashing store 
+#
+CachingStoreConfig: 
+  CachingEnabled: {{ .CachingStoreConfig.CachingEnabled }}
+  # Number of cache shards, value must be a power of two
+  Shards: {{ .CachingStoreConfig.Shards }} 
+  # Time after we need to evict the key
+  EvictionTimeInSeconds: {{ .CachingStoreConfig.EvictionTimeInSeconds }} 
+  # interval at which clean up of expired keys will occur
+  CleaningIntervalInSeconds: {{ .CachingStoreConfig.CleaningIntervalInSeconds }} 
+  # Total size of cache would be: MaxKeys*MaxSizeOfValueInBytes
+  MaxKeys: {{ .CachingStoreConfig.MaxKeys }} 
+  MaxSizeOfValueInBytes: {{ .CachingStoreConfig.MaxSizeOfValueInBytes }} 
+  # Logs operations
+  Verbose: {{ .CachingStoreConfig.Verbose }} 
+  LogLevel: "{{ .CachingStoreConfig.LogLevel }}" 
+  LogDestination: "{{ .CachingStoreConfig.LogDestination }}" 
+#
+# Hsm 
+#
+HsmConfig:
+  # flag to enable HSM
+  HsmEnabled: {{ .HsmConfig.HsmEnabled }}
+  # device type of HSM
+  HsmDevType: "{{ .HsmConfig.HsmDevType }}"
+  # the path of PKCS#11 library
+  HsmP11LibPath: "{{ .HsmConfig.HsmP11LibPath }}"
+  # connection URL to YubiHSM
+  HsmConnURL: {{ .HsmConfig.HsmConnURL }}
+  # Auth key ID for YubiHSM
+  HsmAuthKeyID: {{ .HsmConfig.HsmAuthKeyID }}
+  # Auth password
+  HsmAuthPassword: "{{ .HsmConfig.HsmAuthPassword }}"
+  # Sign Key ID for YubiHSM
+  HsmSignKeyID: {{ .HsmConfig.HsmSignKeyID }}
+  # key domain
+  HsmSignKeyDomain: {{ .HsmConfig.HsmSignKeyDomain }}
+#
+# Oracle serializable 
+#
+DPOSv2OracleConfig:
+  Enabled: {{ .DPOSv2OracleConfig.Enabled }}
+  StatusServiceAddress: "{{ .DPOSv2OracleConfig.StatusServiceAddress }}"
+  MainnetPollInterval: {{ .DPOSv2OracleConfig.MainnetPollInterval }}
+{{if .DPOSv2OracleConfig.DAppChainCfg -}}
+  DAppChainCfg: 
+     WriteURI: "{{ .DPOSv2OracleConfig.DAppChainCfg.WriteURI }}"
+     ReadURI: "{{ .DPOSv2OracleConfig.DAppChainCfg.ReadURI }}"
+     PrivateKeyPath: "{{ .DPOSv2OracleConfig.DAppChainCfg.PrivateKeyPath }}"
+{{end}}
+{{if .DPOSv2OracleConfig.EthClientCfg -}}
+  EthClientCfg: 
+     EthereumURI: "{{ .DPOSv2OracleConfig.EthClientCfg.EthereumURI }}"
+     PrivateKeyPath: {{ .DPOSv2OracleConfig.EthClientCfg.PrivateKeyPath }}
+{{end}}
+{{if .DPOSv2OracleConfig.TimeLockWorkerCfg -}}
+  TimeLockWorkerCfg: 
+     TimeLockFactoryHexAddress: "{{ .DPOSv2OracleConfig.TimeLockWorkerCfg.TimeLockFactoryHexAddress }}"
+     Enabled: {{ .DPOSv2OracleConfig.TimeLockWorkerCfg.Enabled }}
+{{end}}
+#
+# App store
+#
+AppStore:
+  # If true the app store will be compacted before it's loaded to reclaim disk space.
+  CompactOnLoad: {{ .AppStore.CompactOnLoad }}
+  # Maximum number of app store versions to keep, if zero old versions will never be deleted.
+  MaxVersions: {{ .AppStore.MaxVersions }}
+  # Number of seconds to wait after pruning a batch of old versions from the app store.
+  # If this is set to zero the app store will only be pruned after a new version is saved.
+  PruneInterval: {{ .AppStore.PruneInterval }}
+  # Number of versions to prune at a time.
+  PruneBatchSize: {{ .AppStore.PruneBatchSize }}
 # These should pretty much never be changed
 RootDir: "{{ .RootDir }}"
 DBName: "{{ .DBName }}"
 GenesisFile: "{{ .GenesisFile }}"
 PluginsDir: "{{ .PluginsDir }}"
-
 #
 # Here be dragons, don't change the defaults unless you know what you're doing
 #
-
-UseCheckTx: {{ .UseCheckTx }}
 EVMDebugEnabled: {{ .EVMDebugEnabled }}
+
+{{if .EventStore -}}
+#
+# EventStore
+#
+EventStore:
+  DBName: {{.EventStore.DBName}}
+  DBBackend: {{.EventStore.DBBackend}}
+{{end}}
+#
+# EventDispatcher
+#
+EventDispatcher:
+  # Available dispatcher: "db_indexer" | "log" | "redis"
+  Dispatcher: {{.EventDispatcher.Dispatcher}}
+  {{if eq .EventDispatcher.Dispatcher "redis"}}
+  # Redis will be use when Dispatcher is "redis"
+  Redis:
+	URI: "{{.EventDispatcher.Redis.URI}}"
+  {{end}}
 `
