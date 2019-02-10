ae9049d14a862bc397072902d5fe885b595d9d09
suhasagg <suhasagg@gmail.com>
2019-02-10 20:32:25 +0530

    Review change.

--- receipts/chain/v1/chain.go
+++ receipts/chain/v1/chain.go
@@ -35,7 +35,7 @@ func (r *ReceiptHandler) GetReceipt(state loomchain.ReadOnlyState, txHash []byte
 	txReceiptProto := receiptState.Get(txHash)
 	txReceipt := types.EvmTxReceipt{}
 	if txReceiptProto == nil {
-		return txReceipt, errors.New("Nil Transcation Receipt")
+		return txReceipt, errors.New("Tx receipt not found")
 	}
 	err := proto.Unmarshal(txReceiptProto, &txReceipt)
 	return txReceipt, err
