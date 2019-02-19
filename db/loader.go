3cbe1bc59622fe5f13a60843a75ee186fc919324
suhasagg <suhasagg@gmail.com>
2019-02-19 16:08:01 +0530

    BadgerDB Integration loomchain

--- db/loader.go
+++ db/loader.go
@@ -10,6 +10,7 @@ const (
 	GoLevelDBBackend = "goleveldb"
 	CLevelDBBackend  = "cleveldb"
 	MemDBackend      = "memdb"
+	BadgerDBBackend  = "badgerdb"
 )
 
 type DBWrapper interface {
@@ -25,6 +26,8 @@ func LoadDB(dbBackend, name, directory string) (DBWrapper, error) {
 		return LoadCLevelDB(name, directory)
 	case MemDBackend:
 		return LoadMemDB()
+	case BadgerDBBackend:
+		return LoadBadgerDB(name,directory)
 	default:
 		return nil, fmt.Errorf("unknown db backend: %s", dbBackend)
 	}
