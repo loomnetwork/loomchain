c6caef6fa3c84899c94b24ea2381bf2b5ca2ef22
suhasagg <suhasagg@gmail.com>
2019-02-26 18:20:09 +0530

    Go Level DB Prometheus metrics collection

---
+++ db/promgo_test.go
@@ -0,0 +1,103 @@
+package db
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	cmn "github.com/tendermint/tendermint/libs/common"
+	dbm "github.com/tendermint/tendermint/libs/db"
+)
+
+func TestGoLevelDBMetricsCollector(t *testing.T) {
+
+	numItems := int64(100)
+	internal := map[int64]int64{}
+	for i := 0; i < int(numItems); i++ {
+		internal[int64(i)] = int64(0)
+	}
+	db, err := dbm.NewGoLevelDB("app2", "")
+	if err != nil {
+		t.Fatal(err.Error())
+		return
+	}
+
+	for i := 0; i < 1000; i++ {
+		// Write something
+
+		idx := (int64(cmn.RandInt()) % numItems)
+		internal[idx]++
+		val := internal[idx]
+		idxBytes := int642Bytes(int64(idx))
+		valBytes := int642Bytes(int64(val))
+		db.Set(
+			idxBytes,
+			valBytes,
+		)
+	}
+
+	for i := 0; i < 1000; i++ {
+		// Read something
+		idx := (int64(cmn.RandInt()) % numItems)
+		val := internal[idx]
+		idxBytes := int642Bytes(int64(idx))
+		_ = int642Bytes(int64(val))
+		_ = db.Get(idxBytes)
+
+	}
+
+	collector := newStatsCollector("goleveldb", "app2", "")
+	counter := 0
+	fmt.Println("ok, starting Collection")
+	for {
+		if counter > 5 {
+			break
+		}
+		time.Sleep(2 * time.Second)
+		go testCollector(collector)
+		counter++
+	}
+
+}
+
+// testCollector performs a single metrics collection pass against the input
+// prometheus.Collector, and returns a string containing metrics output.
+// Go level DB metrics are available at end point and get ingested in prometheus
+func testCollector(collector prometheus.Collector) {
+	if err := prometheus.Register(collector); err != nil {
+		fmt.Println(err)
+	}
+	defer prometheus.Unregister(collector)
+
+	promServer := httptest.NewServer(prometheus.Handler())
+	defer promServer.Close()
+
+	resp, err := http.Get(promServer.URL)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Print(string(buf))
+}
+
+func int642Bytes(
+	i int64) []byte {
+	buf := make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, uint64(i))
+	return buf
+}
+
+func bytes2Int64(buf []byte) int64 {
+	return int64(binary.BigEndian.Uint64(buf))
+}
---
+++ db/promgolevel.go
@@ -0,0 +1,50 @@
+//provides a Prometheus metrics collector for Golevel database backend in tendermint
+package db
+
+import (
+	"sync"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+const (
+	// namespace is the top-level namespace metric names.
+	namespace = "leveldb"
+)
+
+// New creates a new prometheus.Collector that can be registered with
+// Prometheus to scrape metrics from a Golevel database handle.
+//
+// Name should specify a unique name for the collector (preferably name of database backend), and will be added
+// as a label to all produced Prometheus metrics.
+func New(name, dbname, dbpath string) prometheus.Collector {
+	return &collector{
+		stats: newStatsCollector(name, dbname, dbpath),
+		//bucketStats: newBucketStatsCollector(name, db),
+	}
+}
+
+// Enforce that collector is a prometheus.Collector.
+var _ prometheus.Collector = &collector{}
+
+// A collector is a prometheus.Collector for Golevel database metrics.
+type collector struct {
+	mu    sync.Mutex
+	stats *statsCollector
+}
+
+// Describe implements the prometheus.Collector interface.
+func (c *collector) Describe(ch chan<- *prometheus.Desc) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.stats.Describe(ch)
+}
+
+// Collect implements the prometheus.Collector interface.
+func (c *collector) Collect(ch chan<- prometheus.Metric) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.stats.Collect(ch)
+}
