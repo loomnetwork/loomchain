8dfc9045fd8f8e8d1d74be2bd2692dc410cddbfb
pathornteng <pathorn.teng@gmail.com>
2019-02-26 13:30:33 +0700

    move get-mapping and list-mapping to staking

--- cmd/loom/staking.go
+++ cmd/loom/staking.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/loomnetwork/go-loom/builtin/commands"
+	"github.com/loomnetwork/go-loom/builtin/types/address_mapper"
 	"github.com/loomnetwork/go-loom/cli"
 	"github.com/loomnetwork/loomchain/builtin/plugins/dposv2"
 	"github.com/spf13/cobra"
@@ -15,6 +16,8 @@ func newStakingCommand() *cobra.Command {
 	cmd.Short = "Run staking commands"
 	cmd.AddCommand(
 		StakingListAllDelegationsCmd(),
+		StakingGetMappingCmd(),
+		StakingListMappingCmd(),
 	)
 	return cmd
 }
@@ -27,6 +30,52 @@ func StakingListAllDelegationsCmd() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			var resp dposv2.ListAllDelegationsResponse
 			err := cli.StaticCallContract(commands.DPOSV2ContractName, "ListAllDelegations", &dposv2.ListAllDelegationsRequest{}, &resp)
+			if err != nil {
+				return err
+			}
+			out, err := formatJSON(&resp)
+			if err != nil {
+				return err
+			}
+			fmt.Println(out)
+			return nil
+		},
+	}
+}
+
+func StakingGetMappingCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "get-mapping",
+		Short: "Get mapping address",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var resp address_mapper.AddressMapperGetMappingResponse
+			from, err := cli.ParseAddress(args[0])
+			if err != nil {
+				return err
+			}
+			err = cli.StaticCallContract(commands.AddressMapperContractName, "GetMapping", &address_mapper.AddressMapperGetMappingRequest{
+				From: from.MarshalPB(),
+			}, &resp)
+			if err != nil {
+				return err
+			}
+			out, err := formatJSON(&resp)
+			if err != nil {
+				return err
+			}
+			fmt.Println(out)
+			return nil
+		},
+	}
+}
+
+func StakingListMappingCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "list-mapping",
+		Short: "List mapping address",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var resp address_mapper.AddressMapperListMappingResponse
+			err := cli.StaticCallContract(commands.AddressMapperContractName, "ListMapping", &address_mapper.AddressMapperListMappingRequest{}, &resp)
 			if err != nil {
 				return err
 			}
