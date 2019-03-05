47f20d468db71ee2667f771f7cca693958d5e85a
Piers Shepperson <piers.shepperson@btinternet.com>
2019-03-05 14:43:31 +0000

    chain Id middleware verification

--- auth/address_mapping.go
+++ auth/address_mapping.go
@@ -6,9 +6,11 @@ import (
 
 	"github.com/gogo/protobuf/proto"
 	"github.com/loomnetwork/go-loom"
+	"github.com/loomnetwork/go-loom/auth"
 	"github.com/loomnetwork/go-loom/plugin/contractpb"
 	"github.com/loomnetwork/loomchain"
 	"github.com/loomnetwork/loomchain/builtin/plugins/address_mapper"
+	"github.com/loomnetwork/go-loom/vm"
 	"github.com/pkg/errors"
 	"golang.org/x/crypto/ed25519"
 )
@@ -16,7 +18,7 @@ import (
 const (
 	EthChainId = "eth" // hard coded in address-mapper as only chainId supported by address-mapper
 	DefaultLoomChainId = "default"
-	Loom = ""
+	Loom = "loom"
 	Eth  = "eth"
 )
 
@@ -71,40 +73,94 @@ func GetSignatureTxMiddleware(
 			return r, err
 		}
 
-		chain, found := externalNetworks[tx.ChainName]
-		if !found {
-			return r, errors.Wrapf(err, "chain type %v not supported", tx.ChainName)
-		}
-
-		if !chain.Enabled {
-			return r, errors.Wrapf(err, "chain type %v not enabled", tx.ChainName)
-		}
-
-		var localAddr []byte
-		verifier, found := originVerification[chain.Type]
-
-		if !found {
-			return r, fmt.Errorf("invalid chain id %v", chain.Type)
-		}
-
-		localAddr, err = verifier(tx)
-		if err != nil {
-			return r, errors.Wrapf(err, "tx origin cannot be verifyed as type %v", chain.Type)
-		}
-
-		addressMappingCtx, err := createAddressMappingCtx(state)
-		if err != nil {
-			return res, errors.Wrap(err, "failed to create address-mapping contract context")
-		}
-
-		origin, err := GetMappedOrigin(addressMappingCtx, localAddr, chain.Prefix, state.Block().ChainID, externalNetworks)
-		if err != nil {
-			return r, err
+		var origin loom.Address
+		// If ChainName is nil then use chainId to determine verification method.
+		// Otherwise use address mapping
+		if len(tx.ChainName) == 0 {
+			origin, err = chainIdVerication(tx)
+		} else {
+			addressMappingCtx, err := createAddressMappingCtx(state)
+			if err != nil {
+				return r, errors.Wrap(err, "failed to create address-mapping contract context")
+			}
+			origin, err = addressMappingVerification(addressMappingCtx, tx, externalNetworks, state.Block().ChainID)
+			if err != nil {
+				return r, err
+			}
 		}
 
 		ctx := context.WithValue(state.Context(), ContextKeyOrigin, origin)
 		return next(state.WithContext(ctx), tx.Inner, isCheckTx)
 	})
+}
+
+func chainIdVerication(signedTx SignedTx) (loom.Address, error) {
+	var nonceTx auth.NonceTx
+	if err := proto.Unmarshal(signedTx.Inner, &nonceTx); err != nil {
+		return loom.Address{}, errors.Wrap(err, "unwrap nonce Tx")
+	}
+
+	var tx loomchain.Transaction
+	if err := proto.Unmarshal(nonceTx.Inner, &tx); err != nil {
+		return loom.Address{}, errors.New("unmarshal tx")
+	}
+
+	var msg vm.MessageTx
+	if err := proto.Unmarshal(tx.Data, &msg); err != nil {
+		return loom.Address{}, errors.Wrapf(err, "unmarshal message tx %v", tx.Data)
+	}
+
+	if msg.From == nil {
+		return loom.Address{}, errors.Errorf("nil from address")
+	}
+
+	fromAddr := loom.UnmarshalAddressPB(msg.From)
+	var err error
+	switch fromAddr.ChainID  {
+	case DefaultLoomChainId:
+		fromAddr.Local, err = verifyEd25519(signedTx)
+	case EthChainId:
+		fromAddr.Local, err = verifySolidity(signedTx)
+	default:
+		return loom.Address{}, errors.Wrapf(err, "unspported chain id %v", fromAddr.ChainID)
+	}
+
+	return fromAddr, nil
+
+}
+
+func addressMappingVerification(
+	ctx contractpb.Context,
+	tx SignedTx,
+	externalNetworks map[string]ExternalNetworks,
+	appChainId string,
+) (loom.Address, error) {
+	chain, found := externalNetworks[tx.ChainName]
+	if !found {
+		return loom.Address{}, errors.Errorf( "chain type %v not supported", tx.ChainName)
+	}
+
+	if !chain.Enabled {
+		return loom.Address{}, errors.Errorf( "chain type %v not enabled", tx.ChainName)
+	}
+
+	var localAddr []byte
+	verifier, found := originVerification[chain.Type]
+
+	if !found {
+		return loom.Address{}, fmt.Errorf("invalid chain id %v", chain.Type)
+	}
+
+	localAddr, err := verifier(tx)
+	if err != nil {
+		return loom.Address{}, errors.Wrapf(err, "tx origin cannot be verifyed as type %v", chain.Type)
+	}
+
+	origin, err := GetMappedOrigin(ctx, localAddr, chain.Prefix, appChainId, externalNetworks)
+	if err != nil {
+		return loom.Address{}, err
+	}
+	return origin, nil
 }
 
 func GetMappedOrigin(
