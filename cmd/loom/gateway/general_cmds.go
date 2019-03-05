58c62d1aa5efeb8a7f67ae0746e8a6d3f542c639
Georgios Konstantopoulos <me@gakonst.com>
2019-03-05 18:17:56 +0200

    Scaffold battle plan for doing the withdrawal

--- cmd/loom/gateway/general_cmds.go
+++ cmd/loom/gateway/general_cmds.go
@@ -262,6 +262,117 @@ func newGetOraclesCommand() *cobra.Command {
 	return cmd
 }
 
+func newWithdrawRewardsToMainnetCommand() *cobra.Command {
+	var loomKeyStr, ethAddressStr, loomcoinAddress string
+	var silent bool
+	cmd := &cobra.Command{
+		Use:     "withdraw-rewards",
+		Short:   "Links a DAppChain account to an Ethereum account via the Transfer Gateway. Requires interaction for the user to provide the ethereum signature instead of doing it in the node.",
+		Example: mapAccountsCmdExample,
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+            /**
+              * 1 Check dappchain balance before
+              * 2. Claim rewards on dappchain
+              * 3. Check balance aftr (should be bigger)
+              * 4. Call approve transactino on dappchain
+              * 5. Call withdrawLoom transaction on the dappchain 
+              * 6. Check dappchain balance, check dappchain gateway balance
+              * 7. Check account receipt
+              * 8. Create unsigned transaction and print it. GG:)
+              */
+
+			addr, err := loom.LocalAddressFromHexString(ethAddressStr)
+			if err != nil {
+				return errors.Wrap(err, "cannot parse local address from ethereum hex address")
+			}
+
+			signer, err := getDAppChainSigner(loomKeyStr)
+			if err != nil {
+				return errors.Wrap(err, "failed to load owner DAppChain key")
+			}
+
+			localOwnerAddr := loom.Address{
+				ChainID: gatewayCmdFlags.ChainID,
+				Local:   loom.LocalAddressFromPublicKey(signer.PublicKey()),
+			}
+			foreignOwnerAddr := loom.Address{
+				ChainID: "eth",
+				Local:   addr,
+			}
+
+			rpcClient := getDAppChainClient()
+			mapperAddr, err := rpcClient.Resolve("addressmapper")
+			if err != nil {
+				return errors.Wrap(err, "failed to resolve DAppChain Address Mapper address")
+			}
+			mapper := client.NewContract(rpcClient, mapperAddr.Local)
+			mappedAccount, err := getMappedAccount(mapper, localOwnerAddr)
+			if err == nil {
+				return fmt.Errorf("Account %v is already mapped to %v", localOwnerAddr, mappedAccount)
+			}
+
+			if !silent {
+				fmt.Printf(mapAccountsConfirmationMsg, localOwnerAddr, foreignOwnerAddr)
+				var input string
+				n, err := fmt.Scan(&input)
+				if err != nil {
+					return err
+				}
+				if n != 1 {
+					return errors.New("expected y/n")
+				}
+				if strings.ToLower(input) != "y" && strings.ToLower(input) != "yes" {
+					return nil
+				}
+			}
+
+			hash := ssha.SoliditySHA3(
+				ssha.Address(common.BytesToAddress(localOwnerAddr.Local)),
+				ssha.Address(common.BytesToAddress(foreignOwnerAddr.Local)),
+			)
+
+			fmt.Printf("Please paste the following hash to your signing software. After signing it, paste the signature below (prefixed with 0x\n")
+			fmt.Printf("0x%v\n", hex.EncodeToString(hash))
+
+			var sig string
+			fmt.Print("> ")
+			n, err := fmt.Scan(&sig)
+			if err != nil {
+				return err
+			}
+			if n != 1 {
+				return errors.New("invalid signature")
+			}
+
+			sigStripped, err := hex.DecodeString(sig[2:])
+			if err != nil {
+				return err
+			}
+
+			typedSig := append(make([]byte, 0, 66), byte(1))
+			var sigBytes [66]byte
+			copy(sigBytes[:], append(typedSig, sigStripped...))
+
+			req := &amtypes.AddressMapperAddIdentityMappingRequest{
+				From:      localOwnerAddr.MarshalPB(),
+				To:        foreignOwnerAddr.MarshalPB(), // eth = to
+				Signature: sigBytes[:],
+			}
+
+			_, err = mapper.Call("AddIdentityMapping", req, signer, nil)
+			return err
+		},
+	}
+	cmdFlags := cmd.Flags()
+	cmdFlags.StringVar(&ethAddressStr, "eth-address", "", "Ethereum address of account owner")
+	cmdFlags.StringVarP(&loomKeyStr, "key", "k", "", "DAppChain private key of account owner")
+	cmdFlags.BoolVar(&silent, "silent", false, "Don't ask for address confirmation")
+	cmd.MarkFlagRequired("eth-address")
+	cmd.MarkFlagRequired("key")
+	return cmd
+}
+
 func formatJSON(pb proto.Message) (string, error) {
 	marshaler := jsonpb.Marshaler{
 		Indent:       "  ",
