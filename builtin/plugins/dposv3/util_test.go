572bbd0d1944f5db59ca15db4c968975d209c23c
Roy Blankman <riblankman@gmail.com>
2019-03-07 17:31:57 -0500

    adding parameter to set new locktime in Redelegate
    - simplifying calculateTierLocktime arguments
      + resulted in test simplification as well

--- builtin/plugins/dposv3/util_test.go
+++ builtin/plugins/dposv3/util_test.go
@@ -13,24 +13,24 @@ func TestCalculateTierLockTime(t *testing.T) {
 	tier2 := TierMap[2]
 	tier3 := TierMap[3]
 
-	assert.Equal(t, TierLocktimeMap[0], calculateTierLocktime(tier0, electionCycleLength))
-	assert.Equal(t, TierLocktimeMap[1], calculateTierLocktime(tier1, electionCycleLength))
-	assert.Equal(t, TierLocktimeMap[2], calculateTierLocktime(tier2, electionCycleLength))
-	assert.Equal(t, TierLocktimeMap[3], calculateTierLocktime(tier3, electionCycleLength))
+	assert.Equal(t, TierLocktimeMap[0], calculateTierLocktime(tier0))
+	assert.Equal(t, TierLocktimeMap[1], calculateTierLocktime(tier1))
+	assert.Equal(t, TierLocktimeMap[2], calculateTierLocktime(tier2))
+	assert.Equal(t, TierLocktimeMap[3], calculateTierLocktime(tier3))
 
 	electionCycleLength = uint64(1209600) // Election cycle length = TierLockTimeMap[0] = 2 weeks
 
-	assert.Equal(t, electionCycleLength, calculateTierLocktime(tier0, electionCycleLength))
-	assert.Equal(t, TierLocktimeMap[0], calculateTierLocktime(tier0, electionCycleLength))
-	assert.Equal(t, TierLocktimeMap[1], calculateTierLocktime(tier1, electionCycleLength))
-	assert.Equal(t, TierLocktimeMap[2], calculateTierLocktime(tier2, electionCycleLength))
-	assert.Equal(t, TierLocktimeMap[3], calculateTierLocktime(tier3, electionCycleLength))
+	assert.Equal(t, electionCycleLength, calculateTierLocktime(tier0))
+	assert.Equal(t, TierLocktimeMap[0], calculateTierLocktime(tier0))
+	assert.Equal(t, TierLocktimeMap[1], calculateTierLocktime(tier1))
+	assert.Equal(t, TierLocktimeMap[2], calculateTierLocktime(tier2))
+	assert.Equal(t, TierLocktimeMap[3], calculateTierLocktime(tier3))
 
 	electionCycleLength = uint64(1209601) // For an election cycle larger than 2 weeks, the locktime is 2 weeks.
 
-	assert.NotEqual(t, electionCycleLength, calculateTierLocktime(tier0, electionCycleLength))
-	assert.Equal(t, TierLocktimeMap[0], calculateTierLocktime(tier0, electionCycleLength))
-	assert.Equal(t, TierLocktimeMap[1], calculateTierLocktime(tier1, electionCycleLength))
-	assert.Equal(t, TierLocktimeMap[2], calculateTierLocktime(tier2, electionCycleLength))
-	assert.Equal(t, TierLocktimeMap[3], calculateTierLocktime(tier3, electionCycleLength))
+	assert.NotEqual(t, electionCycleLength, calculateTierLocktime(tier0))
+	assert.Equal(t, TierLocktimeMap[0], calculateTierLocktime(tier0))
+	assert.Equal(t, TierLocktimeMap[1], calculateTierLocktime(tier1))
+	assert.Equal(t, TierLocktimeMap[2], calculateTierLocktime(tier2))
+	assert.Equal(t, TierLocktimeMap[3], calculateTierLocktime(tier3))
 }
