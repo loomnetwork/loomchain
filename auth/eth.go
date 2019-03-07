e136fcdf9ebd1b34324d98337276823fdcb51562
Eduardo Pereira <eduardonunesp@gmail.com>
2019-03-07 20:48:47 -0300

    Fix solidity verify address

--- auth/eth.go
+++ auth/eth.go
@@ -3,6 +3,8 @@
 package auth
 
 import (
+	"github.com/loomnetwork/go-ethereum/common"
+	"github.com/loomnetwork/go-ethereum/crypto"
 	"github.com/loomnetwork/go-loom/common/evmcompat"
 	"github.com/pkg/errors"
 )
@@ -16,7 +18,9 @@ func verifySolidity65Byte(tx SignedTx) ([]byte, error) {
 }
 
 func verifySolidity66Byte(tx SignedTx) ([]byte, error) {
-	ethAddr, err := evmcompat.RecoverAddressFromTypedSig(tx.PublicKey, tx.Signature)
+	ethLocalAdr := common.BytesToAddress(crypto.Keccak256(tx.PublicKey[1:])[12:])
+	ethAddr, err := evmcompat.RecoverAddressFromTypedSig(crypto.Keccak256(ethLocalAdr.Bytes()), tx.Signature)
+
 	if err != nil {
 		return nil, errors.Wrap(err, "verify solidity key")
 	}
