448885614b39df2266c776b5406f5bd6511f34e2
Piers Shepperson <piers.shepperson@btinternet.com>
2019-03-08 12:59:33 +0000

    verfy match loom-js e2e. Strengthen unit test.

--- auth/eth.go
+++ auth/eth.go
@@ -3,6 +3,8 @@
 package auth
 
 import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/loomnetwork/go-loom/common/evmcompat"
 	"github.com/pkg/errors"
 )
@@ -16,6 +18,16 @@ func verifySolidity65Byte(tx SignedTx) ([]byte, error) {
 }
 
 func verifySolidity66Byte(tx SignedTx) ([]byte, error) {
+	hash := crypto.Keccak256(common.BytesToAddress(crypto.Keccak256(tx.PublicKey[1:])[12:]).Bytes())
+
+	ethAddr, err := evmcompat.RecoverAddressFromTypedSig(hash, tx.Signature)
+	if err != nil {
+		return nil, errors.Wrap(err, "verify solidity key")
+	}
+	return ethAddr.Bytes(), nil
+}
+
+func verifySolidity66Byte2(tx SignedTx) ([]byte, error) {
 	ethAddr, err := evmcompat.RecoverAddressFromTypedSig(tx.PublicKey, tx.Signature)
 	if err != nil {
 		return nil, errors.Wrap(err, "verify solidity key")
