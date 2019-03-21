ed07a040d7940ee202e1deded271fe48fe8e0070
pathornteng <pathorn.teng@gmail.com>
2019-03-21 13:50:02 +0700

    set default value

--- auth/chainconfig_middleware.go
+++ auth/chainconfig_middleware.go
@@ -5,18 +5,23 @@ import (
 	"github.com/loomnetwork/loomchain"
 )
 
+var (
+	multiChainFaature = "mutichain"
+)
+
 func NewChainConfigMiddleware(
 	chains map[string]ChainConfig,
 	createAddressMapperCtx func(state loomchain.State) (contractpb.Context, error),
 ) loomchain.TxMiddlewareFunc {
 	MultiChainSignatureTxMiddleware := NewMultiChainSignatureTxMiddleware(chains, createAddressMapperCtx)
+	DefaultVal := (len(chains) > 0)
 	return loomchain.TxMiddlewareFunc(func(
 		state loomchain.State,
 		txBytes []byte,
 		next loomchain.TxHandlerFunc,
 		isCheckTx bool,
 	) (loomchain.TxHandlerResult, error) {
-		if state.FeatureEnabled("multichain", false) {
+		if state.FeatureEnabled(multiChainFaature, DefaultVal) {
 			return MultiChainSignatureTxMiddleware(state, txBytes, next, isCheckTx)
 		} else {
 			return SignatureTxMiddleware(state, txBytes, next, isCheckTx)
