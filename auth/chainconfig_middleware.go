ed3e22d526e259347fa04e35786e7f728ea709a8
Vadim Macagon <vadim.macagon@gmail.com>
2019-03-22 02:09:50 +0700

    Simplify ChainConfigMiddleware & update nonce RPC method
    Fixed the nonce RPC method to take into account both the on-chain
    (feature flags) and off-chain (loom.yml) config when looking up the
    nonce for an account.
    
    Removed DebugMultiChainSignatureTxMiddlewareEnabled config setting
    because it wasn't going to be any use for upgrading the test clusters.

--- auth/chainconfig_middleware.go
+++ auth/chainconfig_middleware.go
@@ -1,6 +1,9 @@
 package auth
 
 import (
+	"fmt"
+
+	loom "github.com/loomnetwork/go-loom"
 	"github.com/loomnetwork/go-loom/plugin/contractpb"
 	"github.com/loomnetwork/loomchain"
 )
@@ -9,6 +12,9 @@ const (
 	chainFeaturePrefix = "auth:sigtx:"
 )
 
+// NewChainConfigMiddleware returns middleware that verifies signed txs using either
+// SignedTxMiddleware or MultiChainSignatureTxMiddleware, it switches the underlying middleware
+// based on the on-chain and off-chain auth config settings.
 func NewChainConfigMiddleware(
 	authConfig *Config,
 	createAddressMapperCtx func(state loomchain.State) (contractpb.Context, error),
@@ -19,15 +25,10 @@ func NewChainConfigMiddleware(
 		next loomchain.TxHandlerFunc,
 		isCheckTx bool,
 	) (loomchain.TxHandlerResult, error) {
-		if authConfig.DebugMultiChainSignatureTxMiddlewareEnabled {
-			multiChainSignatureTxMiddleware := NewMultiChainSignatureTxMiddleware(authConfig.Chains, createAddressMapperCtx)
-			return multiChainSignatureTxMiddleware(state, txBytes, next, isCheckTx)
-		}
-
 		chains := getEnabledChains(authConfig.Chains, state)
 		if len(chains) > 0 {
-			multiChainSignatureTxMiddleware := NewMultiChainSignatureTxMiddleware(chains, createAddressMapperCtx)
-			return multiChainSignatureTxMiddleware(state, txBytes, next, isCheckTx)
+			mw := NewMultiChainSignatureTxMiddleware(chains, createAddressMapperCtx)
+			return mw(state, txBytes, next, isCheckTx)
 		}
 
 		return SignatureTxMiddleware(state, txBytes, next, isCheckTx)
@@ -46,4 +47,40 @@ func getEnabledChains(chains map[string]ChainConfig, state loomchain.State) map[
 		}
 	}
 	return enabledChains
+}
+
+// ResolveAccountAddress takes a local or foreign account address and returns the address used
+// to identify the account on this chain.
+func ResolveAccountAddress(
+	account loom.Address, state loomchain.State, authCfg *Config,
+	createAddressMapperCtx func(state loomchain.State) (contractpb.Context, error),
+) (loom.Address, error) {
+	chains := getEnabledChains(authCfg.Chains, state)
+	if len(chains) > 0 {
+		chain, found := authCfg.Chains[account.ChainID]
+		if !found {
+			return loom.Address{}, fmt.Errorf("unknown chain ID %s", account.ChainID)
+		}
+
+		switch chain.AccountType {
+		case NativeAccountType:
+			return account, nil
+
+		case MappedAccountType:
+			addr, err := GetActiveAddress(state, account, createAddressMapperCtx)
+			if err != nil {
+				return loom.Address{}, err
+			}
+			return addr, nil
+
+		default:
+			return loom.Address{},
+				fmt.Errorf("invalid account type %v for chain ID %s", chain.AccountType, account.ChainID)
+		}
+	}
+
+	if account.ChainID != state.Block().ChainID {
+		return loom.Address{}, fmt.Errorf("unknown chain ID %s", account.ChainID)
+	}
+	return account, nil
 }
