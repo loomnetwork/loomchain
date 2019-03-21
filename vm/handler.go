bef25673d70c6e077b71173ea3c1250193ce35fe
Vadim Macagon <vadim.macagon@gmail.com>
2019-03-21 17:01:23 +0700

    Revert changes to DeployTxHandler & CallTxHandler
    This was changed in rev 719e8c2fb13f958ad2e79976739fd199fe1ab4b7 in the
    initial implementation of multi-chain signing, but the origin & caller
    should always have the same chain ID as of rev
    88e051cb929398f33e007e7ad735283f8c12fde2

--- vm/handler.go
+++ vm/handler.go
@@ -35,11 +35,7 @@ func (h *DeployTxHandler) ProcessTx(
 	origin := auth.Origin(state.Context())
 	caller := loom.UnmarshalAddressPB(msg.From)
 
-	// TODO: This origin.ChainID == caller.ChainID must only be enabled when MultiChainSignedTxMiddleware
-	//       is used!
-	// If origin and caller have different chainIds then there has been addressmapping
-	// and this test was made in signature middleware
-	if origin.ChainID == caller.ChainID && caller.Compare(origin) != 0 {
+	if caller.Compare(origin) != 0 {
 		return r, fmt.Errorf("Origin doesn't match caller: - %v != %v", origin, caller)
 	}
 
@@ -83,8 +79,7 @@ func (h *DeployTxHandler) ProcessTx(
 	}
 
 	reg := h.CreateRegistry(state)
-	// TODO: used to pass the caller instead of the origin, must revet this change!
-	reg.Register(tx.Name, addr, origin)
+	reg.Register(tx.Name, addr, caller)
 
 	if tx.VmType == VMType_EVM {
 		r.Info = utils.DeployEvm
@@ -116,11 +111,7 @@ func (h *CallTxHandler) ProcessTx(
 	caller := loom.UnmarshalAddressPB(msg.From)
 	addr := loom.UnmarshalAddressPB(msg.To)
 
-	// TODO: This origin.ChainID == caller.ChainID must only be enabled when MultiChainSignedTxMiddleware
-	//       is used!
-	// If origin and caller have different chainIds then there has been addressmapping
-	// and this test was made in signature middleware
-	if origin.ChainID == caller.ChainID && caller.Compare(origin) != 0 {
+	if caller.Compare(origin) != 0 {
 		return r, fmt.Errorf("Origin doesn't match caller: %v != %v", origin, caller)
 	}
 
