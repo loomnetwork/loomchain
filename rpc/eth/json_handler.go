1d0ad564a1bba1f8651ce6884d6530ecf9c2a957
Piers Shepperson <piers.shepperson@btinternet.com>
2019-03-29 10:11:25 +0000

    split HttpRPC out into seperate file

--- rpc/eth/json_handler.go
+++ rpc/eth/json_handler.go
@@ -5,10 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"reflect"
-	"strings"
 
 	"github.com/gorilla/websocket"
+
 	"github.com/loomnetwork/loomchain/log"
 )
 
@@ -33,90 +32,6 @@ type JsonRpcErrorResponse struct {
 
 type RPCFunc interface {
 	unmarshalParamsAndCall(JsonRpcRequest, http.ResponseWriter, *http.Request, *websocket.Conn) (*JsonRpcResponse, *Error)
-}
-
-type HttpRPCFunc struct {
-	method    reflect.Value
-	signature []reflect.Type
-}
-
-func NewRPCFunc(method interface{}, paramNamesString string) RPCFunc {
-	var paramNames []string
-	if len(paramNamesString) > 0 {
-		paramNames = strings.Split(paramNamesString, ",")
-
-	} else {
-		paramNames = []string{}
-	}
-
-	rMethod := reflect.TypeOf(method)
-	if len(paramNames) != rMethod.NumIn() {
-		panic("parameter count mismatch making loom api method")
-	}
-	signature := []reflect.Type{}
-	for p := 0; p < rMethod.NumIn(); p++ {
-		signature = append(signature, rMethod.In(p))
-	}
-
-	return &HttpRPCFunc{
-		method:    reflect.ValueOf(method),
-		signature: signature,
-	}
-}
-
-func (m HttpRPCFunc) getInputValues(input JsonRpcRequest) (resp []reflect.Value, jsonErr *Error) {
-	paramsBytes := []json.RawMessage{}
-	if len(input.Params) > 0 {
-		if err := json.Unmarshal(input.Params, &paramsBytes); err != nil {
-			return resp, NewError(EcParseError, "Parse params", err.Error())
-		}
-	}
-	if len(paramsBytes) > len(m.signature) {
-		return resp, NewErrorf(EcInvalidParams, "Parse params", "excess input arguments, expected %v got %v", len(m.signature), len(paramsBytes))
-	}
-
-	var inValues []reflect.Value
-	for i := 0; i < len(m.signature); i++ {
-		paramValue := reflect.New(m.signature[i])
-		if i < len(paramsBytes) {
-			if err := json.Unmarshal(paramsBytes[i], paramValue.Interface()); err != nil {
-				return resp, NewErrorf(EcParseError, "Parse params", "unmarshal input parameter position %v", i)
-			}
-		}
-		inValues = append(inValues, paramValue.Elem())
-	}
-	return inValues, nil
-}
-
-func (m HttpRPCFunc) unmarshalParamsAndCall(input JsonRpcRequest, writer http.ResponseWriter, reader *http.Request, _ *websocket.Conn) (resp *JsonRpcResponse, jsonErr *Error) {
-	inValues, jsonErr := m.getInputValues(input)
-	if jsonErr != nil {
-		return resp, jsonErr
-	}
-	result, jsonErr := m.call(inValues, input.ID)
-	if jsonErr != nil {
-		return resp, jsonErr
-	}
-	return &JsonRpcResponse{
-		Result:  result,
-		Version: "2.0",
-		ID:      input.ID,
-	}, nil
-}
-
-func (m HttpRPCFunc) call(inValues []reflect.Value, id int64) (resp json.RawMessage, jsonErr *Error) {
-	outValues := m.method.Call(inValues)
-
-	if outValues[1].Interface() != nil {
-		return resp, NewErrorf(EcServer, "Server error", "loom error: %v", outValues[1].Interface())
-	}
-
-	value := outValues[0].Interface()
-	outBytes, err := json.Marshal(value)
-	if err != nil {
-		return resp, NewErrorf(EcServer, "Parse response", "json marshall return value %v", value)
-	}
-	return json.RawMessage(outBytes), nil
 }
 
 func RegisterRPCFuncs(mux *http.ServeMux, funcMap map[string]RPCFunc, logger log.TMLogger) {
