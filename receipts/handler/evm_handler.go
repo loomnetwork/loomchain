d858343c0f7fdcc74e07c7e7c61b56620be5b118
Piers Shepperson <piers.shepperson@btinternet.com>
2019-03-29 12:57:42 +0000

    convert nil encoded body value.

--- receipts/handler/evm_handler.go
+++ receipts/handler/evm_handler.go
@@ -6,6 +6,11 @@ import (
 	eth_types "github.com/ethereum/go-ethereum/core/types"
 	"github.com/loomnetwork/go-loom"
 	"github.com/loomnetwork/go-loom/plugin/types"
+	"github.com/loomnetwork/loomchain/rpc/eth"
+)
+
+const (
+	nilData eth.Data = "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
 )
 
 func (r *ReceiptHandler) GetEventsFromLogs(logs []*eth_types.Log, blockHeight int64, caller, contract loom.Address, input []byte) []*types.EventData {
@@ -26,6 +31,13 @@ func (r *ReceiptHandler) GetEventsFromLogs(logs []*eth_types.Log, blockHeight in
 			PluginName:      contract.Local.String(),
 			EncodedBody:     log.Data,
 			OriginalRequest: input,
+		}
+		if eventData.EncodedBody == nil {
+			var err error
+			eventData.EncodedBody, err = eth.DecDataToBytes(nilData)
+			if err != nil {
+				panic("cant covert nilData to bytes")
+			}
 		}
 		events = append(events, eventData)
 	}
