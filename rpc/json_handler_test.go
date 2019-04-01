dc1ed8332ab1bd2944fc3d5ba32607e7a2644d82
Piers Shepperson <piers.shepperson@btinternet.com>
2019-04-01 12:29:24 +0100

    json handler unit test

---
+++ rpc/json_handler_test.go
@@ -0,0 +1,71 @@
+package rpc
+
+import (
+	"fmt"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestQuerySeverJsonHandler(t *testing.T) {
+	qs := &MockQueryService{}
+	qs = qs
+	fmt.Println(qs)
+
+	handler := MakeEthQueryServiceHandler(qs, testlog)
+
+	tests := []struct {
+		method string
+		target string
+		params string
+	}{
+		{"eth_blockNumber", "EthBlockNumber", ``},
+		{"eth_getBlockByNumber", "EthGetBlockByNumber", ``},
+		{"eth_getBlockByHash", "EthGetBlockByHash", ``},
+		{"eth_getTransactionReceipt", "EthGetTransactionReceipt", ``},
+		{"eth_getTransactionByHash", "EthGetTransactionByHash", ``},
+		{"eth_getCode", "EthGetCode", ``},
+		{"eth_call", "EthCall", ``},
+		{"eth_getLogs", "EthGetLogs", ``},
+		{"eth_getBlockTransactionCountByNumber", "EthGetBlockTransactionCountByNumber", ``},
+		{"eth_getBlockTransactionCountByHash", "EthGetBlockTransactionCountByHash", ``},
+		{"eth_getTransactionByBlockHashAndIndex", "EthGetTransactionByBlockHashAndIndex", ``},
+		{"eth_getTransactionByBlockNumberAndIndex", "EthGetTransactionByBlockNumberAndIndex", ``},
+		{"eth_newBlockFilter", "EthNewBlockFilter", ``},
+		{"eth_newPendingTransactionFilter", "EthNewPendingTransactionFilter", ``},
+		{"eth_uninstallFilter", "EthUninstallFilter", ``},
+		{"eth_getFilterChanges", "EthGetFilterChanges", ``},
+		{"eth_getFilterLogs", "EthGetFilterLogs", ``},
+		{"eth_newFilter", "EthNewFilter", ``},
+		//{"eth_subscribe", "EthSubscribe",``},
+		{"eth_unsubscribe", "EthUnsubscribe", ``},
+		{"eth_getBalance", "EthGetBalance", ``},
+		{"eth_estimateGas", "EthEstimateGas", ``},
+		{"eth_gasPrice", "EthGasPrice", ``},
+		{"net_version", "EthNetVersion", ``},
+	}
+	for _, test := range tests {
+		payload := `{"jsonrpc":"2.0","method":"` + test.method + `","params":[` + test.params + `],"id":99}`
+		fmt.Println("payload", payload)
+		req := httptest.NewRequest("POST", "http://localhost/eth", strings.NewReader(payload))
+		rec := httptest.NewRecorder()
+		handler.ServeHTTP(rec, req)
+		require.Equal(t, test.target, qs.MethodsCalled[0])
+	}
+
+	payload := "["
+	for _, test := range tests {
+		payload += `{"jsonrpc":"2.0","method":"` + test.method + `","params":[` + test.params + `],"id":99}`
+	}
+	payload += "]"
+	fmt.Println("payload", payload)
+	req := httptest.NewRequest("POST", "http://localhost/eth", strings.NewReader(payload))
+	rec := httptest.NewRecorder()
+	handler.ServeHTTP(rec, req)
+	for i, test := range tests {
+		require.Equal(t, test.target, qs.MethodsCalled[len(qs.MethodsCalled)-1-i])
+	}
+
+}
