07b7f8b20e97dccb29e1c00e6112f39ca17db4b9
Roy Blankman <blankman@boustro.com>
2019-04-04 21:07:33 -0400

    testing v2 -> v3 state transfer in e2e
    preparing for migration unit tests

--- migrations/migration_test.go
+++ migrations/migration_test.go
@@ -1,11 +1,10 @@
-package dposv2
+package migrations
 
 import (
 	"encoding/hex"
 	"math/big"
 	"testing"
 
-	// "github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	loom "github.com/loomnetwork/go-loom"
@@ -16,7 +15,19 @@ import (
 	"github.com/loomnetwork/loomchain/builtin/plugins/coin"
 
 	d2types "github.com/loomnetwork/go-loom/builtin/types/dposv2"
-	"github.com/loomnetwork/loomchain/builtin/plugins/dposv3"
+	"github.com/loomnetwork/loomchain/builtin/plugins/dposv2"
+)
+
+var (
+	validatorPubKeyHex1 = "3866f776276246e4f9998aa90632931d89b0d3a5930e804e02299533f55b39e1"
+	validatorPubKeyHex2 = "7796b813617b283f81ea1747fbddbe73fe4b5fce0eac0728e47de51d8e506701"
+	validatorPubKeyHex3 = "e4008e26428a9bca87465e8de3a8d0e9c37a56ca619d3d6202b0567528786618"
+
+	delegatorAddress1 = loom.MustParseAddress("chain:0xb16a379ec18d4093666f8f38b11a3071c920207d")
+	delegatorAddress2 = loom.MustParseAddress("chain:0xfa4c7920accfd66b86f5fd0e69682a79f762d49e")
+	delegatorAddress3 = loom.MustParseAddress("chain:0x5cecd1f7261e1f4c684e297be3edf03b825e01c4")
+	delegatorAddress4 = loom.MustParseAddress("chain:0x000000000000000000000000e3edf03b825e01e0")
+	delegatorAddress5 = loom.MustParseAddress("chain:0x020000000000000000000000e3edf03b825e0288")
 )
 
 // TODO test the situation where there are redundant delegations in dposv2
@@ -48,28 +59,23 @@ func TestMigration(t *testing.T) {
 	})
 
 	// create dposv2 contract
-	dposv2Contract := &DPOS{}
+	dposv2Contract := &dposv2.DPOS{}
 	dposv2Addr := pctx.CreateContract(contractpb.MakePluginContract(dposv2Contract))
 	dposv2Ctx := pctx.WithAddress(dposv2Addr)
-
-	// create dposv3 contract
-	dposv3Contract := &dposv3.DPOS{}
-	dposv3Addr := pctx.CreateContract(contractpb.MakePluginContract(dposv3Contract))
-	// dposv3Ctx := pctx.WithAddress(dposv3Addr)
 
 	// transfer coins to reward fund
 	amount := big.NewInt(10)
 	amount.Exp(amount, big.NewInt(19), nil)
 	coinContract.Transfer(contractpb.WrapPluginContext(coinCtx), &coin.TransferRequest{
-		To: dposv3Addr.MarshalPB(),
+		To: dposv2Addr.MarshalPB(),
 		Amount: &types.BigUInt{
 			Value: common.BigUInt{amount},
 		},
 	})
 
 	// Init the dpos contract
-	err := dposv2Contract.Init(contractpb.WrapPluginContext(dposv2Ctx.WithSender(addr1)), &InitRequest{
-		Params: &Params{
+	err := dposv2Contract.Init(contractpb.WrapPluginContext(dposv2Ctx.WithSender(addr1)), &dposv2.InitRequest{
+		Params: &dposv2.Params{
 			CoinContractAddress: coinAddr.MarshalPB(),
 			ValidatorCount:      2,
 			ElectionCycleLength: 0,
@@ -80,15 +86,15 @@ func TestMigration(t *testing.T) {
 
 	whitelistAmount := loom.BigUInt{big.NewInt(1000000000000)}
 
-	err = dposv2Contract.ProcessRequestBatch(contractpb.WrapPluginContext(dposv2Ctx.WithSender(addr1)), &RequestBatch{
-		Batch: []*BatchRequest{
-			&BatchRequest{
-				Payload: &d2types.BatchRequestV2_WhitelistCandidate{&WhitelistCandidateRequest{
+	err = dposv2Contract.ProcessRequestBatch(contractpb.WrapPluginContext(dposv2Ctx.WithSender(addr1)), &dposv2.RequestBatch{
+		Batch: []*dposv2.BatchRequest{
+			&dposv2.BatchRequest{
+				Payload: &d2types.BatchRequestV2_WhitelistCandidate{&dposv2.WhitelistCandidateRequest{
 					CandidateAddress: addr1.MarshalPB(),
 					Amount:           &types.BigUInt{Value: whitelistAmount},
 					LockTime:         10,
 				}},
-				Meta: &BatchRequestMeta{
+				Meta: &dposv2.BatchRequestMeta{
 					BlockNumber: 1,
 					TxIndex:     0,
 					LogIndex:    0,
@@ -98,24 +104,38 @@ func TestMigration(t *testing.T) {
 	})
 	require.Nil(t, err)
 
-	err = dposv2Contract.RegisterCandidate(contractpb.WrapPluginContext(dposv2Ctx.WithSender(addr1)), &RegisterCandidateRequest{
+	err = dposv2Contract.RegisterCandidate(contractpb.WrapPluginContext(dposv2Ctx.WithSender(addr1)), &dposv2.RegisterCandidateRequest{
 		PubKey: pubKey1,
 	})
 	require.Nil(t, err)
 
-	err = Elect(contractpb.WrapPluginContext(dposv2Ctx))
+	err = dposv2.Elect(contractpb.WrapPluginContext(dposv2Ctx))
 	require.Nil(t, err)
 
 	// running a second election to make sure addr1 gets a reward delegation
-	err = Elect(contractpb.WrapPluginContext(dposv2Ctx))
+	err = dposv2.Elect(contractpb.WrapPluginContext(dposv2Ctx))
 	require.Nil(t, err)
 
-	// err = dposv2Contract.Dump(contractpb.WrapPluginContext(dposv2Ctx.WithSender(addr1)), dposv3Addr)
-	// require.Nil(t, err)
+	// DPOSv3Migration(dposv2Ctx)
+
+	// // create dposv3 contract
+	// dposv3Contract := &dposv3.DPOS{}
+	// dposv3Addr := pctx.CreateContract(contractpb.MakePluginContract(dposv3Contract))
+	// // dposv3Ctx := pctx.WithAddress(dposv3Addr)
+
 
 	// listValidatorsResponse, err := dposv3Contract.ListValidators(contractpb.WrapPluginContext(dposv3Ctx), &dposv3.ListValidatorsRequest{})
 	// require.Nil(t, err)
 	// assert.Equal(t, len(listValidatorsResponse.Statistics), 1)
 	// validator := listValidatorsResponse.Statistics[0]
 	// assert.Equal(t, whitelistAmount, validator.WhitelistAmount.Value)
+}
+
+// UTILITIES
+
+func makeAccount(owner loom.Address, bal uint64) *coin.InitialAccount {
+	return &coin.InitialAccount{
+		Owner:   owner.MarshalPB(),
+		Balance: bal,
+	}
 }
