c4ed16eb84f3c14d6b204180b76a2ece49809be2
suhasagg <suhasagg@gmail.com>
2019-04-09 20:33:12 +0530

    Enable dynamic contract loader also to prevent plugin not found error

--- config/config_test.go
+++ config/config_test.go
@@ -1,10 +1,11 @@
 package config
 
 import (
-	"github.com/stretchr/testify/assert"
 	"os"
 	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/stretchr/testify/require"
 )
@@ -26,9 +27,9 @@ func TestConfig(t *testing.T) {
 	require.True(t, reflect.DeepEqual(confDef, confRead))
 	exampleRead, err := ParseConfigFrom(exampleLoomYaml)
 	require.NoError(t, err)
-	assert.Equal(t,"static",exampleRead.ContractLoaders[0],"Test order of Loader 1")
-	assert.Equal(t,"dynamic",exampleRead.ContractLoaders[1],"Test order of Loader 2")
-	assert.Equal(t,"external",exampleRead.ContractLoaders[2],"Test order of Loader 3")
+	assert.Equal(t, "static", exampleRead.ContractLoaders[0], "Test order of Loader 1")
+	assert.Equal(t, "dynamic", exampleRead.ContractLoaders[1], "Test order of Loader 2")
+	assert.Equal(t, "external", exampleRead.ContractLoaders[2], "Test order of Loader 3")
 	require.NoError(t, exampleRead.WriteToFile(testExampleFilename+".yaml"))
 	confRead, err = ParseConfigFrom(testExampleFilename)
 	require.NoError(t, err)
