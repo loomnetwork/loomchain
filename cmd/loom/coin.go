60a3e611892e41fa9096628d1b45148384e8d11e
suhasagg <suhasagg@gmail.com>
2019-04-13 11:41:04 +0530

    Refactorings for coin command migration

--- cmd/loom/coin.go
+++ cmd/loom/coin.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	flag "github.com/spf13/pflag"
+
 	"github.com/spf13/cobra"
 
 	"github.com/loomnetwork/go-loom/builtin/types/coin"
@@ -10,10 +12,45 @@ import (
 	"github.com/loomnetwork/go-loom/types"
 )
 
+type ContractCallFlags struct {
+	WriteURI      string
+	ReadURI       string
+	MainnetURI    string
+	ContractAddr  string
+	ChainID       string
+	PrivFile      string
+	HsmConfigFile string
+	Algo          string
+	CallerChainID string
+}
+
+func AddContractCallFlags(flagSet *flag.FlagSet, callFlags *ContractCallFlags) {
+	flagSet.StringVarP(&callFlags.WriteURI, "write", "w", "http://localhost:46658/rpc", "URI for sending txs")
+	flagSet.StringVarP(&callFlags.ReadURI, "read", "r", "http://localhost:46658/query", "URI for quering app state")
+	flagSet.StringVarP(&callFlags.MainnetURI, "ethereum", "e", "http://localhost:8545", "URI for talking to Ethereum")
+	flagSet.StringVarP(&callFlags.ContractAddr, "contract", "", "", "contract address")
+	flagSet.StringVarP(&callFlags.ChainID, "chain", "", "default", "chain ID")
+	flagSet.StringVarP(&callFlags.PrivFile, "key", "k", "", "private key file")
+	flagSet.StringVarP(&callFlags.HsmConfigFile, "hsmconfig", "", "", "hsm config file")
+	flagSet.StringVar(&callFlags.Algo, "algo", "ed25519", "Signing algo: ed25519, secp256k1, tron")
+	flagSet.StringVar(&callFlags.CallerChainID, "caller-chain", "", "Overrides chain ID of caller")
+}
+
+
+func newRootCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "coin <command>",
+		Short: "Methods available in coin contract",
+	}
+	var flags ContractCallFlags
+	AddContractCallFlags(cmd.PersistentFlags(),&flags)
+	return cmd
+	}
+
 const CoinContractName = "coin"
 
 func TransferCmd() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "transfer [address] [amount]",
 		Short: "Transfer coins to another account",
 		Args:  cobra.MinimumNArgs(2),
@@ -35,10 +72,11 @@ func TransferCmd() *cobra.Command {
 			}, nil)
 		},
 	}
+	return cmd
 }
 
 func TransferFromCmd() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "transfer_from [from_address] [to_address] [amount]",
 		Short: "Transfer coins from a specified address to another",
 		Args:  cobra.MinimumNArgs(3),
@@ -65,10 +103,12 @@ func TransferFromCmd() *cobra.Command {
 			}, nil)
 		},
 	}
+	return cmd
+
 }
 
 func ApproveCmd() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "approve [address] [amount]",
 		Short: "Approve the transfer of coins to another account",
 		Args:  cobra.MinimumNArgs(2),
@@ -90,10 +130,12 @@ func ApproveCmd() *cobra.Command {
 			}, nil)
 		},
 	}
-}
+	return cmd
+
+	}
 
 func BalanceCmd() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "balance [address]",
 		Short: "Fetch the balance of a coin account",
 		Args:  cobra.MinimumNArgs(1),
@@ -117,13 +159,16 @@ func BalanceCmd() *cobra.Command {
 			return nil
 		},
 	}
+	return cmd
 }
 
-func AddCoinMethods(coinCmd  *cobra.Command) {
-	coinCmd.AddCommand(
+func AddCoinMethods() *cobra.Command{
+	cmd := newRootCommand()
+	cmd.AddCommand(
 		ApproveCmd(),
 		BalanceCmd(),
 		TransferCmd(),
 		TransferFromCmd(),
 	)
-}
+	return cmd
+	}
