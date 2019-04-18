42596e22fad149fdcb5fd4152bcb4c4a8f4aadb8
suhasagg <suhasagg@gmail.com>
2019-04-18 15:03:47 +0530

    Add `gateway supply` command to loom CLI (#839)

--- cmd/loom/gateway/gateway_cmd.go
+++ cmd/loom/gateway/gateway_cmd.go
@@ -14,6 +14,7 @@ func NewGatewayCommand() *cobra.Command {
 		newMapAccountsCommand(),
 		newQueryAccountCommand(),
 		newQueryUnclaimedTokensCommand(),
+		newQueryGatewaySupplyCommand(),
 		newReplaceOwnerCommand(),
 		newGetStateCommand(),
 		newAddOracleCommand(),
