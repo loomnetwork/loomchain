3f6a32ee6372a7ee26096d1e2b473ca8933ea031
pathornteng <pathorn.teng@gmail.com>
2019-04-19 16:46:14 +0700

    remove featureflag validators()

--- app.go
+++ app.go
@@ -95,9 +95,8 @@ func (s *StoreState) Has(key []byte) bool {
 }
 
 func (s *StoreState) Validators() []*loom.Validator {
-	// Try to get validators from DPOS contract if CtxValidatorsFeature is enabled
-	// and ValidatorSet is empty
-	if s.FeatureEnabled(CtxValidatorsFeature, false) && len(s.validators.Slice()) == 0 {
+	// Try to get validators from DPOS contract if ValidatorSet is empty
+	if len(s.validators) == 0 {
 		if s.getValidatorSet != nil {
 			validatorSet, err := s.getValidatorSet(s)
 			if err != nil {
