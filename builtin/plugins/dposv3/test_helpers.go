ca31436c8c9ae190499dad388590b18c248f6b71
Georgios Konstantopoulos <me@gakonst.com>
2019-04-24 22:24:54 +0300

    add bindings to functions in helpers

--- builtin/plugins/dposv3/test_helpers.go
+++ builtin/plugins/dposv3/test_helpers.go
@@ -4,10 +4,10 @@ import (
 	"math/big"
 
 	loom "github.com/loomnetwork/go-loom"
+	common "github.com/loomnetwork/go-loom/common"
+	"github.com/loomnetwork/go-loom/plugin"
 	contract "github.com/loomnetwork/go-loom/plugin/contractpb"
 	"github.com/loomnetwork/go-loom/types"
-	// common "github.com/loomnetwork/go-loom/common"
-	"github.com/loomnetwork/go-loom/plugin"
 	// "github.com/loomnetwork/loomchain"
 	// "github.com/loomnetwork/loomchain/builtin/plugins/coin"
 )
@@ -17,14 +17,10 @@ type testDPOSContract struct {
 	Address  loom.Address
 }
 
-func (dpos *testDPOSContract) ContractCtx(ctx *plugin.FakeContext) contract.Context {
-	return contract.WrapPluginContext(ctx.WithAddress(dpos.Address))
-}
-
 func deployDPOSContract(
 	ctx *plugin.FakeContext,
 	validatorCount uint64,
-	electionCycleLength int64,
+	electionCycleLength *int64,
 	coinAddr *loom.Address,
 	_maxYearlyReward *loom.BigUInt,
 	_registrationRequirement *loom.BigUInt,
@@ -37,10 +33,19 @@ func deployDPOSContract(
 	contractCtx := contract.WrapPluginContext(ctx.WithAddress(contractAddr))
 
 	params := &Params{
-		CoinContractAddress: coinAddr.MarshalPB(),
-		ValidatorCount:      validatorCount,
-		OracleAddress:       oracleAddr.MarshalPB(),
-		ElectionCycleLength: electionCycleLength,
+		ValidatorCount: validatorCount,
+	}
+
+	if electionCycleLength != nil {
+		params.ElectionCycleLength = *electionCycleLength
+	}
+
+	if oracleAddr != nil {
+		params.OracleAddress = oracleAddr.MarshalPB()
+	}
+
+	if coinAddr != nil {
+		params.CoinContractAddress = coinAddr.MarshalPB()
 	}
 
 	if _crashSlashingPercentage != nil {
@@ -72,7 +77,7 @@ func deployDPOSContract(
 
 func (dpos *testDPOSContract) ListCandidates(ctx *plugin.FakeContext) ([]*CandidateStatistic, error) {
 	resp, err := dpos.Contract.ListCandidates(
-		dpos.ContractCtx(ctx),
+		contract.WrapPluginContext(ctx.WithAddress(dpos.Address)),
 		&ListCandidatesRequest{},
 	)
 	if err != nil {
@@ -83,7 +88,7 @@ func (dpos *testDPOSContract) ListCandidates(ctx *plugin.FakeContext) ([]*Candid
 
 func (dpos *testDPOSContract) ListValidators(ctx *plugin.FakeContext) ([]*ValidatorStatistic, error) {
 	resp, err := dpos.Contract.ListValidators(
-		dpos.ContractCtx(ctx),
+		contract.WrapPluginContext(ctx.WithAddress(dpos.Address)),
 		&ListValidatorsRequest{},
 	)
 	if err != nil {
@@ -92,9 +97,34 @@ func (dpos *testDPOSContract) ListValidators(ctx *plugin.FakeContext) ([]*Valida
 	return resp.Statistics, err
 }
 
+func (dpos *testDPOSContract) CheckRewards(ctx *plugin.FakeContext) (*common.BigUInt, error) {
+	resp, err := dpos.Contract.CheckRewards(
+		contract.WrapPluginContext(ctx.WithAddress(dpos.Address)),
+		&CheckRewardsRequest{},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &resp.TotalRewardDistribution.Value, err
+}
+
+func (dpos *testDPOSContract) CheckDelegation(ctx *plugin.FakeContext, validator *loom.Address, delegator *loom.Address) ([]*Delegation, *common.BigUInt, *common.BigUInt, error) {
+	resp, err := dpos.Contract.CheckDelegation(
+		contract.WrapPluginContext(ctx.WithAddress(dpos.Address)),
+		&CheckDelegationRequest{
+			ValidatorAddress: validator.MarshalPB(),
+			DelegatorAddress: delegator.MarshalPB(),
+		},
+	)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	return resp.Delegations, &resp.Amount.Value, &resp.WeightedAmount.Value, nil
+}
+
 func (dpos *testDPOSContract) WhitelistCandidate(ctx *plugin.FakeContext, candidate loom.Address, amount *big.Int, tier LocktimeTier) error {
 	err := dpos.Contract.WhitelistCandidate(
-		dpos.ContractCtx(ctx),
+		contract.WrapPluginContext(ctx.WithAddress(dpos.Address)),
 		&WhitelistCandidateRequest{
 			CandidateAddress: candidate.MarshalPB(),
 			Amount:           &types.BigUInt{Value: *loom.NewBigUInt(amount)},
@@ -106,7 +136,7 @@ func (dpos *testDPOSContract) WhitelistCandidate(ctx *plugin.FakeContext, candid
 
 func (dpos *testDPOSContract) ChangeFee(ctx *plugin.FakeContext, candidateFee uint64) error {
 	err := dpos.Contract.ChangeFee(
-		dpos.ContractCtx(ctx),
+		contract.WrapPluginContext(ctx.WithAddress(dpos.Address)),
 		&ChangeCandidateFeeRequest{
 			Fee: candidateFee,
 		},
@@ -117,27 +147,104 @@ func (dpos *testDPOSContract) ChangeFee(ctx *plugin.FakeContext, candidateFee ui
 func (dpos *testDPOSContract) RegisterCandidate(
 	ctx *plugin.FakeContext,
 	pubKey []byte,
-	candidateFee uint64,
-	candidateName string,
-	candidateDescription string,
-	candidateWebsite string,
+	candidateFee *uint64,
+	candidateName *string,
+	candidateDescription *string,
+	candidateWebsite *string,
 ) error {
-	err := dpos.Contract.RegisterCandidate(dpos.ContractCtx(ctx),
-		&RegisterCandidateRequest{
-			PubKey:      pubKey,
-			Fee:         candidateFee,
-			Name:        candidateName,
-			Description: candidateDescription,
-			Website:     candidateWebsite,
-		},
+	req := RegisterCandidateRequest{
+		PubKey: pubKey,
+	}
+
+	if candidateFee != nil {
+		req.Fee = *candidateFee
+	}
+
+	if candidateName != nil {
+		req.Name = *candidateName
+	}
+
+	if candidateDescription != nil {
+		req.Description = *candidateDescription
+	}
+
+	if candidateWebsite != nil {
+		req.Website = *candidateWebsite
+	}
+
+	err := dpos.Contract.RegisterCandidate(
+		contract.WrapPluginContext(ctx.WithAddress(dpos.Address)),
+		&req,
 	)
 	return err
 }
 
 func (dpos *testDPOSContract) UnregisterCandidate(ctx *plugin.FakeContext) error {
 	err := dpos.Contract.UnregisterCandidate(
-		dpos.ContractCtx(ctx),
+		contract.WrapPluginContext(ctx.WithAddress(dpos.Address)),
 		&UnregisterCandidateRequest{},
+	)
+	return err
+}
+
+func (dpos *testDPOSContract) RemoveWhitelistedCandidate(ctx *plugin.FakeContext, candidate *loom.Address) error {
+	err := dpos.Contract.RemoveWhitelistedCandidate(
+		contract.WrapPluginContext(ctx.WithAddress(dpos.Address)),
+		&RemoveWhitelistedCandidateRequest{CandidateAddress: candidate.MarshalPB()},
+	)
+	return err
+}
+
+func (dpos *testDPOSContract) Delegate(ctx *plugin.FakeContext, validator *loom.Address, amount *big.Int, tier *uint64, referrer *string) error {
+	req := &DelegateRequest{
+		ValidatorAddress: validator.MarshalPB(),
+		Amount:           &types.BigUInt{Value: *loom.NewBigUInt(amount)},
+	}
+	if tier != nil {
+		req.LocktimeTier = *tier
+	}
+
+	if referrer != nil {
+		req.Referrer = *referrer
+	}
+
+	err := dpos.Contract.Delegate(
+		contract.WrapPluginContext(ctx.WithAddress(dpos.Address)),
+		req,
+	)
+	return err
+}
+
+func (dpos *testDPOSContract) Redelegate(ctx *plugin.FakeContext, validator *loom.Address, newValidator *loom.Address, amount *big.Int, index uint64, tier *uint64, referrer *string) error {
+	req := &RedelegateRequest{
+		FormerValidatorAddress: validator.MarshalPB(),
+		ValidatorAddress:       newValidator.MarshalPB(),
+		Amount:                 &types.BigUInt{Value: *loom.NewBigUInt(amount)},
+		Index:                  index,
+	}
+	if tier != nil {
+		req.NewLocktimeTier = *tier
+	}
+
+	if referrer != nil {
+		req.Referrer = *referrer
+	}
+
+	err := dpos.Contract.Redelegate(
+		contract.WrapPluginContext(ctx.WithAddress(dpos.Address)),
+		req,
+	)
+	return err
+}
+
+func (dpos *testDPOSContract) Unbond(ctx *plugin.FakeContext, validator *loom.Address, amount *big.Int, index uint64) error {
+	err := dpos.Contract.Unbond(
+		contract.WrapPluginContext(ctx.WithAddress(dpos.Address)),
+		&UnbondRequest{
+			ValidatorAddress: validator.MarshalPB(),
+			Amount:           &types.BigUInt{Value: *loom.NewBigUInt(amount)},
+			Index:            index,
+		},
 	)
 	return err
 }
