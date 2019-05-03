8237a360ea955bf7de8d845ec8072a61dfdc7fcb
suhasagg <suhasagg@gmail.com>
2019-05-03 22:20:28 +0530

    Loomchain CLi Refactoring

---
+++ cmd/loom/common/utils.go
@@ -0,0 +1,28 @@
+package common
+
+import (
+	"github.com/loomnetwork/go-loom/cli"
+	flag "github.com/spf13/pflag"
+)
+
+func AddContractCallFlags(flagSet *flag.FlagSet, callFlags *cli.ContractCallFlags) {
+	flagSet.StringVarP(&callFlags.URI, "uri", "u", "http://localhost:46658", "DAppChain base URI")
+	flagSet.StringVarP(&callFlags.MainnetURI, "ethereum", "e", "http://localhost:8545", "URI for talking to Ethereum")
+	flagSet.StringVar(&callFlags.ContractAddr, "contract", "", "contract address")
+	flagSet.StringVarP(&callFlags.ChainID, "chain", "c", "default", "chain ID")
+	flagSet.StringVarP(&callFlags.PrivFile, "key", "k", "", "private key file")
+	flagSet.StringVar(&callFlags.HsmConfigFile, "hsm", "", "hsm config file")
+	flagSet.StringVar(&callFlags.Algo, "algo", "ed25519", "Signing algo: ed25519, secp256k1, tron")
+	flagSet.StringVar(&callFlags.CallerChainID, "caller-chain", "", "Overrides chain ID of caller")
+}
+
+func AddContractStaticCallFlags(flagSet *flag.FlagSet, callFlags *cli.ContractCallFlags) {
+	flagSet.StringVarP(&callFlags.URI, "uri", "u", "http://localhost:46658", "DAppChain base URI")
+	flagSet.StringVarP(&callFlags.MainnetURI, "ethereum", "e", "http://localhost:8545", "URI for talking to Ethereum")
+	flagSet.StringVar(&callFlags.ContractAddr, "contract", "", "contract address")
+	flagSet.StringVarP(&callFlags.ChainID, "chain", "c", "default", "chain ID")
+	flagSet.StringVarP(&callFlags.PrivFile, "key", "k", "", "private key file")
+	flagSet.StringVar(&callFlags.HsmConfigFile, "hsm", "", "hsm config file")
+	flagSet.StringVar(&callFlags.Algo, "algo", "ed25519", "Signing algo: ed25519, secp256k1, tron")
+	flagSet.StringVar(&callFlags.CallerChainID, "caller-chain", "", "Overrides chain ID of caller")
+}
