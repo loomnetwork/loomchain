66a8cee66b23f5fa9b8134dc2d4616cbaa754177
Matthew Campbell <hyper@hyperworks.nu>
2019-05-03 15:31:44 +0700

    dposv3 check nil pointer check

--- builtin/plugins/dposv3/storage.go
+++ builtin/plugins/dposv3/storage.go
@@ -245,7 +245,7 @@ func SetStatistic(ctx contract.Context, statistic *ValidatorStatistic) error {
 func IncreaseRewardDelegation(ctx contract.Context, validator *types.Address, delegator *types.Address, increase loom.BigUInt) error {
 	// check if rewards delegation already exists
 	delegation, err := GetDelegation(ctx, REWARD_DELEGATION_INDEX, *validator, *delegator)
-	if err == contract.ErrNotFound {
+	if err == contract.ErrNotFound || delegation == nil || delegation.Amount == nil {
 		delegation = &Delegation{
 			Validator:    validator,
 			Delegator:    delegator,
