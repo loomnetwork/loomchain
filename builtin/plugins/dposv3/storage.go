c1e938e22e1742998efa15b1f5471babaa9297c1
Roy Blankman <blankman@boustro.com>
2019-05-03 08:27:18 -0400

    resetting nil changes

--- builtin/plugins/dposv3/storage.go
+++ builtin/plugins/dposv3/storage.go
@@ -85,15 +85,6 @@ func GetDelegation(ctx contract.StaticContext, index uint64, validator types.Add
 		return nil, err
 	}
 
-	amount := delegation.Amount
-	if amount == nil {
-		delegation.Amount = loom.BigZeroPB()
-	}
-	updateAmount := delegation.UpdateAmount
-	if updateAmount == nil {
-		delegation.UpdateAmount = loom.BigZeroPB()
-	}
-
 	return &delegation, nil
 }
 
