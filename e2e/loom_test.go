c9f78af52897092ea397de84549a01d44c77e49a
suhasagg <suhasagg@gmail.com>
2019-05-06 15:17:16 +0530

    Fix e2e test

--- e2e/loom_test.go
+++ e2e/loom_test.go
@@ -27,7 +27,7 @@ func TestE2eEvm(t *testing.T) {
 	common.ContractDir = "../contracts"
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			config, err := common.NewConfig(test.name, test.testFile, test.genFile, test.yamlFile, test.validators, test.accounts, 0)
+			config, err := common.NewConfig(test.name, test.testFile, test.genFile, test.yamlFile, test.validators, test.accounts, test.ethAccounts)
 			if err != nil {
 				t.Fatal(err)
 			}
