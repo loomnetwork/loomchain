e1ef574195f90331fc752c5c66a5e58a105ecbc3
suhasagg <suhasagg@gmail.com>
2019-05-14 10:41:34 +0530

    Add AddUserDeployer
    Add User deploy with loom coin balance check for whitelisting + tier check + loom coin debit after adding deploying

--- builtin/plugins/user_deployer_whitelist/user_deployer_whitelist.go
+++ builtin/plugins/user_deployer_whitelist/user_deployer_whitelist.go
@@ -1,13 +1,18 @@
 package user_deployer_whitelist
 
 import (
+	"math/big"
+
 	"github.com/golang/protobuf/proto"
-	loom "github.com/loomnetwork/go-loom"
+	"github.com/loomnetwork/go-loom"
+	ctypes "github.com/loomnetwork/go-loom/builtin/types/coin"
 	dwtypes "github.com/loomnetwork/go-loom/builtin/types/deployer_whitelist"
-	udwtypes "github.com/loomnetwork/go-loom/builtin/types/user_deployer_whitelist"
+	udtypes "github.com/loomnetwork/go-loom/builtin/types/user_deployer_whitelist"
 	"github.com/loomnetwork/go-loom/plugin"
 	contract "github.com/loomnetwork/go-loom/plugin/contractpb"
+	"github.com/loomnetwork/go-loom/types"
 	"github.com/loomnetwork/go-loom/util"
+	"github.com/loomnetwork/loomchain/builtin/plugins/coin"
 	"github.com/loomnetwork/loomchain/vm"
 	"github.com/pkg/errors"
 )
@@ -17,9 +22,9 @@ type (
 	GetUserDeployersResponse = dwtypes.ListDeployersResponse
 	Deployer                 = dwtypes.Deployer
 	AddUserDeployerRequest   = dwtypes.AddUserDeployerRequest
+	WhitelistUserDeployerRequest   = udtypes.WhitelistUserDeployerRequest
 
-	InitRequest = udwtypes.InitRequest
-)
+	)
 
 var (
 	// ErrrNotAuthorized indicates that a contract method failed because the caller didn't have
@@ -34,17 +39,20 @@ var (
 	ErrDeployerAlreadyExists = errors.New("[UserDeployerWhitelist] deployer already exists")
 	// ErrDeployerDoesNotExist returned if an owner try to to remove a deployer that does not exist
 	ErrDeployerDoesNotExist = errors.New("[UserDeployerWhitelist] deployer does not exist")
+	ErrInsufficientBalance  = errors.New("[UserDeployerWhitelist] Insufficient Loom Balance for whitelisting")
+	ErrInvalidTier = errors.New("[UserDeployerWhitelist] Invalid Tier")
 )
 
 const (
 	ownerRole           = "owner"
 	deployerPrefix      = "dep"
-	deployerStatePrefix = "dep-state"
-	userStatePrefix     = "user-state"
+	deployerStatePrefix = "userdepstate"
+	// AllowEVMDeployFlag indicates that a deployer is permitted to deploy EVM contract.
+	AllowEVMDeployFlag = dwtypes.Flags_EVM
 )
 
 var (
-	modifyPerm = []byte("modp")
+	whitelistingfees *big.Int
 )
 
 type UserDeployerWhitelist struct {
@@ -54,6 +62,10 @@ func DeployerStateKey(deployer loom.Address) []byte {
 	return util.PrefixKey([]byte(deployerStatePrefix), deployer.Bytes())
 }
 
+func deployerKey(addr loom.Address) []byte {
+	return util.PrefixKey([]byte(deployerPrefix), addr.Bytes())
+}
+
 func (uw *UserDeployerWhitelist) Meta() (plugin.Meta, error) {
 	return plugin.Meta{
 		Name:    "user-deployer-whitelist",
@@ -61,14 +73,48 @@ func (uw *UserDeployerWhitelist) Meta() (plugin.Meta, error) {
 	}, nil
 }
 
-func (uw *UserDeployerWhitelist) Init(ctx contract.Context, req *InitRequest) error {
-	if req.Owner == nil {
-		return ErrOwnerNotSpecified
+// Add User Deployer
+func (uw *UserDeployerWhitelist) AddUserDeployer(ctx contract.Context, req *WhitelistUserDeployerRequest) error {
+	coinAddr, err := ctx.Resolve("coin")
+	if err != nil {
+		return errors.Wrap(err, "address of coin contract")
 	}
-	ownerAddr := loom.UnmarshalAddressPB(req.Owner)
-	// TODO: Add relevant methods to manage owner and permissions later on.
-	ctx.GrantPermissionTo(ownerAddr, modifyPerm, ownerRole)
-	return nil
+	request := &ctypes.BalanceOfRequest{
+		Owner: req.DeployerAddr,
+	}
+	tier := req.Tier;
+	if tier != 0 {
+		return ErrInvalidTier
+	}
+	var response coin.BalanceOfResponse
+	err = contract.StaticCallMethod(ctx, coinAddr, "BalanceOf", request, &response)
+	if err != nil {
+		return err
+	}
+	if (response.Balance != nil) && (response.Balance.Value.Int.Cmp(whitelistingfees) >= 0) {
+		dwAddr, err := ctx.Resolve("deployerwhitelist")
+		if err != nil {
+			return errors.Wrap(err, "address of deployer_whitelist")
+		}
+		adddeprequest := &dwtypes.AddUserDeployerRequest{
+			DeployerAddr: req.DeployerAddr,
+		}
+		if err := contract.CallMethod(ctx, dwAddr, "AddUserDeployer", adddeprequest, nil); err != nil {
+			return errors.Wrap(err, "Adding User Deployer")
+		}
+		coinReq := &coin.TransferFromRequest{
+			To:     coinAddr.MarshalPB(),
+			From:   req.DeployerAddr,
+			Amount: &types.BigUInt{Value: *loom.NewBigUInt(whitelistingfees)},
+		}
+		if err := contract.CallMethod(ctx, coinAddr, "TransferFrom", coinReq, nil); err != nil {
+			return errors.Wrap(err, "Transferring whitelisting fees to coin contract")
+		}
+		return nil
+
+	}
+	return ErrInsufficientBalance
+
 }
 
 // GetUserDeployers returns whitelisted deployer with addresses and flags
