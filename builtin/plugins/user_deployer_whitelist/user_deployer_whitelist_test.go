991ec785b83ce6068118ff55974bce95c71e7727
suhasagg <suhasagg@gmail.com>
2019-05-17 04:20:00 +0530

    More Test Cases Added

--- builtin/plugins/user_deployer_whitelist/user_deployer_whitelist_test.go
+++ builtin/plugins/user_deployer_whitelist/user_deployer_whitelist_test.go
@@ -1,7 +1,6 @@
 package user_deployer_whitelist
 
 import (
-	"math/big"
 	"testing"
 	"time"
 
@@ -10,29 +9,33 @@ import (
 	"github.com/loomnetwork/go-loom/plugin"
 	"github.com/loomnetwork/go-loom/plugin/contractpb"
 	"github.com/loomnetwork/go-loom/types"
-	vm "github.com/loomnetwork/go-loom/vm"
+	"github.com/loomnetwork/go-loom/vm"
+	"github.com/loomnetwork/loomchain"
 	"github.com/loomnetwork/loomchain/builtin/plugins/coin"
 	"github.com/loomnetwork/loomchain/builtin/plugins/deployer_whitelist"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 var (
-	addr1        = loom.MustParseAddress("default:0xb16a379ec18d4093666f8f38b11a3071c920207d")
-	addr2        = loom.MustParseAddress("default:0x5cecd1f7261e1f4c684e297be3edf03b825e01c4")
-	addr3        = loom.MustParseAddress("default:0x5cecd1f7261e1f4c684e297be3edf03b825e01c5")
-	addr4        = loom.MustParseAddress("default:0x5cecd1f7261e1f4c684e297be3edf03b825e01c7")
-	addr5        = loom.MustParseAddress("default:0x5cecd1f7261e1f4c684e297be3edf03b825e01c9")
-	contractAddr = loom.MustParseAddress("default:0x5cecd1f7261e1f4c684e297be3edf03b825e01ab")
-	user         = addr1.MarshalPB()
-	user_addr    = addr3
-	chainID      = "default"
+	addr1         = loom.MustParseAddress("default:0xb16a379ec18d4093666f8f38b11a3071c920207d")
+	addr2         = loom.MustParseAddress("default:0x5cecd1f7261e1f4c684e297be3edf03b825e01c4")
+	addr3         = loom.MustParseAddress("default:0x5cecd1f7261e1f4c684e297be3edf03b825e01c5")
+	addr4         = loom.MustParseAddress("default:0x5cecd1f7261e1f4c684e297be3edf03b825e01c7")
+	addr5         = loom.MustParseAddress("default:0x5cecd1f7261e1f4c684e297be3edf03b825e01c9")
+	contractAddr  = loom.MustParseAddress("default:0x5cecd1f7261e1f4c684e297be3edf03b825e01ab")
+	user          = addr3.MarshalPB()
+	deployer_addr = addr1.MarshalPB()
+	chainID       = "default"
 )
 
 func TestUserDeployerWhitelistContract(t *testing.T) {
-
+	fees := sciNot(100, 18)
 	tier := &udwtypes.Tier{
-		Id:   0,
-		Fee:  &types.BigUInt{Value: *loom.NewBigUInt(big.NewInt(100))},
+		Id: 0,
+		Fee: &types.BigUInt{
+			Value: *fees,
+		},
 		Name: "DEFAULT",
 	}
 	tierList := []*udwtypes.Tier{}
@@ -42,10 +45,10 @@ func TestUserDeployerWhitelistContract(t *testing.T) {
 	}
 
 	pctx := createCtx()
-
+	pctx.SetFeature(loomchain.CoinVersion1_1Feature, true)
 	deployContract := &deployer_whitelist.DeployerWhitelist{}
-	dAddr := pctx.CreateContract(deployer_whitelist.Contract)
-	dctx := pctx.WithAddress(dAddr)
+	deployerAddr := pctx.CreateContract(deployer_whitelist.Contract)
+	dctx := pctx.WithAddress(deployerAddr)
 	err := deployContract.Init(contractpb.WrapPluginContext(dctx), &deployer_whitelist.InitRequest{
 		Owner: addr4.MarshalPB(),
 		Deployers: []*Deployer{
@@ -56,10 +59,12 @@ func TestUserDeployerWhitelistContract(t *testing.T) {
 		},
 	})
 
+	require.Nil(t, err)
+
 	coinContract := &coin.Coin{}
 	coinAddr := pctx.CreateContract(coin.Contract)
 	coinCtx := pctx.WithAddress(coinAddr)
-	coinContract.Init(contractpb.WrapPluginContext(coinCtx), &coin.InitRequest{
+	err = coinContract.Init(contractpb.WrapPluginContext(coinCtx), &coin.InitRequest{
 		Accounts: []*coin.InitialAccount{
 			{Owner: user, Balance: uint64(100)},
 		},
@@ -68,18 +73,37 @@ func TestUserDeployerWhitelistContract(t *testing.T) {
 	require.Nil(t, err)
 
 	deployerContract := &UserDeployerWhitelist{}
-	deployerAddr := pctx.CreateContract(Contract)
-	deployerCtx := pctx.WithAddress(deployerAddr)
+	userWhitelistDeployerAddr := pctx.CreateContract(Contract)
+	deployerCtx := pctx.WithAddress(userWhitelistDeployerAddr)
 
-	deployerContract.Init(contractpb.WrapPluginContext(deployerCtx), &InitRequest{
-		Owner:    addr1.MarshalPB(),
+	err = deployerContract.Init(contractpb.WrapPluginContext(deployerCtx), &InitRequest{
+		Owner:    nil,
 		TierInfo: tierInfo,
 	})
 
-	coinContract.Approve(contractpb.WrapPluginContext(coinCtx.WithSender(addr1)), &coin.ApproveRequest{
-		Spender: deployerAddr.MarshalPB(),
-		Amount:  &types.BigUInt{Value: *loom.NewBigUIntFromInt(10000)},
+	require.EqualError(t, ErrOwnerNotSpecified, err.Error(), "Owner Not specified at the time of Initialization")
+
+	err = deployerContract.Init(contractpb.WrapPluginContext(deployerCtx), &InitRequest{
+		Owner:    addr4.MarshalPB(),
+		TierInfo: nil,
 	})
+
+	require.EqualError(t, ErrMissingTierInfo, err.Error(), "Tier Info Not specified at the time of Initialization")
+
+	err = deployerContract.Init(contractpb.WrapPluginContext(deployerCtx), &InitRequest{
+		Owner:    addr4.MarshalPB(),
+		TierInfo: tierInfo,
+	})
+
+	require.Nil(t, err)
+
+	approvalAmount := sciNot(1000, 18)
+
+	err = coinContract.Approve(contractpb.WrapPluginContext(coinCtx.WithSender(addr3)), &coin.ApproveRequest{
+		Spender: userWhitelistDeployerAddr.MarshalPB(),
+		Amount:  &types.BigUInt{Value: *approvalAmount},
+	})
+
 	require.Nil(t, err)
 
 	err = deployerContract.AddUserDeployer(contractpb.WrapPluginContext(deployerCtx.WithSender(addr3)),
@@ -87,27 +111,45 @@ func TestUserDeployerWhitelistContract(t *testing.T) {
 			DeployerAddr: addr1.MarshalPB(),
 			TierId:       0,
 		})
-	require.NoError(t, err)
+
+	require.Nil(t, err)
 
 	resp1, err := coinContract.BalanceOf(contractpb.WrapPluginContext(coinCtx.WithSender(addr1)), &coin.BalanceOfRequest{
-		Owner: deployerAddr.MarshalPB(),
+		Owner: addr1.MarshalPB(),
 	})
-	require.Equal(t, int64(0), resp1.Balance.Value.Int64())
+
+	//Whitelisted fees is debited and balance of user's loom coin equals 0 as user has balance equal to
+	// whitelisted fees
+	assert.Equal(t, 0, int(resp1.Balance.Value.Int64()))
 	require.Nil(t, err)
 
+	//Error Cases
+	//Trying to Add Duplicate Deployer
 	err = deployerContract.AddUserDeployer(contractpb.WrapPluginContext(deployerCtx.WithSender(addr3)),
 		&WhitelistUserDeployerRequest{
 			DeployerAddr: addr1.MarshalPB(),
 			TierId:       0,
 		})
-	require.Equal(t, ErrDeployerAlreadyExists, err, "Trying to Add Deployer which Already Exists for User")
 
+	require.EqualError(t, ErrDeployerAlreadyExists, err.Error(), "Trying to Add Deployer which Already Exists for User")
+
+	//Invalid Tier Id specified
 	err = deployerContract.AddUserDeployer(contractpb.WrapPluginContext(deployerCtx.WithSender(addr3)),
 		&WhitelistUserDeployerRequest{
 			DeployerAddr: addr2.MarshalPB(),
 			TierId:       1,
 		})
-	require.Equal(t, ErrInvalidTier, err, "Tier Supplied is Invalid")
+
+	require.EqualError(t, ErrInvalidTier, err.Error(), "Tier Supplied is Invalid")
+
+	//Trying To Add Deployer, if User Balance is less than  whitelisting fees
+	err = deployerContract.AddUserDeployer(contractpb.WrapPluginContext(deployerCtx.WithSender(addr3)),
+		&WhitelistUserDeployerRequest{
+			DeployerAddr: addr5.MarshalPB(),
+			TierId:       0,
+		})
+
+	require.EqualError(t, ErrInsufficientBalance, err.Error(), "User Does not Have Sufficient Balance to Add Deployer")
 
 	getUserDeployersResponse, err := deployerContract.GetUserDeployers(contractpb.WrapPluginContext(
 		deployerCtx.WithSender(addr3)), &GetUserDeployersRequest{})
@@ -133,6 +175,13 @@ func TestUserDeployerWhitelistContract(t *testing.T) {
 	})
 	require.NoError(t, err)
 	require.Equal(t, 1, len(getDeployedContractsResponse.ContractAddresses))
+}
+
+func sciNot(m, n int64) *loom.BigUInt {
+	ret := loom.NewBigUIntFromInt(10)
+	ret.Exp(ret, loom.NewBigUIntFromInt(n), nil)
+	ret.Mul(ret, loom.NewBigUIntFromInt(m))
+	return ret
 }
 
 func createCtx() *plugin.FakeContext {
