25454135ca01274a967e714fbaabe7d3bc011577
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2019-05-22 11:37:27 +0700

    Add UnsafeAddAuthorizedContractMapping

--- builtin/plugins/gateway/unsafe_gateway.go
+++ builtin/plugins/gateway/unsafe_gateway.go
@@ -3,6 +3,7 @@
 package gateway
 
 import (
+	"github.com/gogo/protobuf/proto"
 	loom "github.com/loomnetwork/go-loom"
 	tgtypes "github.com/loomnetwork/go-loom/builtin/types/transfer_gateway"
 	contract "github.com/loomnetwork/go-loom/plugin/contractpb"
@@ -61,4 +62,59 @@ func (gw *UnsafeGateway) ResetOwnerKey(ctx contract.Context, req *AddOracleReque
 	ctx.GrantPermissionTo(ownerAddr, changeOraclesPerm, ownerRole)
 
 	return saveState(ctx, state)
+}
+
+// AddAuthorizedContractMapping allows overriding the existing contract mapping
+func (gw *UnsafeGateway) UnsafeAddAuthorizedContractMapping(ctx contract.Context, req *AddContractMappingRequest) error {
+	if req.ForeignContract == nil || req.LocalContract == nil {
+		return ErrInvalidRequest
+	}
+	foreignAddr := loom.UnmarshalAddressPB(req.ForeignContract)
+	localAddr := loom.UnmarshalAddressPB(req.LocalContract)
+	if foreignAddr.ChainID == "" || localAddr.ChainID == "" {
+		return ErrInvalidRequest
+	}
+	if foreignAddr.Compare(localAddr) == 0 {
+		return ErrInvalidRequest
+	}
+
+	state, err := loadState(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	callerAddr := ctx.Message().Sender
+
+	// Only the Gateway owner is allowed to bypass contract ownership checks
+	if callerAddr.Compare(loom.UnmarshalAddressPB(state.Owner)) != 0 {
+		return ErrNotAuthorized
+	}
+
+	err = ctx.Set(contractAddrMappingKey(foreignAddr), &ContractAddressMapping{
+		From: req.ForeignContract,
+		To:   req.LocalContract,
+	})
+	if err != nil {
+		return err
+	}
+
+	err = ctx.Set(contractAddrMappingKey(localAddr), &ContractAddressMapping{
+		From: req.LocalContract,
+		To:   req.ForeignContract,
+	})
+	if err != nil {
+		return err
+	}
+
+	payload, err := proto.Marshal(&ContractMappingConfirmed{
+		ForeignContract: req.ForeignContract,
+		LocalContract:   req.LocalContract,
+	})
+	if err != nil {
+		return err
+	}
+
+	ctx.EmitTopics(payload, contractMappingConfirmedEventTopic)
+	return nil
 }
