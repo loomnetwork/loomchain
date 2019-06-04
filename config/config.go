b479cecdde8493fd698cabd48436aa4ede686be5
pathornteng <pathorn.teng@gmail.com>
2019-06-04 17:55:05 +0700

    change default receipt version back to 1

--- config/config.go
+++ config/config.go
@@ -374,7 +374,7 @@ func DefaultConfig() *Config {
 		LogStateDB:                 false,
 		LogEthDbBatch:              false,
 		RegistryVersion:            int32(registry.RegistryV1),
-		ReceiptsVersion:            int32(receipts.ReceiptHandlerLevelDb),
+		ReceiptsVersion:            int32(receipts.DefaultReceiptStorage),
 		EVMPersistentTxReceiptsMax: receipts.DefaultMaxReceipts,
 		SessionDuration:            600,
 		EVMAccountsEnabled:         false,
