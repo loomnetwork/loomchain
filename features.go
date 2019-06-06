c0329bd6449d7d4818219bd74ee63a7b69ba9b43
Vadim Macagon <vadim.macagon@gmail.com>
2019-06-06 12:40:28 +0700

    Fix naming & add comment

--- features.go
+++ features.go
@@ -4,6 +4,8 @@ package loomchain
 const (
 	// Enables deduping of Mainnet events in the Gateway contract by tx hash.
 	TGCheckTxHashFeature = "tg:check-txhash"
+	// Enables hot wallet (users can submit Ethereum deposit tx hashes).
+	TGHotWalletFeature = "tg:hot-wallet"
 
 	// Enables processing of txs via MultiChainSignatureTxMiddleware, there's a feature flag per
 	// allowed chain ID, e.g. auth:sigtx:default, auth:sigtx:eth
@@ -46,6 +48,4 @@ const (
 
 	// Enables Coin v1.1 contract (also applies to ETHCoin)
 	CoinVersion1_1Feature = "coin:v1.1"
-
-	TGHotWalletClearTxHashFeature = "tg:hw:clear-tx-hash"
 )
