2c86e850f09b706cb73d70120db19e6b572f2491
Parth <desaiparth08@gmail.com>
2019-06-06 11:11:28 +0530

    Add TGHotWalletFeature (#1206)

--- features.go
+++ features.go
@@ -4,6 +4,8 @@ package loomchain
 const (
 	// Enables deduping of Mainnet events in the Gateway contract by tx hash.
 	TGCheckTxHashFeature = "tg:check-txhash"
+	// Enables hot wallet (users can submit Ethereum deposit tx hashes).
+	TGHotWalletFeature = "tg:hot-wallet"
 
 	// Enables processing of txs via MultiChainSignatureTxMiddleware, there's a feature flag per
 	// allowed chain ID, e.g. auth:sigtx:default, auth:sigtx:eth
