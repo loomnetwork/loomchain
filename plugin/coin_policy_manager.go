f2d23de21b5416aff349d27fe97782b10e020856
suhasagg <suhasagg@gmail.com>
2019-06-13 14:00:10 +0530

    Add coin policy manager

---
+++ plugin/coin_policy_manager.go
@@ -0,0 +1,52 @@
+package plugin
+
+import (
+	"github.com/loomnetwork/go-loom"
+	ctypes "github.com/loomnetwork/go-loom/builtin/types/coin"
+	contract "github.com/loomnetwork/go-loom/plugin/contractpb"
+	"github.com/loomnetwork/loomchain"
+	"github.com/loomnetwork/loomchain/builtin/plugins/coin"
+	regcommon "github.com/loomnetwork/loomchain/registry"
+	"github.com/pkg/errors"
+)
+
+var (
+	// ErrCoinContractNotFound indicates that the Coin contract hasn't been deployed yet.
+	ErrCoinContractNotFound = errors.New("[CoinDeflationManager] CoinContract contract not found")
+)
+
+// CoinPolicyManager implements loomchain.CoinPolicyManager interface
+type CoinPolicyManager struct {
+	ctx   contract.Context
+	state loomchain.State
+}
+
+type Policy = ctypes.Policy
+
+// NewCoinPolicyManager attempts to create an instance of CoinPolicyManager.
+func NewCoinPolicyManager(pvm *PluginVM, state loomchain.State) (*CoinPolicyManager,
+	error) {
+	caller := loom.RootAddress(pvm.State.Block().ChainID)
+	contractAddr, err := pvm.Registry.Resolve("coin")
+	if err != nil {
+		if err == regcommon.ErrNotFound {
+			return nil, ErrCoinContractNotFound
+		}
+		return nil, err
+	}
+	readOnly := false
+	ctx := contract.WrapPluginContext(pvm.CreateContractContext(caller, contractAddr, readOnly))
+	return &CoinPolicyManager{
+		ctx:   ctx,
+		state: state,
+	}, nil
+}
+
+//MintCoins method of coin_deflation_Manager will be called from Block
+func (c *CoinPolicyManager) MintCoins() error {
+	err := coin.Mint(c.ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
