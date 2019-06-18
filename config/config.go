962820c099bd68a046609b99acc1d2ddc9ca17dd
Atcha Prachayapron <atcha.crypto@gmail.com>
2019-06-18 12:49:34 +0700

    set default contractLoader to static and fix dpos_test

--- config/config.go
+++ config/config.go
@@ -369,7 +369,7 @@ func DefaultConfig() *Config {
 		UnsafeRPCEnabled:           false,
 		UnsafeRPCBindAddress:       "tcp://127.0.0.1:26680",
 		CreateEmptyBlocks:          true,
-		ContractLoaders:            []string{"static", "dynamic"},
+		ContractLoaders:            []string{"static"},
 		LogStateDB:                 false,
 		LogEthDbBatch:              false,
 		RegistryVersion:            int32(registry.RegistryV2),
