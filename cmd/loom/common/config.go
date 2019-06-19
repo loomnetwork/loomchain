37d83777401407639f6ff7709e68a416bb3656ba
Atcha Prachayapron <atcha.crypto@gmail.com>
2019-06-19 17:43:18 +0700

    only read HsmConfig key in loom_hsm config file

--- cmd/loom/common/config.go
+++ cmd/loom/common/config.go
@@ -1,7 +1,6 @@
 package common
 
 import (
-	"log"
 	"path/filepath"
 
 	"github.com/loomnetwork/loomchain/config"
@@ -13,24 +12,23 @@ func ParseConfig() (*config.Config, error) {
 	v := viper.New()
 	v.AutomaticEnv()
 	v.SetEnvPrefix("LOOM")
-	PasreConfigFileName("loom", v)
-	PasreConfigFileName("loom_hsm", v) // load hsm config if loom_hsm.yaml  exist
+	v.SetConfigName("loom")                       // name of config file (without extension)
+	v.AddConfigPath(".")                          // search root directory
+	v.AddConfigPath(filepath.Join(".", "config")) // search root directory /config
+	v.AddConfigPath("./../../../")
 
+	v.ReadInConfig()
 	conf := config.DefaultConfig()
 	err := v.Unmarshal(conf)
 	if err != nil {
 		return nil, err
 	}
-	return conf, err
-}
 
-func PasreConfigFileName(name string, v *viper.Viper) {
-
-	v.SetConfigName(name)                         // name of config file (without extension)
-	v.AddConfigPath(".")                          // search root directory
-	v.AddConfigPath(filepath.Join(".", "config")) // search root directory /config
-	v.AddConfigPath("./../../../")
-	if err := v.ReadInConfig(); err != nil {
-		log.Fatalf("Error reading config file, %s", err)
+	v.SetConfigName("loom_hsm")
+	v.ReadInConfig()
+	err = v.UnmarshalKey("HsmConfig", conf.HsmConfig)
+	if err != nil {
+		return nil, err
 	}
+	return conf, err
 }
