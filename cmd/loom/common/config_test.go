74dbb0757cc13ebf827b205182b20bdadd89e756
Atcha Prachayapron <atcha.crypto@gmail.com>
2019-06-19 18:56:10 +0700

    change snake case to camel case

--- cmd/loom/common/config_test.go
+++ cmd/loom/common/config_test.go
@@ -9,47 +9,47 @@ import (
 )
 
 const (
-	example_loom_hsm = "loom_hsm"
-	example_loom     = "loom"
+	exampleLoomHsm = "loom_hsm"
+	exampleLoom    = "loom"
 )
 
 func TestParseConfigWith_HSMfile(t *testing.T) {
 	conf := config.DefaultConfig()
-	conf.WriteToFile(example_loom + ".yaml")
-	conf_hsm := config.DefaultConfig()
-	conf_hsm.HsmConfig.HsmSignKeyID = 1010
-	conf_hsm.WriteToFile(example_loom_hsm + ".yaml")
+	conf.WriteToFile(exampleLoom + ".yaml")
+	confHsm := config.DefaultConfig()
+	confHsm.HsmConfig.HsmSignKeyID = 1010
+	confHsm.WriteToFile(exampleLoomHsm + ".yaml")
 	actual, err := ParseConfig()
 	if err != nil {
 		t.Error(err)
 	}
-	assert.Equal(t, conf_hsm, actual)
-	_ = os.Remove(example_loom_hsm + ".yaml")
-	_ = os.Remove(example_loom + ".yaml")
+	assert.Equal(t, confHsm, actual)
+	_ = os.Remove(exampleLoomHsm + ".yaml")
+	_ = os.Remove(exampleLoom + ".yaml")
 }
 
 func TestParseConfigWithout_HSMfile(t *testing.T) {
 	conf := config.DefaultConfig()
-	conf.WriteToFile(example_loom + ".yaml")
+	conf.WriteToFile(exampleLoom + ".yaml")
 	actual, err := ParseConfig()
 	if err != nil {
 		t.Error(err)
 	}
 	assert.Equal(t, conf, actual)
-	_ = os.Remove(example_loom + ".yaml")
+	_ = os.Remove(exampleLoom + ".yaml")
 }
 
 func TestParseConfigWith_OtherKey_In_HSMfile(t *testing.T) {
 	conf := config.DefaultConfig()
-	conf.WriteToFile(example_loom + ".yaml")
+	conf.WriteToFile(exampleLoom + ".yaml")
 	conf_hsm := config.DefaultConfig()
 	conf_hsm.DPOSVersion = 100
-	conf_hsm.WriteToFile(example_loom_hsm + ".yaml")
+	conf_hsm.WriteToFile(exampleLoomHsm + ".yaml")
 	actual, err := ParseConfig()
 	if err != nil {
 		t.Error(err)
 	}
 	assert.Equal(t, conf, actual)
-	_ = os.Remove(example_loom_hsm + ".yaml")
-	_ = os.Remove(example_loom + ".yaml")
+	_ = os.Remove(exampleLoomHsm + ".yaml")
+	_ = os.Remove(exampleLoom + ".yaml")
 }
