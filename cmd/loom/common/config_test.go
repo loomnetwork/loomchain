57ff8bc18b2c979c05ba1b3c2fecce555cd0d24a
Atcha Prachayapron <atcha.crypto@gmail.com>
2019-06-19 18:12:36 +0700

    add test case for overiding HSM configuration

--- cmd/loom/common/config_test.go
+++ cmd/loom/common/config_test.go
@@ -5,19 +5,51 @@ import (
 	"testing"
 
 	"github.com/loomnetwork/loomchain/config"
-	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
 
 const (
-	exampleHsm = "loom_hsm_test"
+	example_loom_hsm = "loom_hsm"
+	example_loom     = "loom"
 )
 
-func TestParseConfigFileName(t *testing.T) {
+func TestParseConfigWith_HSMfile(t *testing.T) {
+	conf := config.DefaultConfig()
+	conf.WriteToFile(example_loom + ".yaml")
 	conf_hsm := config.DefaultConfig()
-	conf_hsm.WriteToFile(exampleHsm + ".yaml")
-	v_actual := viper.New()
-	PasreConfigFileName(exampleHsm, v_actual)
-	assert.Equal(t, conf_hsm.HsmConfig.HsmDevType, v_actual.GetString("HsmConfig.HsmDevType"))
-	_ = os.Remove(exampleHsm + ".yaml")
+	conf_hsm.HsmConfig.HsmSignKeyID = 1010
+	conf_hsm.WriteToFile(example_loom_hsm + ".yaml")
+	actual, err := ParseConfig()
+	if err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, conf_hsm, actual)
+	_ = os.Remove(example_loom_hsm + ".yaml")
+	_ = os.Remove(example_loom + ".yaml")
+}
+
+func TestParseConfigWithout_HSMfile(t *testing.T) {
+	conf := config.DefaultConfig()
+	conf.WriteToFile(example_loom + ".yaml")
+	actual, err := ParseConfig()
+	if err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, conf, actual)
+	_ = os.Remove(example_loom + ".yaml")
+}
+
+func TestParseConfigWith_OtherKey_In_HSMfile(t *testing.T) {
+	conf := config.DefaultConfig()
+	conf.WriteToFile(example_loom + ".yaml")
+	conf_hsm := config.DefaultConfig()
+	conf_hsm.DPOSVersion = 100
+	conf_hsm.WriteToFile(example_loom_hsm + ".yaml")
+	actual, err := ParseConfig()
+	if err != nil {
+		t.Error(err)
+	}
+	assert.Equal(t, conf, actual)
+	_ = os.Remove(example_loom_hsm + ".yaml")
+	_ = os.Remove(example_loom + ".yaml")
 }
