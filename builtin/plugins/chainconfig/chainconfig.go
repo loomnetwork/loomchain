b322a3756d756fabe76a3d50898046f3a9855deb
suhasagg <suhasagg@gmail.com>
2019-07-09 08:51:28 +0530

    List Features command of chaincfg - show features present in StoreState enabled via Migration as well

--- builtin/plugins/chainconfig/chainconfig.go
+++ builtin/plugins/chainconfig/chainconfig.go
@@ -2,7 +2,7 @@ package chainconfig
 
 import (
 	"github.com/gogo/protobuf/proto"
-	loom "github.com/loomnetwork/go-loom"
+	"github.com/loomnetwork/go-loom"
 	cctypes "github.com/loomnetwork/go-loom/builtin/types/chainconfig"
 	dpostypes "github.com/loomnetwork/go-loom/builtin/types/dposv2"
 	"github.com/loomnetwork/go-loom/plugin"
@@ -16,27 +16,21 @@ import (
 )
 
 type (
-	InitRequest                = cctypes.InitRequest
-	ListFeaturesRequest        = cctypes.ListFeaturesRequest
-	ListFeaturesResponse       = cctypes.ListFeaturesResponse
-	GetFeatureRequest          = cctypes.GetFeatureRequest
-	GetFeatureResponse         = cctypes.GetFeatureResponse
-	AddFeatureRequest          = cctypes.AddFeatureRequest
-	AddFeatureResponse         = cctypes.AddFeatureResponse
-	RemoveFeatureRequest       = cctypes.RemoveFeatureRequest
-	SetParamsRequest           = cctypes.SetParamsRequest
-	GetParamsRequest           = cctypes.GetParamsRequest
-	GetParamsResponse          = cctypes.GetParamsResponse
-	Params                     = cctypes.Params
-	Feature                    = cctypes.Feature
-	ValidatorInfo              = cctypes.ValidatorInfo
-	EnableFeatureRequest       = cctypes.EnableFeatureRequest
-	EnableFeatureResponse      = cctypes.EnableFeatureResponse
-	GetValidatorInfoRequest    = cctypes.GetValidatorInfoRequest
-	GetValidatorInfoResponse   = cctypes.GetValidatorInfoResponse
-	SetValidatorInfoRequest    = cctypes.SetValidatorInfoRequest
-	ListValidatorsInfoRequest  = cctypes.ListValidatorsInfoRequest
-	ListValidatorsInfoResponse = cctypes.ListValidatorsInfoResponse
+	InitRequest           = cctypes.InitRequest
+	ListFeaturesRequest   = cctypes.ListFeaturesRequest
+	ListFeaturesResponse  = cctypes.ListFeaturesResponse
+	GetFeatureRequest     = cctypes.GetFeatureRequest
+	GetFeatureResponse    = cctypes.GetFeatureResponse
+	AddFeatureRequest     = cctypes.AddFeatureRequest
+	AddFeatureResponse    = cctypes.AddFeatureResponse
+	RemoveFeatureRequest  = cctypes.RemoveFeatureRequest
+	SetParamsRequest      = cctypes.SetParamsRequest
+	GetParamsRequest      = cctypes.GetParamsRequest
+	GetParamsResponse     = cctypes.GetParamsResponse
+	Params                = cctypes.Params
+	Feature               = cctypes.Feature
+	EnableFeatureRequest  = cctypes.EnableFeatureRequest
+	EnableFeatureResponse = cctypes.EnableFeatureResponse
 )
 
 const (
@@ -73,15 +67,11 @@ var (
 	ErrFeatureNotSupported = errors.New("[ChainConfig] feature is not supported in the current build")
 	// ErrFeatureNotFound indicates that a feature does not exist
 	ErrFeatureNotFound = errors.New("[ChainConfig] feature not found")
-	// ErrFeatureNotEnabled indacates that a feature has not been enabled
-	// by majority of validators, and has not been activated on the chain.
-	ErrFeatureNotEnabled = errors.New("[ChainConfig] feature not enabled")
 )
 
 const (
-	featurePrefix       = "ft"
-	ownerRole           = "owner"
-	validatorInfoPrefix = "vi"
+	featurePrefix = "ft"
+	ownerRole     = "owner"
 )
 
 var (
@@ -93,10 +83,6 @@ var (
 
 func featureKey(featureName string) []byte {
 	return util.PrefixKey([]byte(featurePrefix), []byte(featureName))
-}
-
-func validatorInfoKey(addr loom.Address) []byte {
-	return util.PrefixKey([]byte(validatorInfoPrefix), addr.Bytes())
 }
 
 type ChainConfig struct {
@@ -215,14 +201,15 @@ func (c *ChainConfig) RemoveFeature(ctx contract.Context, req *RemoveFeatureRequ
 }
 
 // ListFeatures returns info about all the currently known features.
+// Support for fetching Features Enabled via Migration is also added
 func (c *ChainConfig) ListFeatures(ctx contract.StaticContext, req *ListFeaturesRequest) (*ListFeaturesResponse, error) {
 	curValidators, err := getCurrentValidators(ctx)
 	if err != nil {
 		return nil, err
 	}
-
 	featureRange := ctx.Range([]byte(featurePrefix))
 	features := []*Feature{}
+	concatenatedFeatures := []*Feature{}
 	for _, m := range featureRange {
 		var f Feature
 		if err := proto.Unmarshal(m.Value, &f); err != nil {
@@ -234,10 +221,34 @@ func (c *ChainConfig) ListFeatures(ctx contract.StaticContext, req *ListFeatures
 		}
 		features = append(features, feature)
 	}
-
+	featuresFromState := ctx.GetEnabledFeatures()
+	for _, feature := range features {
+		concatenatedFeatures = append(concatenatedFeatures, feature)
+	}
+	for _, feature := range featuresFromState {
+		var f Feature
+		f.Name = feature
+		f.BlockHeight = 0
+		f.BuildNumber = 0
+		f.Status = cctypes.Feature_ENABLED
+		concatenatedFeatures = append(concatenatedFeatures, &f)
+	}
+	concatenatedFeatures = Unique(concatenatedFeatures)
 	return &ListFeaturesResponse{
-		Features: features,
+		Features: concatenatedFeatures,
 	}, nil
+}
+
+func Unique(features []*Feature) []*Feature {
+	keys := make(map[string]bool)
+	featureSet := []*Feature{}
+	for _, feature := range features {
+		if _, value := keys[feature.Name]; !value {
+			keys[feature.Name] = true
+			featureSet = append(featureSet, feature)
+		}
+	}
+	return featureSet
 }
 
 // GetFeature returns info about a specific feature.
@@ -556,69 +567,3 @@ func removeFeature(ctx contract.Context, name string) error {
 }
 
 var Contract plugin.Contract = contract.MakePluginContract(&ChainConfig{})
-
-func (c *ChainConfig) SetValidatorInfo(ctx contract.Context, req *SetValidatorInfoRequest) error {
-	if req.BuildNumber == 0 {
-		return ErrInvalidRequest
-	}
-	if !ctx.FeatureEnabled(loomchain.ChainCfgVersion1_2, false) {
-		return ErrFeatureNotEnabled
-	}
-	senderAddr := ctx.Message().Sender
-	validators, err := getCurrentValidators(ctx)
-	if err != nil {
-		return err
-	}
-	isValidator := false
-	for _, validator := range validators {
-		if validator.Compare(senderAddr) == 0 {
-			isValidator = true
-			break
-		}
-	}
-	if !isValidator {
-		return ErrNotAuthorized
-	}
-
-	validator := &ValidatorInfo{
-		Address:     senderAddr.MarshalPB(),
-		BuildNumber: req.BuildNumber,
-		UpdatedAt:   uint64(ctx.Now().Unix()),
-	}
-	return ctx.Set(validatorInfoKey(senderAddr), validator)
-}
-
-func (c *ChainConfig) GetValidatorInfo(ctx contract.StaticContext, req *GetValidatorInfoRequest) (*GetValidatorInfoResponse, error) {
-	if req.Address == nil {
-		return nil, ErrInvalidRequest
-	}
-	address := loom.UnmarshalAddressPB(req.Address)
-
-	var validatorInfo ValidatorInfo
-	err := ctx.Get(validatorInfoKey(address), &validatorInfo)
-	if err == contract.ErrNotFound {
-		return &GetValidatorInfoResponse{}, nil
-	} else if err != nil {
-		return nil, errors.Wrap(err, "failed to retrieve validator info")
-	}
-	return &GetValidatorInfoResponse{
-		Validator: &validatorInfo,
-	}, nil
-}
-
-// ListValidatorsInfo returns the build number for each validators
-func (c *ChainConfig) ListValidatorsInfo(ctx contract.StaticContext, req *ListValidatorsInfoRequest) (*ListValidatorsInfoResponse, error) {
-	validatorRange := ctx.Range([]byte(validatorInfoPrefix))
-	validators := []*ValidatorInfo{}
-	for _, m := range validatorRange {
-		var v ValidatorInfo
-		if err := proto.Unmarshal(m.Value, &v); err != nil {
-			return nil, errors.Wrapf(err, "unmarshal validators %s", string(m.Key))
-		}
-		validators = append(validators, &v)
-	}
-
-	return &ListValidatorsInfoResponse{
-		Validators: validators,
-	}, nil
-}
