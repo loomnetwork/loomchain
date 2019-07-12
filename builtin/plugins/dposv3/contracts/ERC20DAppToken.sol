cf332d10b1f4794f782551f0bb303acb2a82e25b
suhasagg <suhasagg@gmail.com>
2019-07-12 08:51:26 +0530

    ERC20 interface for token contracts deployed to Loom DAppChains

---
+++ builtin/plugins/dposv3/contracts/ERC20DAppToken.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.26;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title ERC20 interface for token contracts deployed to Loom DAppChains.
+ */
+contract ERC20DAppToken is ERC20 {
+    // Called by the DAppChain DPOS contract to mint tokens
+    //
+    // NOTE: This function will only be called by the DAppChain DPOS contract to mint ERC20 tokens
+    function mintToDPOS(uint256 amount) public;
+}
