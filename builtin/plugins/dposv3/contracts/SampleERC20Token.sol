be31cf362370f7bd2d6d77fef60951a7126b6484
suhasagg <suhasagg@gmail.com>
2019-07-18 16:13:55 +0530

    Add event support in SampleERC20Token.sol

--- builtin/plugins/dposv3/contracts/SampleERC20Token.sol
+++ builtin/plugins/dposv3/contracts/SampleERC20Token.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.28;
+pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 import "./ERC20DAppToken.sol";
@@ -10,19 +10,21 @@ contract SampleERC20Token is ERC20DAppToken, StandardToken {
     string public name = "ERC20";
     string public symbol = "ERC";
     uint8 public decimals = 6;
-    
+
+    event mintingDPOS(uint256 _amount, address _dposAddress);
+
     /**
      * @dev Constructor function
      */
     constructor(address _dpos) public {
         dpos = _dpos;
         totalSupply_ = 1000000000 * (10 ** uint256(decimals));
-        balances[_dpos] = totalSupply_;
     }
 
-    function mintToDPOS(uint256 _amount) public {
-        require(msg.sender == gateway);
+    function mintToDPOS(uint256 _amount, address _dposAddress) public {
+        require(msg.sender == _dposAddress);
         totalSupply_ = totalSupply_.add(_amount);
-        balances[dpos] = balances[dpos].add(_amount);
+        balances[_dposAddress] = balances[_dposAddress].add(_amount);
+        event mintingDPOS(_amount,_dposAddress);
     }
 }
