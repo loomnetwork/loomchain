59946f916ccf05883e790df35b15d8ed7a8bd6b2
suhasagg <suhasagg@gmail.com>
2019-07-18 16:15:25 +0530

    Modify interace to support additional parameter in function

--- builtin/plugins/dposv3/contracts/ERC20DAppToken.sol
+++ builtin/plugins/dposv3/contracts/ERC20DAppToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.26;
+pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
@@ -9,5 +9,5 @@ contract ERC20DAppToken is ERC20 {
     // Called by the DAppChain DPOS contract to mint tokens
     //
     // NOTE: This function will only be called by the DAppChain DPOS contract to mint ERC20 tokens
-    function mintToDPOS(uint256 amount) public;
+    function mintToDPOS(uint256 amount, address _dposAddress) public;
 }
