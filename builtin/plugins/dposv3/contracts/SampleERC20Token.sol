cae78927fb1ea583bcd7accb6473090a686d636a
suhasagg <suhasagg@gmail.com>
2019-07-19 14:28:26 +0530

    Updated SampleERC20Token.sol

--- builtin/plugins/dposv3/contracts/SampleERC20Token.sol
+++ builtin/plugins/dposv3/contracts/SampleERC20Token.sol
@@ -10,21 +10,21 @@ contract SampleERC20Token is ERC20DAppToken, StandardToken {
     string public name = "ERC20";
     string public symbol = "ERC";
     uint8 public decimals = 6;
-
+    event startMining();
     event mintingDPOS(uint256 _amount, address _dposAddress);
 
     /**
      * @dev Constructor function
      */
-    constructor(address _dpos) public {
-        dpos = _dpos;
+    constructor() public {
+        emit startMining();
         totalSupply_ = 1000000000 * (10 ** uint256(decimals));
     }
 
-    function mintToDPOS(uint256 _amount, address _dposAddress) public {
-        require(msg.sender == dpos);
+    function mintToDPOS(uint256 _amount, address _dposAddress) public returns (bool ok){
+        emit mintingDPOS(_amount,_dposAddress);
         totalSupply_ = totalSupply_.add(_amount);
         balances[_dposAddress] = balances[_dposAddress].add(_amount);
-        mintingDPOS(_amount,_dposAddress);
+        return true;
     }
 }
