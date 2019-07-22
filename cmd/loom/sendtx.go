80c362ed361526e838f85889447dd7cc9c7ec36b
SHA <atcha.crypto@gmail.com>
2019-07-22 12:26:46 +0700

    fix typo

--- cmd/loom/sendtx.go
+++ cmd/loom/sendtx.go
@@ -152,7 +152,7 @@ func deployGoTx(initFile, privFile, pubFile, algo, callerChainID string) error {
 			continue
 		}
 		addr := loom.UnmarshalAddressPB(response.Contract)
-		fmt.Printf("Contract %s deplyed to address %s\n", contract.Name, addr.String())
+		fmt.Printf("Contract %s deployed to address %s\n", contract.Name, addr.String())
 	}
 	fmt.Printf("%v contract(s) successfully deployed\n", numDeployed)
 	return nil
