8e89f5bab91937930ec7753b57341d62ceadbef5
Piers Shepperson <piers.shepperson@btinternet.com>
2019-07-29 15:47:29 +0100

    noevm

--- auth/noeth.go
+++ auth/noeth.go
@@ -8,7 +8,7 @@ import (
 	"github.com/loomnetwork/go-loom/common/evmcompat"
 )
 
-func verifySolidity66Byte(_ SignedTx, _ []evmcompat.SignatureType) ([]byte, error) {
+func VerifySolidity66Byte(_ SignedTx, _ []evmcompat.SignatureType) ([]byte, error) {
 	return nil, fmt.Errorf("not implemented")
 }
 
