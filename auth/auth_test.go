a56f382b4e31c29be8ed1027adc3836850d366cf
suhasagg <suhasagg@gmail.com>
2019-07-30 08:39:35 +0530

    Fix ineffassign linter error

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -72,6 +72,7 @@ func TestSignatureTxMiddlewareMultipleTxSameBlock(t *testing.T) {
 	//State is reset on every run
 	ctx2 := context.WithValue(context.Background(), ContextKeyOrigin, origin)
 	state2 := loomchain.NewStoreState(ctx2, store.NewMemStore(), abci.Header{Height: 27}, nil, nil)
+	//nolint: ineffassign
 	ctx2 = context.WithValue(ctx2, ContextKeyCheckTx, true)
 
 	//If we get the same sequence number in same block we should get an error
@@ -86,6 +87,7 @@ func TestSignatureTxMiddlewareMultipleTxSameBlock(t *testing.T) {
 	//State is reset on every run
 	ctx3 := context.WithValue(context.Background(), ContextKeyOrigin, origin)
 	state3 := loomchain.NewStoreState(ctx3, store.NewMemStore(), abci.Header{Height: 27}, nil, nil)
+	//nolint: ineffassign
 	ctx3 = context.WithValue(ctx3, ContextKeyCheckTx, true)
 
 	//If we get to tx with incrementing sequence numbers we should be fine in the same block
@@ -98,8 +100,10 @@ func TestSignatureTxMiddlewareMultipleTxSameBlock(t *testing.T) {
 	NonceTxPostNonceMiddleware(state, nonceTxBytes, loomchain.TxHandlerResult{}, nil)
 
 	//Try a deliverTx at same height it should be fine
+	//nolint: ineffassign
 	ctx3Dx := context.WithValue(context.Background(), ContextKeyOrigin, origin)
 	state3Dx := loomchain.NewStoreState(ctx3Dx, store.NewMemStore(), abci.Header{Height: 27}, nil, nil)
+	//nolint: ineffassign
 	ctx3Dx = context.WithValue(ctx3Dx, ContextKeyCheckTx, true)
 
 	_, err = NonceTxMiddleware(state3Dx, nonceTxBytes,
