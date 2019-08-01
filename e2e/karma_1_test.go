ef66b390704c500a136edcccfa90dc4eafcbfdb2
suhasagg <suhasagg@gmail.com>
2019-08-01 19:33:20 +0530

    Fix line length errors

--- e2e/karma_1_test.go
+++ e2e/karma_1_test.go
@@ -25,7 +25,8 @@ func TestE2eKarma(t *testing.T) {
 	common.ContractDir = "../contracts"
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			config, err := common.NewConfig(test.name, test.testFile, test.genFile, test.yamlFile, test.validators, test.accounts, 0, false)
+			config, err := common.NewConfig(test.name, test.testFile, test.genFile, test.yamlFile, test.validators,
+				test.accounts, 0, false)
 			if err != nil {
 				t.Fatal(err)
 			}
