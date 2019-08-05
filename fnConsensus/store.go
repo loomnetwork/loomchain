6f5f4910d13eb94f77d77faf584b553b5f019fba
Vadim Macagon <vadim.macagon@gmail.com>
2019-08-05 12:38:22 +0700

    [fnConsensus] Add some docs and minor refactoring for clarity (#1381)

--- fnConsensus/store.go
+++ fnConsensus/store.go
@@ -2,10 +2,10 @@ package fnConsensus
 
 import dbm "github.com/tendermint/tendermint/libs/db"
 
-const ReactorStateKey = "fnConsensusReactor:state"
+const reactorStateKey = "fnConsensusReactor:state"
 
-func LoadReactorState(db dbm.DB) (*ReactorState, error) {
-	rectorStateBytes := db.Get([]byte(ReactorStateKey))
+func loadReactorState(db dbm.DB) (*ReactorState, error) {
+	rectorStateBytes := db.Get([]byte(reactorStateKey))
 	if rectorStateBytes == nil {
 		return NewReactorState(), nil
 	}
@@ -17,16 +17,16 @@ func LoadReactorState(db dbm.DB) (*ReactorState, error) {
 	return persistedRectorState, nil
 }
 
-func SaveReactorState(db dbm.DB, reactorState *ReactorState, sync bool) error {
+func saveReactorState(db dbm.DB, reactorState *ReactorState, sync bool) error {
 	marshalledBytes, err := reactorState.Marshal()
 	if err != nil {
 		return err
 	}
 
 	if sync {
-		db.SetSync([]byte(ReactorStateKey), marshalledBytes)
+		db.SetSync([]byte(reactorStateKey), marshalledBytes)
 	} else {
-		db.Set([]byte(ReactorStateKey), marshalledBytes)
+		db.Set([]byte(reactorStateKey), marshalledBytes)
 	}
 
 	return nil
