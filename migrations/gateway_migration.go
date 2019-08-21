7be70a0ee6983aa02d82e70714ce55df49f981a4
Marktrs <traisak@loomx.io>
2019-08-21 14:23:35 +0700

    add gateway migration file

---
+++ migrations/gateway_migration.go
@@ -0,0 +1,16 @@
+// +build evm
+
+package migrations
+
+import (
+	"github.com/loomnetwork/transfer-gateway/builtin/plugins/gateway"
+)
+
+func GatewayMigration(ctx *MigrationContext) error {
+	gatewayCtx, err := ctx.ContractContext("gateway")
+	if err != nil {
+		return err
+	}
+	gateway.SetSignedReceiptsToUnsigned(gatewayCtx)
+	return nil
+}
