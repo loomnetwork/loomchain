12213769375215c2a7b7546fe2004b7af91a797f
Marktrs <traisak@loomx.io>
2019-08-22 19:16:27 +0700

    pass address as param to migration function

--- tx_handler/migration_tx_handler.go
+++ tx_handler/migration_tx_handler.go
@@ -1,3 +1,4 @@
+
 package tx_handler
 
 import (
--- tx_handler/migration_tx_handler_test.go
+++ tx_handler/migration_tx_handler_test.go
@@ -5,12 +5,14 @@ import (
 	"testing"
 
 	proto "github.com/gogo/protobuf/proto"
-	"github.com/loomnetwork/go-loom"
+	loom "github.com/loomnetwork/go-loom"
+	"github.com/loomnetwork/go-loom/types"
 	"github.com/loomnetwork/go-loom/vm"
 	"github.com/loomnetwork/loomchain"
 	"github.com/loomnetwork/loomchain/auth"
 	"github.com/loomnetwork/loomchain/migrations"
 	"github.com/loomnetwork/loomchain/store"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 	abci "github.com/tendermint/tendermint/abci/types"
 )
@@ -19,6 +21,10 @@ var (
 	origin = loom.MustParseAddress("chain:0x5cecd1f7261e1f4c684e297be3edf03b825e01c4")
 )
 
+type Param struct {
+	name string
+	age  int
+}
 
 func TestMigrationTxHandler(t *testing.T) {
 	state := loomchain.NewStoreState(nil, store.NewMemStore(), abci.Header{}, nil, nil)
@@ -27,12 +33,12 @@ func TestMigrationTxHandler(t *testing.T) {
 	ctx := context.WithValue(state.Context(), auth.ContextKeyOrigin, origin)
 	s := state.WithContext(ctx)
 
-	migrationTx1 := mockMessageTx(t, uint32(1), origin, origin)
+	migrationTx1 := mockMessageTx(t, uint32(1), origin, origin, []byte{})
 
 	migrationFuncs := map[int32]MigrationFunc{
 		1: func(ctx *migrations.MigrationContext, parameters []byte) error { return nil },
 		2: func(ctx *migrations.MigrationContext, parameters []byte) error { return nil },
-		3: func(ctx *migrations.MigrationContext, parameters []byte) error { return nil },
+		3: mockFunction,
 	}
 
 	migrationTxHandler := &MigrationTxHandler{
@@ -49,8 +55,17 @@ func TestMigrationTxHandler(t *testing.T) {
 	_, err = migrationTxHandler.ProcessTx(s, migrationTx1, false)
 	require.Error(t, err)
 
-	migrationTx2 := mockMessageTx(t, uint32(2), origin, origin)
-	migrationTx4 := mockMessageTx(t, uint32(4), origin, origin)
+	addressBytes, err := proto.Marshal(origin.MarshalPB())
+	require.NoError(t, err)
+
+	migrationTx2 := mockMessageTx(t, uint32(2), origin, origin, []byte{})
+	migrationTx3 := mockMessageTx(t, uint32(3), origin, origin, addressBytes)
+	migrationTx4 := mockMessageTx(t, uint32(4), origin, origin, []byte{})
+
+	state.SetFeature(loomchain.MigrationTxFeature, true)
+	state.SetFeature(loomchain.MigrationFeaturePrefix+"3", true)
+	_, err = migrationTxHandler.ProcessTx(s, migrationTx3, false)
+	require.NoError(t, err)
 
 	//Expect an error if migrationtx feature is not enabled
 	state.SetFeature(loomchain.MigrationTxFeature, false)
@@ -73,11 +88,12 @@ func TestMigrationTxHandler(t *testing.T) {
 
 }
 
-func mockMessageTx(t *testing.T, id uint32, to loom.Address, from loom.Address) []byte {
+func mockMessageTx(t *testing.T, id uint32, to loom.Address, from loom.Address, input []byte) []byte {
 	var messageTx []byte
 
 	migrationTx, err := proto.Marshal(&vm.MigrationTx{
-		ID: id,
+		ID:    id,
+		Input: input,
 	})
 	require.NoError(t, err)
 
@@ -89,4 +105,15 @@ func mockMessageTx(t *testing.T, id uint32, to loom.Address, from loom.Address) 
 	require.NoError(t, err)
 
 	return messageTx
+}
+
+func mockFunction(ctx *migrations.MigrationContext, parameters []byte) error {
+	var addr types.Address
+	if err := proto.Unmarshal(parameters, &addr); err != nil {
+		return err
+	}
+	if origin.Local.Hex() != addr.Local.Hex() {
+		return errors.New("Invalid input message")
+	}
+	return nil
 }
