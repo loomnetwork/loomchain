e2c04a5fa0fd0868268ebe939503eb3a7e163257
Marktrs <traisak@loomx.io>
2019-08-22 17:44:38 +0700

    add switch gateway request

--- migrations/gateway_migration.go
+++ migrations/gateway_migration.go
@@ -1,16 +1,24 @@
 // +build gateway
-
 package migrations
 
 import (
+	"github.com/gogo/protobuf/proto"
+	tgtypes "github.com/loomnetwork/go-loom/builtin/types/transfer_gateway"
 	"github.com/loomnetwork/transfer-gateway/builtin/plugins/gateway"
 )
 
-func GatewayMigration(ctx *MigrationContext) error {
+func GatewayMigration(ctx *MigrationContext, parameters []byte) error {
+
 	gatewayCtx, err := ctx.ContractContext("gateway")
 	if err != nil {
 		return err
 	}
-	gateway.SetSignedReceiptsToUnsigned(gatewayCtx)
+
+	gwMigrationRequest := tgtypes.TransferGatewaySwitchMainnetGatewayRequest{}
+	err = proto.Unmarshal(parameters, &gwMigrationRequest)
+	if err != nil {
+		return err
+	}
+	gateway.SwitchMainnetGateway(gatewayCtx, gwMigrationRequest)
 	return nil
 }
