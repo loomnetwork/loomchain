8017b8c8553e46f7c14233b75307350c259364b9
Marktrs <traisak@loomx.io>
2019-09-03 18:43:52 +0700

    contractpb -> jsonpb

--- migrations/gateway_migration.go
+++ migrations/gateway_migration.go
@@ -5,21 +5,17 @@ package migrations
 import (
 	"bytes"
 
+	"github.com/gogo/protobuf/jsonpb"
 	tgtypes "github.com/loomnetwork/go-loom/builtin/types/transfer_gateway"
-	"github.com/loomnetwork/go-loom/plugin/contractpb"
-	"github.com/loomnetwork/loomchain/plugin"
 	"github.com/loomnetwork/transfer-gateway/builtin/plugins/gateway"
 )
 
 func GatewayMigration(ctx *MigrationContext, parameters []byte) error {
 	gwMigrationRequest := tgtypes.TransferGatewaySwitchMainnetGatewayRequest{}
 
-	unmarshaler, err := contractpb.UnmarshalerFactory(plugin.EncodingType_JSON)
-	if err != nil {
-		return err
-	}
 	buf := bytes.NewBuffer(parameters)
-	if err := unmarshaler.Unmarshal(buf, &gwMigrationRequest); err != nil {
+	err := jsonpb.Unmarshal(buf, &gwMigrationRequest)
+	if err != nil {
 		return err
 	}
 
