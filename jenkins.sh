18dd7a333e7ed72b12e72effd27319b935d317ce
Vadim Macagon <vadim.macagon@gmail.com>
2019-09-11 15:31:35 +0700

    Build everything before linting
    This should ensure all deps are in place when the linter runs.

--- jenkins.sh
+++ jenkins.sh
@@ -25,8 +25,6 @@ cd $LOOM_SRC
 make clean
 make get_lint
 make deps
-make lint || true
-make linterrors
 make  # on OSX we don't need any C precompiles like cleveldb
 make validators-tool
 make tgoracle
@@ -43,6 +41,9 @@ cp loom loom-gateway
 make loom-cleveldb
 make plasmachain-cleveldb
 
+# lint after building everything
+make lint || true
+make linterrors
 
 export LOOM_BIN=`pwd`/loom
 export LOOM_VALIDATORS_TOOL=`pwd`/e2e/validators-tool
