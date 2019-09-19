a358a274efad92bf189f9fdd4e12766dd371a7d1
Piers Shepperson <piers.shepperson@btinternet.com>
2019-09-19 15:06:38 +0100

    tx_handler to txhandler

--- middleware/tx_middleware.go
+++ middleware/tx_middleware.go
@@ -14,7 +14,7 @@ import (
 	"github.com/loomnetwork/loomchain/state"
 	"github.com/loomnetwork/loomchain/store"
 	"github.com/loomnetwork/loomchain/throttle"
-	"github.com/loomnetwork/loomchain/tx_handler"
+	"github.com/loomnetwork/loomchain/txhandler"
 	"github.com/loomnetwork/loomchain/vm"
 )
 
@@ -131,10 +131,10 @@ func Router(
 		Manager: vmManager,
 	}
 
-	migrationTxHandler := &tx_handler.MigrationTxHandler{
+	migrationTxHandler := &txhandler.MigrationTxHandler{
 		Manager:        vmManager,
 		CreateRegistry: createRegistry,
-		Migrations: map[int32]tx_handler.MigrationFunc{
+		Migrations: map[int32]txhandler.MigrationFunc{
 			1: migrations.DPOSv3Migration,
 			2: migrations.GatewayMigration,
 			3: migrations.GatewayMigration,
