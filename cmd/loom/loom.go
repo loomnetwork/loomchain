d0efa79bd95d236afb81e81023ed1271e458fa1a
Pathorn <pathorn.teng@gmail.com>
2019-10-02 17:46:50 +0700

    address PR comment

--- cmd/loom/loom.go
+++ cmd/loom/loom.go
@@ -589,15 +589,13 @@ func destroyApp(cfg *config.Config) error {
 }
 
 func destroyReceiptsDB(cfg *config.Config) error {
-	if cfg.ReceiptsVersion == 2 || cfg.ReceiptsVersion == 3 {
-		dbPath := filepath.Join(cfg.RootPath(), cfg.EvmAuxStore.DBName+".db")
-		if !util.FileExists(dbPath) {
-			return nil
-		}
-		err := os.RemoveAll(dbPath)
-		if err != nil {
-			return err
-		}
+	dbPath := filepath.Join(cfg.RootPath(), cfg.EvmAuxStore.DBName+".db")
+	if !util.FileExists(dbPath) {
+		return nil
+	}
+	err := os.RemoveAll(dbPath)
+	if err != nil {
+		return err
 	}
 	return nil
 }
