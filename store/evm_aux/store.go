72206809f8c052202ec6272c056e6de9a22100b2
Pathorn <pathorn.teng@gmail.com>
2019-10-03 10:33:40 +0700

    address PR comment

--- store/evm_aux/store.go
+++ store/evm_aux/store.go
@@ -2,12 +2,16 @@ package evmaux
 
 import (
 	"encoding/binary"
+	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/gogo/protobuf/proto"
 	"github.com/loomnetwork/go-loom/plugin/types"
 	"github.com/loomnetwork/go-loom/util"
+	"github.com/loomnetwork/loomchain/eth/bloom"
+	"github.com/loomnetwork/loomchain/log"
+	"github.com/pkg/errors"
 	dbm "github.com/tendermint/tendermint/libs/db"
 )
 
@@ -23,6 +27,13 @@ var (
 	headKey          = []byte("leveldb:head")
 	tailKey          = []byte("leveldb:tail")
 	currentDbSizeKey = []byte("leveldb:size")
+
+	ErrTxReceiptNotFound      = errors.New("Tx receipt not found")
+	ErrPendingReceiptNotFound = errors.New("Pending receipt not found")
+)
+
+const (
+	statusTxSuccess = int32(1)
 )
 
 func dupTxHashKey(txHash []byte) []byte {
@@ -160,4 +171,180 @@ func (s *EvmAuxStore) IsDupEVMTxHash(txHash []byte) bool {
 
 func (s *EvmAuxStore) DB() dbm.DB {
 	return s.db
+}
+
+func (s *EvmAuxStore) GetReceipt(txHash []byte) (types.EvmTxReceipt, error) {
+	txReceiptProto := s.db.Get(txHash)
+	if len(txReceiptProto) == 0 {
+		return types.EvmTxReceipt{}, ErrTxReceiptNotFound
+	}
+	txReceipt := types.EvmTxReceiptListItem{}
+	err := proto.Unmarshal(txReceiptProto, &txReceipt)
+	if err != nil {
+		return types.EvmTxReceipt{}, err
+	}
+	return *txReceipt.Receipt, nil
+}
+
+func (s *EvmAuxStore) CommitReceipts(receipts []*types.EvmTxReceipt, height uint64) error {
+	if len(receipts) == 0 || s.maxReceipts == 0 {
+		return nil
+	}
+
+	batch := s.db.NewBatch()
+
+	size, headHash, tailHash, err := getDBParams(s.db)
+	if err != nil {
+		return errors.Wrap(err, "getting db params.")
+	}
+
+	tailReceiptItem := types.EvmTxReceiptListItem{}
+	if len(headHash) > 0 {
+		tailItemProto := s.db.Get(tailHash)
+		if len(tailItemProto) == 0 {
+			return errors.Wrap(err, "cannot find tail")
+		}
+		if err = proto.Unmarshal(tailItemProto, &tailReceiptItem); err != nil {
+			return errors.Wrap(err, "unmarshalling tail")
+		}
+	}
+
+	var txHashArray [][]byte
+	events := make([]*types.EventData, 0, len(receipts))
+	for _, txReceipt := range receipts {
+		if txReceipt == nil || len(txReceipt.TxHash) == 0 {
+			continue
+		}
+
+		// Update previous tail to point to current receipt
+		if len(headHash) == 0 {
+			headHash = txReceipt.TxHash
+		} else {
+			tailReceiptItem.NextTxHash = txReceipt.TxHash
+			protoTail, err := proto.Marshal(&tailReceiptItem)
+			if err != nil {
+				log.Error(fmt.Sprintf("commit block receipts: marshal receipt item: %s", err.Error()))
+				continue
+			}
+			batch.Set(tailHash, protoTail)
+			if !s.db.Has(tailHash) {
+				size++
+			}
+
+		}
+
+		// Set current receipt as next tail
+		tailHash = txReceipt.TxHash
+		tailReceiptItem = types.EvmTxReceiptListItem{Receipt: txReceipt, NextTxHash: nil}
+
+		// only upload hashes to app db if transaction successful
+		if txReceipt.Status == statusTxSuccess {
+			txHashArray = append(txHashArray, txReceipt.TxHash)
+		}
+
+		events = append(events, txReceipt.Logs...)
+	}
+
+	if len(tailHash) > 0 {
+		protoTail, err := proto.Marshal(&tailReceiptItem)
+		if err != nil {
+			log.Error(fmt.Sprintf("commit block receipts: marshal receipt item: %s", err.Error()))
+		} else {
+			batch.Set(tailHash, protoTail)
+			if !s.db.Has(tailHash) {
+				size++
+			}
+		}
+	}
+	setDBParams(batch, size, headHash, tailHash)
+
+	// Set TxHashList
+	postTxHashList, err := proto.Marshal(&types.EthTxHashList{EthTxHash: txHashArray})
+	if err != nil {
+		return errors.Wrap(err, "marshal tx hash list")
+	}
+	batch.Set(evmTxHashKey(height), postTxHashList)
+	// Set BloomFilter
+	filter := bloom.GenBloomFilter(events)
+	batch.Set(bloomFilterKey(height), filter)
+
+	// Commit
+	batch.WriteSync()
+
+	return s.RemoveOldReceipts()
+}
+
+// RemoveOldReceipts removes old receipts once the number of receipts exceeds the limit
+// TODO: This function is not used in the production since we keep all the receipts.
+//       We should probaby get rid of it and the entire linked-list structure.
+func (s *EvmAuxStore) RemoveOldReceipts() error {
+	size, headHash, tailHash, err := getDBParams(s.db)
+	if err != nil {
+		return errors.Wrap(err, "getting db params.")
+	}
+
+	// do nothing if the number of receipts does not exceed the limit
+	if s.maxReceipts >= size {
+		return nil
+	}
+
+	batch := s.db.NewBatch()
+
+	itemsDeleted := uint64(0)
+	head := headHash
+	toDeletedReceipt := size - s.maxReceipts
+	for i := uint64(0); i < toDeletedReceipt && len(head) > 0; i++ {
+		headItem := s.db.Get(head)
+		txHeadReceiptItem := types.EvmTxReceiptListItem{}
+		if err := proto.Unmarshal(headItem, &txHeadReceiptItem); err != nil {
+			return errors.Wrapf(err, "unmarshal head %s", string(headItem))
+		}
+		batch.Delete(head)
+		itemsDeleted++
+		head = txHeadReceiptItem.NextTxHash
+	}
+	if itemsDeleted < toDeletedReceipt {
+		return errors.Errorf("Unable to delete %v receipts, only %v deleted", toDeletedReceipt, itemsDeleted)
+	}
+	if size < itemsDeleted {
+		return errors.Wrap(err, "invalid count of deleted receipts")
+	}
+	size -= itemsDeleted
+
+	setDBParams(batch, size, head, tailHash)
+	batch.WriteSync()
+	return nil
+}
+
+func getDBParams(db dbm.DB) (size uint64, head, tail []byte, err error) {
+	notEmpty := db.Has(currentDbSizeKey)
+	if !notEmpty {
+		return 0, []byte{}, []byte{}, nil
+	}
+
+	sizeB := db.Get(currentDbSizeKey)
+	size = binary.LittleEndian.Uint64(sizeB)
+	if size == 0 {
+		return 0, []byte{}, []byte{}, nil
+	}
+
+	head = db.Get(headKey)
+	if len(head) == 0 {
+		return 0, []byte{}, []byte{}, errors.New("no head for non zero size receipt db")
+	}
+
+	tail = db.Get(tailKey)
+	if len(tail) == 0 {
+		return 0, []byte{}, []byte{}, errors.New("no tail for non zero size receipt db")
+	}
+
+	return size, head, tail, nil
+}
+
+func setDBParams(batch dbm.Batch, size uint64, head, tail []byte) {
+	batch.Set(headKey, head)
+	batch.Set(tailKey, tail)
+	sizeB := make([]byte, 8)
+	binary.LittleEndian.PutUint64(sizeB, size)
+	batch.Set(currentDbSizeKey, sizeB)
 }
--- store/evm_aux/store_test.go
+++ store/evm_aux/store_test.go
@@ -1,9 +1,13 @@
 package evmaux
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"fmt"
 	"testing"
 
+	"github.com/gogo/protobuf/proto"
+	"github.com/loomnetwork/go-loom/plugin/types"
 	"github.com/loomnetwork/go-loom/util"
 	"github.com/stretchr/testify/require"
 	dbm "github.com/tendermint/tendermint/libs/db"
@@ -41,4 +45,160 @@ func TestLoadDupEvmTxHashes(t *testing.T) {
 	evmAuxStore.SetDupEVMTxHashes(dupEVMTxHashes)
 	dupEvmTxHashes := evmAuxStore.GetDupEVMTxHashes()
 	require.Equal(t, 100, len(dupEvmTxHashes))
+}
+
+const (
+	dbConfigKeys = 3
+)
+
+func TestReceiptsCyclicDB(t *testing.T) {
+	evmAuxStore := NewEvmAuxStore(dbm.NewMemDB(), 10)
+	// start db
+	height := uint64(1)
+	receipts1 := makeDummyReceipts(t, 5, height)
+	commit := 1 // number of commits
+	// store 5 receipts
+	require.NoError(t, evmAuxStore.CommitReceipts(receipts1, height))
+	confirmDbConsistency(t, evmAuxStore, 5, receipts1[0].TxHash, receipts1[4].TxHash, receipts1, commit)
+	confirmStateConsistency(t, evmAuxStore, receipts1, height)
+	// db reaching max
+	height = 2
+	receipts2 := makeDummyReceipts(t, 7, height)
+	commit = 2
+	// store another 7 receipts
+	require.NoError(t, evmAuxStore.CommitReceipts(receipts2, height))
+	confirmDbConsistency(t, evmAuxStore, 10, receipts1[2].TxHash, receipts2[6].TxHash, append(receipts1[2:5], receipts2...), commit)
+	confirmStateConsistency(t, evmAuxStore, receipts2, height)
+
+	// db at max
+	height = 3
+	receipts3 := makeDummyReceipts(t, 5, height)
+	commit = 3
+	// store another 5 receipts
+	require.NoError(t, evmAuxStore.CommitReceipts(receipts3, height))
+	confirmDbConsistency(t, evmAuxStore, 10, receipts2[2].TxHash, receipts3[4].TxHash, append(receipts2[2:7], receipts3...), commit)
+	confirmStateConsistency(t, evmAuxStore, receipts3, height)
+}
+
+func TestReceiptsCommitAllInOneBlock(t *testing.T) {
+	maxSize := uint64(10)
+	evmAuxStore := NewEvmAuxStore(dbm.NewMemDB(), maxSize)
+
+	height := uint64(1)
+	receipts1 := makeDummyReceipts(t, maxSize+1, height)
+	commit := 1
+	// store 11 receipts, which is more than max that can be stored
+	require.NoError(t, evmAuxStore.CommitReceipts(receipts1, height))
+
+	confirmDbConsistency(t, evmAuxStore, maxSize, receipts1[1].TxHash, receipts1[10].TxHash, receipts1[1:], commit)
+	confirmStateConsistency(t, evmAuxStore, receipts1, height)
+
+}
+
+func confirmDbConsistency(t *testing.T, evmAuxStore *EvmAuxStore,
+	size uint64, head, tail []byte, receipts []*types.EvmTxReceipt, commit int) {
+	var err error
+	dbSize, dbHead, dbTail, err := evmAuxStore.getDBParams()
+	require.NoError(t, err)
+
+	require.EqualValues(t, size, uint64(len(receipts)))
+	require.EqualValues(t, size, dbSize)
+	require.EqualValues(t, 0, bytes.Compare(dbHead, head))
+	require.EqualValues(t, 0, bytes.Compare(dbTail, tail))
+	if size == 0 {
+		require.EqualValues(t, 0, len(head))
+		require.EqualValues(t, 0, len(tail))
+		return
+	}
+
+	for i := 0; i < len(receipts); i++ {
+		getDBReceipt, err := evmAuxStore.GetReceipt(receipts[i].TxHash)
+		require.NoError(t, err)
+		require.EqualValues(t, receipts[i].TransactionIndex, getDBReceipt.TransactionIndex)
+		require.EqualValues(t, receipts[i].BlockNumber, getDBReceipt.BlockNumber)
+		require.EqualValues(t, 0, bytes.Compare(receipts[i].TxHash, getDBReceipt.TxHash))
+	}
+	metadataCount := uint64(commit * 2)
+
+	dbActualSize := countDbEntries(evmAuxStore)
+	require.EqualValues(t, size+dbConfigKeys+metadataCount, dbActualSize)
+
+	require.EqualValues(t, 0, bytes.Compare(head, receipts[0].TxHash))
+	require.EqualValues(t, 0, bytes.Compare(tail, receipts[len(receipts)-1].TxHash))
+
+	previous := types.EvmTxReceiptListItem{}
+	for i := 0; i < len(receipts); i++ {
+		if previous.Receipt != nil {
+			require.EqualValues(t, 0, bytes.Compare(receipts[i].TxHash, previous.NextTxHash))
+		}
+		previous, err := evmAuxStore.GetReceipt(receipts[i].TxHash)
+		require.NoError(t, err)
+		require.EqualValues(t, 0, bytes.Compare(receipts[i].TxHash, previous.TxHash))
+	}
+}
+
+func confirmStateConsistency(t *testing.T, evmAuxStore *EvmAuxStore, receipts []*types.EvmTxReceipt, height uint64) {
+	txHashes, err := evmAuxStore.GetTxHashList(height)
+	require.NoError(t, err)
+	for i := 0; i < len(receipts); i++ {
+		require.EqualValues(t, 0, bytes.Compare(txHashes[i], receipts[i].TxHash))
+	}
+}
+
+func TestConfirmTransactionReceipts(t *testing.T) {
+	maxSize := uint64(10)
+	evmAuxStore := NewEvmAuxStore(dbm.NewMemDB(), maxSize)
+	height := uint64(1)
+	receipts1 := makeDummyReceipts(t, 5, height)
+	// store 5 receipts
+	require.NoError(t, evmAuxStore.CommitReceipts(receipts1, height))
+	txHashes, err := evmAuxStore.GetTxHashList(height)
+	require.NoError(t, err)
+	a := []byte("0xf0675dc27bC62b584Ab2E8E1D483a55CFac9E960")
+	b := []byte("0xe288d6eec7150D6a22FDE33F0AA2d81E06591C4d")
+	c := append(txHashes, a, b)
+
+	for i := 0; i < len(c); i++ {
+		//for i > len(c)-3 These are invalid tx hashes, so error must be returned by GetReceipt in this case
+		if i > len(c)-3 {
+			_, err1 := evmAuxStore.GetReceipt(c[i])
+			require.Error(t, err1)
+		} else {
+			//These are valid hashes so valid txReceipt must be returned
+			txReceipt, err1 := evmAuxStore.GetReceipt(c[i])
+			require.NoError(t, err1)
+			require.EqualValues(t, 0, bytes.Compare(c[i], txReceipt.TxHash))
+		}
+	}
+}
+
+func countDbEntries(evmAuxStore *EvmAuxStore) uint64 {
+	count := uint64(0)
+	db := evmAuxStore.DB()
+	iter := db.Iterator(nil, nil)
+	defer iter.Close()
+	for iter.Valid() {
+		count++
+		iter.Next()
+	}
+	return count
+}
+
+func makeDummyReceipts(t *testing.T, num, block uint64) []*types.EvmTxReceipt {
+	var dummies []*types.EvmTxReceipt
+	for i := uint64(0); i < num; i++ {
+		dummy := types.EvmTxReceipt{
+			TransactionIndex: int32(i),
+			BlockNumber:      int64(block),
+			Status:           statusTxSuccess,
+		}
+		protoDummy, err := proto.Marshal(&dummy)
+		require.NoError(t, err)
+		h := sha256.New()
+		h.Write(protoDummy)
+		dummy.TxHash = h.Sum(nil)
+
+		dummies = append(dummies, &dummy)
+	}
+	return dummies
 }
