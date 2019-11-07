621b9ef9305de56a69145ed39dddaa91ade063b3
Piers Shepperson <piers.shepperson@btinternet.com>
2019-11-07 15:27:54 +0000

    unit tests, truffle test

---
+++ e2e/tests/truffle/test/DebugFunctions.js
@@ -0,0 +1,57 @@
+const Web3 = require('web3');
+const fs = require('fs');
+const path = require('path');
+const EthereumTx = require('ethereumjs-tx').Transaction;
+const { getLoomEvmTxHash, waitForXBlocks } = require('./helpers');
+const {
+    SpeculativeNonceTxMiddleware, SignedTxMiddleware, Client,
+    LocalAddress, CryptoUtils, LoomProvider
+} = require('loom-js');
+const TxHashTestContract = artifacts.require('TxHashTestContract');
+
+contract('debug_traceTransaction', async (accounts) => {
+    let contract, fromAddr, nodeAddr, txHashTestContract, web3, web3js;
+    const StructLogsLength = 87;
+    beforeEach(async () => {
+        nodeAddr = fs.readFileSync(path.join(process.env.CLUSTER_DIR, '0', 'node_rpc_addr'), 'utf-8').trim()
+        const chainID = 'default';
+        const writeUrl = `ws://${nodeAddr}/websocket`;
+        const readUrl = `ws://${nodeAddr}/queryws`;
+
+        const privateKey = CryptoUtils.generatePrivateKey();
+        const publicKey = CryptoUtils.publicKeyFromPrivateKey(privateKey);
+
+        fromAddr = LocalAddress.fromPublicKey(publicKey);
+        const from = fromAddr.toString();
+
+        var client = new Client(chainID, writeUrl, readUrl);
+        client.on('error', msg => {
+            console.error('Error on connect to client', msg);
+            console.warn('Please verify if loom cluster is running')
+        });
+        const setupMiddlewareFn = function(client, privateKey) {
+            const publicKey = CryptoUtils.publicKeyFromPrivateKey(privateKey);
+            return [new SpeculativeNonceTxMiddleware(publicKey, client), new SignedTxMiddleware(privateKey)]
+        };
+        var loomProvider = new LoomProvider(client, privateKey, setupMiddlewareFn);
+
+        web3 = new Web3(loomProvider);
+        web3js = new Web3(new Web3.providers.HttpProvider(`http://${nodeAddr}/eth`));
+
+        txHashTestContract = await TxHashTestContract.deployed();
+        contract = new web3.eth.Contract(TxHashTestContract._json.abi, txHashTestContract.address, {from});
+    });
+
+    it('Test debug_traceTransaction', async () => {
+        const txResult = await contract.methods.set(1111).send();
+        await web3js.currentProvider.send({
+            method: "debug_traceTransaction",
+            params: [txResult.transactionHash,{"disableStorage":true,"disableMemory":false,"disableStack":false,"fullStorage":false}],
+            jsonrpc: "2.0",
+            id: new Date().getTime()
+        }, function (error, result) {
+            assert.equal(StructLogsLength, result.result.structLogs.length);
+        });
+        await waitForXBlocks(nodeAddr, 1)
+    })
+});
