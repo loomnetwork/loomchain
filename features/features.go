46306495435fbb289ea4723c38df68da22c36b70
Deeprom <grooveeeeee@gmail.com>
2019-11-18 16:18:12 +0700

    Add feature flag to enable fees for LOOM withdrawals via Binance Gateway (#1583)

--- features/features.go
+++ features/features.go
@@ -21,6 +21,8 @@ const (
 	TGVersion1_2 = "tg:v1.2"
 	// Enable token precision adjustment for LOOM deposits & withdrawals via Binance Gateway
 	TGVersion1_3 = "tg:v1.3"
+	// Enable charging fees (in BNB) for LOOM withdrawals via Binance Gateway contract
+	TGVersion1_4 = "tg:v1.4"
 
 	// Enables support for mapping DAppChain accounts to Binance accounts
 	AddressMapperVersion1_1 = "addrmapper:v1.1"
