bfe5ba344c35034a1a3ab8a2b5cf3bed55342507
Andrei Popescu <andrei.popescu@gmail.com>
2020-06-04 19:34:42 +0300

    grammar
    grammar

--- parselintreport.sh
+++ parselintreport.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 count=`grep ".go" lintreport | wc -l`
-echo "The number of errors $count"
+echo "The number of errors is $count"
 if [ $count -le 54 ] #Set this to a value higher than the current number of linter errors. We should lower this number over time
 then
   echo "The number of errors is within the threshold."
